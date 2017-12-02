d80d0a987d91113a847f89a31fc83cb403d0678c
rmontserrat <romulomontserrat@gmail.com>
2017-12-02 12:03:54 -0200

    criando componente weather

---
+++ src/components/weatherCard.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const RenderWeatherToday = (props) => {
+  const data = props.data
+
+  if (data.location) {
+    return (
+      <div>
+        <p>HOJE</p>
+        <p>{data.item.condition.temp}</p>
+        <p>{data.item.condition.text}</p>
+        <p>Vento: NO {data.wind.speed}km/h</p>
+        <p>Humidade:{data.atmosphere.humidity}</p>
+        <p>Pressão:{data.atmosphere.pressure}</p>
+      </div>
+    )
+  } else {
+    return (
+      <div className='weather-empty' />
+    )
+  }
+}
+
+const RenderWeatherTomorrow = (props) => {
+  const data = props.data
+
+  if (data.location) {
+    return (
+      <div>
+        <p>AMANHÃ</p>
+        <p>{data.item.forecast[1].low}</p>
+      </div>
+    )
+  } else {
+    return (
+        <div className='weather-empty' />
+    )
+  }
+}
+
+const RenderWeatherAfterTomorrow = (props) => {
+  const data = props.data
+
+  if (data.location) {
+    return (
+      <div>
+        <p>DEPOIS DE AMANHÃ</p>
+        <p>{data.item.forecast[2].low}</p>
+      </div>
+    )
+  } else {
+    return (
+      <div className='weather-empty' />
+    )
+  }
+}
+
+export default (props) =>
+  <section>
+    <RenderWeatherToday data={props.data} />
+    <RenderWeatherTomorrow data={props.data} />
+    <RenderWeatherAfterTomorrow data={props.data} />
+  </section>
