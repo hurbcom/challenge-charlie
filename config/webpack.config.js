4f753448f7b805736d047e7f2d4a19093368fe1e
Tiago Rocha <tiagofarocha@yahoo.com.br>
2018-02-01 22:14:39 -0200

    first commit

---
+++ config/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
+            }]
+        }]
+    }
+};
