25e3636816df6a48989bb1bcb950b03a9a53d8fd
Tiago Rocha <tiagofarocha@yahoo.com.br>
2018-02-04 21:48:21 -0200

    add forecast and updating css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
-import Text from './components/Text'
+import './app.css';
+import Text from './components/Text';
 import ForecastViewModel from './models/ForecastViewModel';
+/*import Forecast from './components/Forecast';*/
 
-class App extends Component {
+export default class App extends Component {
 
   constructor() {
     super()
     this.state = {
       image: null,
-      forecast: null
+      forecast: null,
+      willMount: false,
+      isCelsius: true,
+      today: null,
+      tomorrow: null,
+      afterTomorrow: null,
+      temperature: null
     }
   }
 
   componentWillMount() {
-    this.getBackgroundPage()
+    this.fetchBackgroundPage();
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(this.successFunction.bind(this));
@@ -30,16 +37,20 @@ class App extends Component {
     fetch(url)
       .then(response => response.json())
       .then(response => {
-        const teste = new ForecastViewModel(response);
+        const forecast = new ForecastViewModel(response);
         this.setState({
-          forecast: teste
+          forecast: forecast,
+          willMount: true
         })
+
+        this.toggleTemperature(this.state.forecast.temperatures);
       })
   }
 
-  getBackgroundPage() {
+  fetchBackgroundPage() {
+    const no_cors = 'https://cors-anywhere.herokuapp.com/';
     const url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR";
-    fetch(url)
+    fetch(`${no_cors}${url}`)
       .then(response => response.json())
       .then(response => {
           const image = `https://www.bing.com${response.images[0].url}`;
@@ -49,11 +60,12 @@ class App extends Component {
       });
   }
 
-  getForecast() {
+  fetchForecast() {
     const location_name = this.textInput.value;
     if(!location_name) {
       return
     }
+
     const url = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${location_name}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
 
     fetch(url)
@@ -62,41 +74,108 @@ class App extends Component {
         if(!response.query.results) {
           return
         }
+
         const forecast = new ForecastViewModel(response);
+
         this.setState({
-          forecast: forecast
+          forecast: forecast,
+          isCelsius: true
         })
+
+        this.toggleTemperature(this.state.forecast.temperatures);
       })
   }
+
+  toCelsius(temperature) {
+    return Math.round((parseInt(temperature) - 32) / (9 / 5));
+  }
+  toFarenheit(temperature) {
+    return Math.round((parseInt(temperature) + 32) / (9 / 5));
+  }
+
+  toggleTemperature(temperature) {
+
+    if(this.state.isCelsius) {
+      let today = this.toCelsius(temperature[0]); 
+      let tomorrow = this.toCelsius(temperature[1]);  
+      let afterTomorrow = this.toCelsius(temperature[2]); 
+
+      this.setState({
+        isCelsius: false,
+        today: today,
+        tomorrow: tomorrow,
+        afterTomorrow: afterTomorrow
+      })
+
+    } else {
+      let today = this.toFarenheit(temperature[0]); 
+      let tomorrow = this.toFarenheit(temperature[1]);  
+      let afterTomorrow = this.toFarenheit(temperature[2]); 
+
+      this.setState({
+        isCelsius: true,
+        today: today,
+        tomorrow: tomorrow,
+        afterTomorrow: afterTomorrow
+      })
+      
+    }
+  }
+
   render() {
     let { forecast } = this.state;
+
     return (
+      this.state.willMount ?
       <div className="app" style={{background: `url(${this.state.image})`}}>
         <div className="main-box">
           <div className="search">
             <input 
-              type="text"
+              type="combobox"
               ref={(input) => { this.textInput = input; }}
               className="text-input"/>
             <button 
-              onClick={ this.getForecast.bind(this) }
+              onClick={ this.fetchForecast.bind(this) }
               className="button-search">Buscar</button>
             <Text 
               className="text-location"
-              text={forecast ? forecast.location : 'Buscando localização atual...'}/>
+              text={ forecast.location }/>
           </div>
-          <div className="box-today">
-            <Text 
-              className="text"
-              text={forecast ? forecast.temperature : null}/>
-          </div>
-          <div className="box-tommorrow">
-          </div>
-          <div className="box-after-tommorrow"></div>
+          <div className="forecast">
+        <div className="container box-today">
+          <Text 
+            className="box-forecast"
+            text="Hoje"/>
+
+          <p 
+            className="temperatures" 
+            onClick={ this.toggleTemperature.bind(this, forecast.temperatures)}>
+            { this.state.isCelsius ? `${this.state.today}ºF ` : `${this.state.today}ºC ` }
+          </p>
+        </div>
+        <div className="container box-tommorrow">
+          <Text 
+            className="box-forecast"
+            text="Amanhã"/>
+          <p 
+            className="temperatures" 
+            onClick={ this.toggleTemperature.bind(this, forecast.temperatures)}>
+            { this.state.isCelsius ? `${this.state.tomorrow}ºF ` : `${this.state.tomorrow}ºC ` }
+          </p>
+        </div>
+        <div className="container box-after-tommorrow">
+          <Text 
+            className="box-forecast"
+            text="Depois de amanhã"/>
+          <p 
+            className="temperatures" 
+            onClick={ this.toggleTemperature.bind(this, forecast.temperatures)}>
+            { this.state.isCelsius ? `${this.state.afterTomorrow}ºF ` : `${this.state.afterTomorrow}ºC ` }
+          </p>
         </div>
       </div>
+        </div>
+      </div> : null
     );
   }
 }
-
-export default App;
--- src/models/ForecastViewModel.js
+++ src/models/ForecastViewModel.js
@@ -11,16 +11,28 @@ export default class ForecastViewModel {
     minAfterTomorrow;
     
     constructor(response) {
-
         this.location = response.query.results.channel.location.city;
-        this.temperature = response.query.results.channel.item.condition.temp;
         this.condition = response.query.results.channel.item.condition.text;           
         this.windCurrent = response.query.results.channel.wind.speed;
         this.humidityCurrent = response.query.results.channel.atmosphere.humidity;
         this.pressureCurrent = response.query.results.channel.atmosphere.pressure;
-        this.maxTomorrow = response.query.results.channel.item.forecast[0].high;
-        this.minTomorrow = response.query.results.channel.item.forecast[0].low;
-        this.maxAfterTomorrow = response.query.results.channel.item.forecast[1].high;
-        this.minAfterTomorrow = response.query.results.channel.item.forecast[1].low;
+        this.maxToday = response.query.results.channel.item.forecast[0].high;
+        this.minToday = response.query.results.channel.item.forecast[0].low;
+        this.maxTomorrow = response.query.results.channel.item.forecast[1].high;
+        this.minTomorrow = response.query.results.channel.item.forecast[1].low;
+        this.maxAfterTomorrow = response.query.results.channel.item.forecast[2].high;
+        this.minAfterTomorrow = response.query.results.channel.item.forecast[2].low;
+        this.temperatures = this.getTemperatures(response.query.results.channel.item.forecast);
+    }
+
+    getTemperatures(response) {
+        let temperatures = [];
+        for (var i = 0; i < 3; i++) {
+            console.log(response)
+            temperatures.push(response[i].high)
+            console.log(response)
+        }
+
+        return temperatures;
     }
 }
