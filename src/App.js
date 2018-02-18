39388dc15051fc4e87eb9aa193e4e824e7ba9296
lucasavs <lucasavs@id.uff.br>
2018-02-17 23:20:41 -0200

    Permitindo que a busca seja feita

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import React from 'react';
 //import Api from './api-Yahoo.js' //se quiser mudar a API, pode faze-la aqui.
+import Search from "./components/Search";
+import Weather from "./components/Weather";
 
 class App extends React.Component {
+    /*
     constructor(props){
         console.log("teste");
         super(props);
         this.getWeather();
     }
-    
+    */
+    state = {
+        temperature: undefined,
+        name: undefined
+    }
     //todo: colocar isso a parte no codigo para alterar as apis mais facilmente
-    //getWeather = async (e) => {
-    async getWeather() {
-        //e.preventDefault();
-        const api_call = await fetch(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{rio%20de%20janeiro}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`);
+    getWeather = async (e) => {
+    //async getWeather() {
+        e.preventDefault(); //impedindo de recarregar a tela
+        const city = e.target.elements.city.value;
+        const api_call = await fetch(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{${city}}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`);
         const data = await api_call.json();
         console.log(data);
+        this.setState({
+            name : data.query.results.channel.location.city,
+            temperature: data.query.results.channel.item.condition.temp
+        })
+        console.log(this.state);
     }
     
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+            <Search getWeather={this.getWeather} />
+            <Weather name={this.state.name} temperature = {this.state.temperature} />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
