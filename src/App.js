ff1c6a62527be24ec94c9248253fe155a9f67d69
lucasavs <lucasavs@id.uff.br>
2018-02-17 22:33:01 -0200

    Testando a chama assincrona na API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import Api from './api-Yahoo.js' //se quiser mudar a API, pode faze-la aqui.
 
-class App extends Component {
+class App extends React.Component {
+    constructor(props){
+        console.log("teste");
+        super(props);
+        this.getWeather();
+    }
+    
+    //todo: colocar isso a parte no codigo para alterar as apis mais facilmente
+    //getWeather = async (e) => {
+    async getWeather() {
+        //e.preventDefault();
+        const api_call = await fetch(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{rio%20de%20janeiro}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`);
+        const data = await api_call.json();
+        console.log(data);
+    }
+    
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
