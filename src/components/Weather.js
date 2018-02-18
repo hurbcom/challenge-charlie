bcc75e4478090d19e7800bdadfb542473d8e6d4c
lucasavs <lucasavs@id.uff.br>
2018-02-18 12:57:45 -0300

    Colocando escala dos valores do clima

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,29 +2,43 @@ import React from 'react'
 
 //esse componente vai acabar sendo stateless pelo visto, pode ser reescrito para melhor entendimento
 class Weather extends React.Component{
+    state = {
+        scale : "C",
+    }
     render(){
         return (
             <div>
                 <p>{this.props.day}</p>
             {
                 //https://reactjs.org/docs/conditional-rendering.html#inline-if-with-logical--operator
-                this.props.temperature && <p>Temperatura: {this.props.temperature} </p> 
+                this.props.temperature && <p>Temperatura: {this.showTemperature()} °{this.state.scale} </p> 
             }
             {
                 this.props.weather && <p>Clima: {this.props.weather} </p> 
             }
             {
-                this.props.wind && <p>Vento: {this.props.wind} </p> 
+                this.props.wind && <p>Vento: {this.props.wind} km/h</p> 
             }
             {
-                this.props.humidity && <p>Humidade: {this.props.humidity} </p> 
+                this.props.humidity && <p>Humidade: {this.props.humidity} %</p> 
             }
             {
-                this.props.pression && <p>Pressao: {this.props.pression} </p> 
+                this.props.pression && <p>Pressao: {this.props.pression} mb</p> 
             }
   
             </div>
         );
+    }
+    //mostra a temperatura em celsius oum fahrenheit
+    showTemperature(){
+        if(this.state.scale === "C"){
+            return this.props.temperature;
+        } else {
+            let temperatureImperial = this.props.temperature;
+            temperatureImperial = parseFloat(temperatureImperial);
+            temperatureImperial = (temperatureImperial / 5) * 9 + 32;
+            return temperatureImperial;
+        }
     }
 }
 export default Weather
