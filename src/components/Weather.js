b8121ae884d267ccffed3fe8f72f07faa222e7d0
lucasavs <lucasavs@id.uff.br>
2018-02-19 20:37:10 -0300

    Melhorada na variacao de cores

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -10,26 +10,31 @@ class Weather extends React.Component{
         const divStyle = {backgroundColor : newBackgroundColor}
         return (
             <div className = {this.props.className} style = {divStyle}>
-                <p>{this.props.day}</p>
-            {
-                //https://reactjs.org/docs/conditional-rendering.html#inline-if-with-logical--operator
-                this.props.temperature && 
-                <span onClick={ (event) => {this.changeTemperatureScale(event)}}>
-                    <p>Temperatura: {this.showTemperature()} °{this.state.scale} </p> 
-                </span>
-            }
-            {
-                this.props.weather && <p>Clima: {this.props.weather} </p> 
-            }
-            {
-                this.props.wind && <p>Vento: {this.props.wind} km/h</p> 
-            }
-            {
-                this.props.humidity && <p>Humidade: {this.props.humidity} %</p> 
-            }
-            {
-                this.props.pression && <p>Pressao: {this.props.pression} mb</p> 
-            }
+                <div className = "weather-icon">
+                </div>
+                <div className = "weather-info">
+                    <p>{this.props.day}</p>
+                    {
+                        //https://reactjs.org/docs/conditional-rendering.html#inline-if-with-logical--operator
+                        this.props.temperature && 
+                        <span onClick={ (event) => {this.changeTemperatureScale(event)}}>
+                            <p>Temperatura: {this.showTemperature()} °{this.state.scale} </p> 
+                        </span>
+                    }
+                    {
+                        this.props.weather && <p>Clima: {this.props.weather} </p> 
+                    }
+                    {
+                        this.props.wind && <p>Vento: {this.props.wind} km/h</p> 
+                    }
+                    {
+                        this.props.humidity && <p>Humidade: {this.props.humidity} %</p> 
+                    }
+                    {
+                        this.props.pression && <p>Pressao: {this.props.pression} mb</p> 
+                    }
+                </div>
+                
   
             </div>
         );
@@ -59,14 +64,27 @@ class Weather extends React.Component{
     }
     */
     changeBackgroundColor(){
-        if(this.props.temperature === "undefined"){ //cinza
-            return "gray";
-        } else if(this.props.temperature < 15){ //tons de azul
-            return "blue";
-        } else if(this.props.temperature > 35){ //tons de vermelho
-            return "red";
+        const visibility = 0.5;
+        const temperature = this.props.temperature;
+        const coldColorValue = 15;
+        const hotColorValue = 35;
+        const initialColorValue = 64;
+        
+        let colorValue = initialColorValue;
+        
+        
+        if(temperature === "undefined"){ //cinza
+            return `rgba(0,0,0,${visibility})`;
+        } else if(temperature < coldColorValue){ //tons de azul
+            if(temperature > 0){ //so tera degrade se a temperatura for maior que zero
+                colorValue = initialColorValue + (initialColorValue * temperature)/coldColorValue;
+            }
+            return `rgba(0,0,${colorValue},${visibility})`;
+        } else if(temperature > hotColorValue){ //tons de vermelho
+            return `rgba(255,0,0,${visibility})`;
         } else { //tons de amarelo
-            return "yellow";
+            colorValue = initialColorValue + ( (temperature-coldColorValue) * ( (255 - initialColorValue) / (hotColorValue-coldColorValue) ) );
+            return `rgba(${colorValue},${colorValue},0,${visibility})`;
         }
     }
 }
