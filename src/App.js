a5d698692b9c45884633c214d4ee8ba6e4d99291
Amimaro <amir.zahlan@gmail.com>
2018-05-10 16:16:02 -0300

    Set App as root :art:

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+import LandingPage from './components/LandingPage/LandingPage';
+
+const featuredImage = require('./utils/FeaturedImage');
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            backgroundImage: ''
+        }
+    }
+    componentWillMount() {
+        featuredImage.get().then((res) => {
+            this.setState({backgroundImage: res});
+        }).catch((err) => {
+            console.error(err);
+        });
+    }
+    render() {
+        return (<div className="App" style={{
+                backgroundImage: `url(${this.state.backgroundImage})`
+            }}>
+            <LandingPage/>
+        </div>);
+    }
 }
 
 export default App;
