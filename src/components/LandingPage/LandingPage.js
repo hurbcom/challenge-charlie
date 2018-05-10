5a00f9ff0a00156c71aaf69e6a0f48b1b4f72956
Amimaro <amir.zahlan@gmail.com>
2018-05-10 16:15:15 -0300

    Create LandingPage :art:

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './LandingPage.css';
+
+class LandingPage extends Component {
+    render() {
+        return (<div className="LandingPage">
+            <h1>LandingPage</h1>
+        </div>);
+    }
+}
+
+export default LandingPage;
