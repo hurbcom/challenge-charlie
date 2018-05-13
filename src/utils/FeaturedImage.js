b1dd2aee6143a2f9987e4d3cd1ada94cde60d8b6
amimaro <amir.zahlan@gmail.com>
2018-05-13 12:29:45 -0300

    Add CORS proxy :art:

--- src/utils/FeaturedImage.js
+++ src/utils/FeaturedImage.js
@@ -1,9 +1,10 @@
 export async function get() {
     const axios = require('axios');
+    const CORS_PROXY = 'https://cors.io/?';
     const BING_URL = 'https://www.bing.com';
     const BING_API = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR';
     try {
-        const params = await axios.get(BING_API);
+        const params = await axios.get(CORS_PROXY + BING_API);
         return BING_URL + params.data.images[0].url;
     } catch (err) {
         throw err;
