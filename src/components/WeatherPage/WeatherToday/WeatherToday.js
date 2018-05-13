b00b7cd53d25a5e530f815878875d618c1173150
amimaro <amir.zahlan@gmail.com>
2018-05-13 20:52:30 -0300

    Create WeatherToday :art:

--- src/components/WeatherPage/WeatherToday/WeatherToday.js
+++ src/components/WeatherPage/WeatherToday/WeatherToday.js
@@ -4,12 +4,18 @@ import './WeatherToday.css';
 class WeatherToday extends Component {
     render() {
         return (<div className="WeatherToday">
-            <div className="table">
-                <div className="row">
-                    <div className="cell">
-                        <span data-icon="B"></span>
-                    </div>
-                    <div className="cell"></div>
+            <div className="row">
+                <div className="column" id="column-1">
+                    <span data-icon="B" id="weather-icon"></span>
+                </div>
+                <div className="column" id="column-2">
+                    <h2>HOJE</h2>
+                    <h2>32°C</h2>
+                    <br/>
+                    <h1>Ensolarado</h1>
+                    <h3>Vento: NO 6.4km/h</h3>
+                    <h3>Humidade: 78%</h3>
+                    <h3>Pressão: 1003hPA</h3>
                 </div>
             </div>
         </div>);
