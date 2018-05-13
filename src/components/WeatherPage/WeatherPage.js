032f5180e83ee9d9cddce11542c402ef7a1d99c7
amimaro <amir.zahlan@gmail.com>
2018-05-13 20:58:10 -0300

    Create WeatherTomorrow :art:

--- src/components/WeatherPage/WeatherPage.js
+++ src/components/WeatherPage/WeatherPage.js
@@ -3,12 +3,14 @@ import './WeatherPage.css';
 
 import WeatherLocation from './WeatherLocation/WeatherLocation';
 import WeatherToday from './WeatherToday/WeatherToday';
+import WeatherTomorrow from './WeatherTomorrow/WeatherTomorrow';
 
 class WeatherPage extends Component {
     render() {
         return (<div className="WeatherPage">
             <WeatherLocation location="Rio de Janeiro, Rio de Janeiro"/>
             <WeatherToday/>
+            <WeatherTomorrow/>
         </div>);
     }
 }
---
+++ src/components/WeatherPage/WeatherTomorrow/WeatherTomorrow.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './WeatherTomorrow.css';
+
+class WeatherTomorrow extends Component {
+    render() {
+        return (<div className="WeatherTomorrow">
+            <div className="row">
+                <div className="column" id="column-1"></div>
+                <div className="column" id="column-2">
+                    <h2>AMANHÃ</h2>
+                    <h2>32°C</h2>
+                </div>
+            </div>
+        </div>);
+    }
+}
+
+export default WeatherTomorrow;
