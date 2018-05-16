c8c5f1d40bbc84a1442a94a8b5309b4492a20e64
amimaro <amir.zahlan@gmail.com>
2018-05-15 21:55:13 -0300

    Add responsiveness to Weather today/location :art:

--- src/components/WeatherPage/WeatherToday/WeatherToday.js
+++ src/components/WeatherPage/WeatherToday/WeatherToday.js
@@ -4,13 +4,16 @@ import './WeatherToday.css';
 class WeatherToday extends Component {
     render() {
         return (<div className="WeatherToday">
-            <h2>HOJE</h2>
-            <h2>32°C</h2>
-            <br/>
-            <h1>Ensolarado</h1>
-            <h3>Vento: NO 6.4km/h</h3>
-            <h3>Humidade: 78%</h3>
-            <h3>Pressão: 1003hPA</h3>
+            <div className="TodayTemperature">
+                <h2>HOJE</h2>
+                <h2>32°C</h2>
+            </div>
+            <div className="TodayStatus">
+                <h1>Ensolarado</h1>
+                <h3>Vento: NO 6.4km/h</h3>
+                <h3>Humidade: 78%</h3>
+                <h3>Pressão: 1003hPA</h3>
+            </div>
         </div>);
     }
 }
