3fcf73c8b5d61f5817cfd42b54f21723cf42ade1
Patrick Narciso <patrick.ncsilva@gmail.com>
2018-08-18 19:37:12 -0300

    Add and import Vue-Resource dependency

--- hotel-urbano-challenge/src/main.js
+++ hotel-urbano-challenge/src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueResource from "vue-resource";
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
     store,
+    VueResource,
     render: h => h(App)
 }).$mount("#app");
