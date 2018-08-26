3103b2abdcf853e25e82477edf5f7841393b27f6
Leandro Cunha <leandroscunha@gmail.com>
2018-08-26 12:39:36 -0300

    Attempt to fetch Bing API

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Wrapper from './Wrapper';
 
-const App = () => <h1>Hello, world!</h1>;
+const App = () => (
+    <Wrapper>
+        <h1>Hello, world!</h1>
+    </Wrapper>
+);
 
 export default App;
---
+++ src/components/Wrapper.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Wrapper = props => (
+    <div className="Wrapper">{props.children}</div>
+);
+
+export default Wrapper;
