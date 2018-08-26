d3626949b471df76b107b8819b36537b2cc50567
Pedro <pedrobslisboa@gmail.com>
2018-08-26 20:25:55 -0300

    Reformulando App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { bindActionCreators } from 'redux'
-import {getWeather} from './js/actions/location.action'
-import { connect } from 'react-redux'
-
 
 class App extends Component {
-
-  componentDidMount(){
-    this.props.getImage()
-  }
-
   render() {
     return (
       <div className="App" style={{backgroundImage:'url(https://source.unsplash.com/daily)',backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-        
       </div>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch)=>{
-return{
-  getImage:bindActionCreators(getWeather,dispatch)
-}
-}
-
-export default connect(null,mapDispatchToProps)(App)
+export default App;
