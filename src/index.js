604e0cc087cd03b6f50a7e2438b9c8034520bfb0
Leandro Cunha <leandroscunha@gmail.com>
2018-08-26 11:50:31 -0300

    Adding React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hello, world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
