dfbad75da9585426f409d154a51924f8d5274fde
Pedro <pedrobslisboa@gmail.com>
2018-08-28 09:41:03 -0300

    Estilizando e recebendo dados do Weather Yahoo

--- src/js/initialState.js
+++ src/js/initialState.js
@@ -1,12 +1,39 @@
 const initialState = {
-    errorLocation:false,
-    searching:false,
-    weather:{
-        city:'',
-    condition:{},
-    forecast:{}
+    mainDeg:true,
+    findLocation:false,
+    errorLocation: false,
+    searching: false,
+    weather: {
+        city: '',
+        condition: {},
+        forecast: {
+           amanha:{
+            code: "",
+            date: "",
+            day:
+                "",
+            high:
+                "",
+            low:
+                "",
+            text:
+                "",
+           },
+           depoisAmanha:{
+            code: "",
+            date: "",
+            day:
+                "",
+            high:
+                "",
+            low:
+                "",
+            text:
+                "",
+           }
+        }
     }
-    
+
 }
 
 export default initialState
