09f04bb2691adebe1215a7dfcaaef98f05f9f3f6
Pedro <pedrobslisboa@gmail.com>
2018-08-29 10:35:01 -0300

    Correcao de erros de busca

--- src/js/actions/searching.js
+++ src/js/actions/searching.js
@@ -19,9 +19,12 @@ export const search = (local) => {
             type: types.SEARCHING,
         })
         axios.get(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${local}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`)
-        .then(res => res.data)
+        .then(res => {
+            console.log(res.data.query);
+            
+            return res.data})
         .then(data => 
-            (data.query.results == null || !data.query.results.astronomy)
+            (data.query.results == null || !data.query.results.channel.astronomy)
             ? dispatch({
                 type: types.NO_LOCATION
             })
