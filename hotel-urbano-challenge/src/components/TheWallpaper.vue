90fe769ded83442a38effcedd4a364eb59a28a5f
Patrick Narciso <patrick.ncsilva@gmail.com>
2018-09-02 10:00:44 -0300

    Removing the wallpaper component's geolocation api and including the variables for breakpoints

--- hotel-urbano-challenge/src/components/TheWallpaper.vue
+++ hotel-urbano-challenge/src/components/TheWallpaper.vue
@@ -1,15 +1,6 @@
+<!--suppress ALL -->
 <template>
-    <div>
-        <md-progress-bar md-mode="indeterminate"></md-progress-bar>
 
-        <!-- modal de erro caso não consiga obter localização do usuaário -->
-        <md-dialog-alert
-                :md-active.sync="errorLocation"
-                md-title="Não foi possível obter sua localizacão!"
-                md-content="Your post <strong>Material Design is awesome</strong> has been created." />
-
-
-    </div>
 </template>
 
 <script>
@@ -18,8 +9,6 @@ export default {
     name: "HelloWorld",
     data() {
         return {
-            lat: "",
-            long: "",
             background: "",
             loading: false,
             errorLocation: false,
@@ -27,7 +16,6 @@ export default {
         };
     },
     beforeMount() {
-        this.getGeolocation();
         this.getWallpaper();
     },
     methods: {
@@ -37,9 +25,14 @@ export default {
                 .get(
                     `${BASE_URL}https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR`
                 )
-                .then(res => {
-                        this.background = `http://www.bing.com${res.body.images[0].url}`;
-                        document.body.style.backgroundImage = `url(${this.background})`;
+                .then(
+                    res => {
+                        this.background = `http://www.bing.com${
+                            res.body.images[0].url
+                        }`;
+                        document.getElementById(
+                            "app"
+                        ).style.backgroundImage = `url(${this.background})`;
                         this.loading = false;
                     },
                     () => {
@@ -47,32 +40,12 @@ export default {
                     }
                 );
         },
-        getGeolocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(
-                    position => {
-                        this.lat = position.coords.latitude;
-                        this.long = position.coords.longitude;
-                        this.errorLocation = false;
-                    },
-                    error => {
-                        if (error.code === error.PERMISSION_DENIED)
-                            this.errorLocation = true;
-                    }
-                );
-            } else {
-                this.errorLocation = true;
-            }
-        }
     }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.md-progress-bar {
-    margin: 24px;
-}
 .md-dialog {
     background: #ffffff;
 }
