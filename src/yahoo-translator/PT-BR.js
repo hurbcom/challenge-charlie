19b0d52277f650938233d86398d052726eee59e7
Felippe <felippe.mauricio@olxbr.com>
2018-10-20 01:12:34 -0300

    Add Yahoo Translator

---
+++ src/yahoo-translator/PT-BR.js
@@ -0,0 +1,200 @@
+export default {
+  codes: {
+    0: {
+      text: 'tornado',
+      image: 13,
+    },
+    1: {
+      text: 'tempestade tropical',
+      image: 1,
+    },
+    2: {
+      text: 'furacão',
+      image: 13,
+    },
+    3: {
+      text: 'tempestade severa',
+      image: 1,
+    },
+    4: {
+      text: 'trovoadas',
+      image: 1,
+    },
+    5: {
+      text: 'chuva e neve',
+      image: 1,
+    },
+    6: {
+      text: 'chuva e granizo Fino',
+      image: 1,
+    },
+    7: {
+      text: 'neve e granizo Fino',
+      image: 1,
+    },
+    8: {
+      text: 'garoa gélida',
+      image: 1,
+    },
+    9: {
+      text: 'garoa',
+      image: 1,
+    },
+    10: {
+      text: 'chuva gélida',
+      image: 1,
+    },
+    11: {
+      text: 'chuvisco',
+      image: 1,
+    },
+    12:  {
+      text: 'chuva',
+      image: 1,
+    },
+    13: {
+      text: 'neve em flocos finos',
+      image: 1,
+    },
+    14: {
+      text: 'leve precipitação de neve',
+      image: 1,
+    },
+    15: {
+      text: 'ventos com neve',
+      image: 1,
+    },
+    16: {
+      text: 'neve',
+      image: 1,
+    },
+    17: {
+      text: 'chuva de granizo',
+      image: 1,
+    },
+    18: {
+      text: 'pouco granizo',
+      image: 1,
+    },
+    19: {
+      text: 'pó em suspensão',
+      image: 1,
+    },
+    20: {
+      text: 'neblina',
+      image: 1,
+    },
+    21: {
+      text: 'névoa seca',
+      image: 1,
+    },
+    22: {
+      text: 'enfumaçado',
+      image: 1,
+    },
+    23: {
+      text: 'vendaval',
+      image: 1,
+    },
+    24: {
+      text: 'ventando',
+      image: 1,
+    },
+    25: {
+      text: 'frio',
+      image: 1,
+    },
+    26: {
+      text: 'nublado',
+      image: 1,
+    },
+    27: {
+      text: 'muitas nuvens (noite)',
+      image: 9,
+    },
+    28: {
+      text: 'muitas nuvens (dia)',
+      image: 8,
+    },
+    29: {
+      text: 'parcialmente nublado (noite)',
+      image: 1,
+    },
+    30: {
+      text: 'parcialmente nublado (dia)',
+      image: 1,
+    },
+    31: {
+      text: 'céu limpo (noite)',
+      image: 1,
+    },
+    32: {
+      text: 'ensolarado',
+      image: 1,
+    },
+    33: {
+      text: 'tempo bom (noite)',
+      image: 1,
+    },
+    34: {
+      text: 'tempo bom (dia)',
+      image: 1,
+    },
+    35: {
+      text: 'chuva e granizo',
+      image: 1,
+    },
+    36: {
+      text: 'quente',
+      image: 1,
+    },
+    37: {
+      text: 'tempestades isoladas',
+      image: 1,
+    },
+    38: {
+      text: 'tempestades esparsas',
+      image: 1,
+    },
+    39: {
+      text: 'tempestades esparsas',
+      image: 1,
+    },
+    40: {
+      text: 'chuvas esparsas',
+      image: 1,
+    },
+    41: {
+      text: 'nevasca',
+      image: 1,
+    },
+    42: {
+      text: 'tempestades de neve esparsas',
+      image: 1,
+    },
+    43: {
+      text: 'nevasca',
+      image: 1,
+    },
+    44: {
+      text: 'parcialmente nublado',
+      image: 1,
+    },
+    45: {
+      text: 'chuva com trovoadas',
+      image: 1,
+    },
+    46: {
+      text: 'tempestade de neve',
+      image: 1,
+    },
+    47: {
+      text: 'relâmpagos e chuvas isoladas',
+      image: 1,
+    },
+    3200: {
+      text: 'não disponível',
+      image: 45,
+    },
+  },
+};
