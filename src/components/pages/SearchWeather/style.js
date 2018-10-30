9f5925ac9b76bc132bbd4afe854d98b7c1a1d24a
Felippe <felippe.mauricio@olxbr.com>
2018-10-29 23:36:19 -0300

    Move to top the WeatherBox in cellphone style

--- src/components/pages/SearchWeather/style.js
+++ src/components/pages/SearchWeather/style.js
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 
 const Wrapper = styled.div`
-  align-items: center;
+  align-items: flex-start;
   display: flex;
   height: 100%;
   justify-content: flex-start;
   width: 100%;
 
   @media(min-width: ${({ theme }) => theme.breakpoints.small}) {
+    align-items: center;
     justify-content: center;
   }
 `;
