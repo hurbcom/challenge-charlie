f74cb919e6939c42b95a3ca3c075f80f7db4968f
Bruno Guedão <brunoguedao@edumobi.com.br>
2019-02-22 22:59:54 -0300

    prevent warning

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 // get Bing background image
-$bg = file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR');
+$bg = @file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR');
 $json_bg = json_decode($bg);
 $result = array('image' => 'https://www.bing.com'.$json_bg->images[0]->url);
 echo json_encode($result);
