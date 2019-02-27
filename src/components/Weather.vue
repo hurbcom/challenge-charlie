0be5def117b92cc347b69ce1121685bb1f6c486c
vinnycl <carneiropresent@gmail.com>
2019-02-27 01:50:24 -0300

    Adicionado status e conversao de valores do Json

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,9 +1,29 @@
 <template>
     <div id="content">
-        {{weather.condition}}
-        <span class="icon-font">
-            {{weather.condition | iconWeather}}
-        </span>
+        <h1><span class="icon-font">( </span>{{weather.city}}, {{weather.region}}</h1>
+
+
+
+        <div>
+            <span class="icon-font">
+                {{weather.condition | iconWeather}}
+            </span>
+
+            {{weather.todayC}}
+            {{weather.condition | translate}}
+            Vento:{{weather.windDirection}} {{weather.windSpeed}}
+            Humidade:{{weather.humidity}}
+            Pressão: {{weather.pressure}}
+        </div>
+        <div>
+            Amanhã
+            {{weather.tomorrowC}}
+        </div>
+
+        <div>
+            Depois de amanhã
+            {{weather.afterC}}
+        </div>
     </div>
 </template>
 <script>
@@ -19,9 +39,9 @@ export default {
             let element = document.getElementById("content");
             if (weather.today) {
                 let temp = parseInt(weather.today);
-                if (weather.unit === "C" && temp < 15)
+                if (weather.today === "C" && temp < 15)
                     element.classList.add("card__content--background--blue");
-                else if (weather.unit === "C" && temp > 35)
+                else if (weather.today === "C" && temp > 35)
                     element.classList.add("card__content--background--red");
                 else element.classList.add("card__content--background--yellow");
             } else if (!weather.today) {
@@ -37,7 +57,5 @@ export default {
     font-family: 'MeteoconsRegular';
     font-size: 80px;
 }
-.icon-font:before {
-    content: attr(H);
-}
+
 </style>
--- src/store.js
+++ src/store.js
@@ -4,36 +4,79 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 
+function setWindDirection(val) {
+    if (val >= 0 && val <= 11.25) { return 'N'; }
+    if (val > 11.25 && val <= 33.75) { return 'NNE'; }
+    if (val > 33.75 && val <= 56.25) { return 'NE'; }
+    if (val > 56.25 && val <= 78.75) { return 'LNE'; }
+    if (val > 78.75 && val <= 101.25) { return 'L'; }
+    if (val > 101.25 && val <= 123.75) { return 'LSE'; }
+    if (val > 123.75 && val <= 146.25) { return 'SE'; }
+    if (val > 146.25 && val <= 168.75) { return 'SSE'; }
+    if (val > 168.75 && val <= 191.25) { return 'S'; }
+    if (val > 191.25 && val <= 213.75) { return 'SSO'; }
+    if (val > 213.75 && val <= 236.25) { return 'SO'; }
+    if (val > 236.25 && val <= 258.75) { return 'OSE'; }
+    if (val > 258.75 && val <= 281.25) { return 'O'; }
+    if (val > 281.25 && val <= 303.75) { return 'ONO'; }
+    if (val > 303.75 && val <= 326.25) { return 'NO'; }
+    if (val > 326.25 && val <= 348.75) { return 'NNO'; }
+}
+
+function setToCelsius(val) {
+    var temperature = (val - 32) * 5 / 9;
+    return Math.round(temperature);
+}
+
+function getMedTemperature(low, high) {
+    return ((low + high)) / 2;
+}
+
+function setPressure(val) {
+    return Math.round(val * 33.863886666667);
+}
+
+function setWindSpeed(val) {
+    return Math.round(val * 3.6);
+}
 
 
 export default new Vuex.Store({
     state: {
         weather: {
-            location: "",
+            city: "",
+            region: "",
             condition: "",
+            country: "",
             today: "",
+            todayC: "",
             tomorrow: "",
             after: "",
-            wind: "",
+            tomorrowC: "",
+            afterC: "",
+            windSpeed: "",
+            windDirection: "",
             humidity: "",
-            pressure: "",
-            unit: ""
+            pressure: ""
         }
     },
     mutations: {
         SET_WEATHER(state, payload) {
             let weather = payload;
-            state.weather.location = `${weather.location.city},${
-                weather.location.region
-            }`;
+            state.weather.country = weather.location.country;
+            state.weather.city = weather.location.city;
+            state.weather.region = weather.location.region;
             state.weather.condition = weather.current_observation.condition.text;
-            state.weather.today = weather.current_observation.temp;
-            state.weather.tomorrow = weather.forecasts[1].high;
-            state.weather.after = weather.forecasts[2].high;
-            state.weather.wind = `${weather.current_observation.wind.speed}`;
+            state.weather.today = `${weather.current_observation.condition.temperature}℉`;
+            state.weather.tomorrow = `${getMedTemperature(weather.forecasts[1].high , weather.forecasts[1].low)}℉`;
+            state.weather.after = `${getMedTemperature(weather.forecasts[2].high , weather.forecasts[2].low)}℉`;
+            state.weather.todayC = `${setToCelsius(weather.current_observation.condition.temperature)}℃`;
+            state.weather.tomorrowC = `${setToCelsius(getMedTemperature(weather.forecasts[1].high , weather.forecasts[1].low))}℃`;
+            state.weather.afterC = `${setToCelsius(getMedTemperature(weather.forecasts[2].high , weather.forecasts[2].low))}℃`;
+            state.weather.windSpeed = `${setWindSpeed(weather.current_observation.wind.speed)}km/h`;
+            state.weather.windDirection = `${setWindDirection(weather.current_observation.wind.direction)}`;
             state.weather.humidity = `${weather.current_observation.atmosphere.humidity}%`;
-            state.weather.unit = weather.current_observation.condition.temperature;
-            state.weather.pressure = `${weather.current_observation.atmosphere.pressure}`;
+            state.weather.pressure = `${setPressure(weather.current_observation.atmosphere.pressure)}hPA`;
         }
     }
 });
