5aa2398fff37b91fc47eaa3debe8b87b5dd0611c
vinnycl <carneiropresent@gmail.com>
2019-02-27 19:51:40 -0300

    Adaptacao do layout para dispositivos moveis

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,28 +1,39 @@
 <template>
     <main class="content">
-        <h1 class="colors"><span class="icon-font">( </span>{{weather.city}}, {{weather.region}}</h1>
-        <div class="colors todayC">
+
+        <h1 class="colors"><span class="icon-font">( </span> {{weather.city}}, {{weather.region}}</h1>
+
+        <div class="box-condition colors todayC">
+
             <span class="icon-font">
                 {{weather.condition | iconWeather}}
             </span>
 
-            <span>{{weather.todayC}}<span class="icon-font">*</span></span><span>{{weather.today}}<span class="icon-font">+</span></span>
-            {{weather.condition | translate}}
-            Vento:{{weather.windDirection}} {{weather.windSpeed}} km/h
-            Humidade:{{weather.humidity}} %
-            Pressão: {{weather.pressure}} hPA
-        </div>
-        <div class="colors tomorrowC">
-            Amanhã
-            <span>{{weather.tomorrowC}}<span class="icon-font">*</span></span>
-            <span>{{weather.tomorrow}}<span class="icon-font">+</span></span>
+            <span class="data">Hoje</span>
+            <span class="celsius">{{weather.todayC}}˚C</span>
+            <span class="fahrenheit">{{weather.today}}˚F</span>
+            <span class="weather">{{weather.condition | translate}}</span>
+
+
+            <ul class="wind-list">
+                <li>Vento: {{weather.windDirection}} {{weather.windSpeed}} km/h</li>
+                <li>Humidade:{{weather.humidity}} %</li>
+                <li>Pressão: {{weather.pressure}} hPA</li>
+            </ul>
         </div>
 
-        <div class="colors afterC">
-            Depois de amanhã
-            <span>{{weather.afterC}}<span class="icon-font">*</span></span>
-            <span>{{weather.after}}<span class="icon-font">+</span></span>
+        <div class="box-condition colors tomorrowC">
+            <span class="data">Amanhã</span>
+            <span class="celsius">{{weather.tomorrowC}}˚C</span>
+            <span class="fahrenheit">{{weather.tomorrow}}˚F</span>
         </div>
+
+        <div class="box-condition colors afterC">
+            <span class="data">Depois de amanhã</span>
+            <span class="celsius">{{weather.afterC}}˚C</span>
+            <span class="fahrenheit">{{weather.after}}˚F</span>
+        </div>
+
     </main>
 </template>
 <script>
@@ -56,6 +67,81 @@ export default {
 </script>
 
 <style >
+.content {
+    max-width: 32em;
+    margin: 0 auto;
+}
+.content h1 {
+    margin: 0;
+    padding: 0.9em;
+    font-size: 1.5em;
+    line-height: 1.5em;
+    text-align: center;
+    color: #8d8a87;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+}
+
+.content h1 .icon-font {
+    font-size: 2em;
+    margin-right: 0.3em;
+}
+
+.box-condition {
+    color: #fff;
+    text-shadow: 1px 1px 2px #00000085;
+    padding-left: 55%;
+    position: relative;
+    padding-bottom: 2em;
+}
+
+.box-condition > .icon-font {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 50%;
+    height: 100%;
+    text-align: center;
+    font-size: 900%;
+    padding-top: 0.2em;
+}
+
+.box-condition .data {
+    display: block;
+    text-transform: uppercase;
+    font-size: 1.2em;
+    padding-top: 1em;
+}
+
+.box-condition .celsius, .box-condition .fahrenheit {
+    font-size: 1.5em;
+}
+
+.content.fahrenheit .box-condition .celsius, .box-condition .fahrenheit {
+    display: none;
+}
+
+.content.fahrenheit .box-condition .fahrenheit {
+    display: block;
+}
+
+.box-condition .weather {
+    display: block;
+    font-size: 1.2em;
+    padding-top: 1em;
+}
+
+.box-condition .wind-list {
+    list-style: none;
+    padding: 1em 0 0 0;
+    margin: 0;
+}
+
+.box-condition .wind-list li {
+    padding-top: 0.1em;
+}
+
 .icon-font {
     font-family: 'MeteoconsRegular';
 }
@@ -81,11 +167,11 @@ export default {
 }
 
 .tomorrowC.background-red {
-    background-color: #d01e1ecf;
+    background-color: #d41e1ecf;
 }
 
 .afterC.background-red {
-    background-color: #d01e1ee8;
+    background-color: #ea0909e8;
 }
 
 
