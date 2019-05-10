60ad9a3be9e2923dd683c3fb608b9b905e20f36c
Lucas Alves de Sousa <lucas_comp@outlook.com>
2019-05-10 17:32:55 -0300

    Update spinner service

--- challenge-charlie-front/src/app/spinner/spinner.service.ts
+++ challenge-charlie-front/src/app/spinner/spinner.service.ts
@@ -3,8 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SpinnerService {
 
-  isSpinning: boolean;
+  _isSpinning: boolean;
 
   constructor() { }
 
+  startSpin() {
+    this._isSpinning = true;
+  }
+
+  stopSpin() {
+    this._isSpinning = false;
+  }
+
+  get isSpinning() {
+    return this._isSpinning;
+  }
 }
