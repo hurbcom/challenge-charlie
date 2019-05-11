baa1aceb280036abe678e35e8922deee5f00521d
lucascomp <lucas_comp@outlook.com>
2019-05-11 14:18:54 -0300

    Split weather component

--- challenge-charlie-front/src/app/app.component.ts
+++ challenge-charlie-front/src/app/app.component.ts
@@ -1,30 +1,83 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { SpinnerService } from './spinner/spinner.service';
+import { Observable } from 'rxjs';
+
 import { SearchComponent } from './search/search.component';
+import { SpinnerService } from './spinner/spinner.service';
+import { Weather } from './weather/weather';
 import { WeatherService } from './weather/weather.service';
-import { WeatherComponent } from './weather/weather.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   @ViewChild(SearchComponent) searchComponent: SearchComponent;
-  @ViewChild(WeatherComponent) weatherComponent: WeatherComponent;
+  weathers: Weather[];
+  openedWeather: Weather;
 
   constructor(
     public spinnerService: SpinnerService,
     private weatherService: WeatherService
   ) {}
 
-  getWeathersWithLocation(location: string) {
-    this.weatherComponent.getWeathers(this.weatherService.getWeathersWithLocation(location));
+  ngOnInit() {
+    if (!navigator.geolocation) {
+      this.setEmptyWeathers();
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      position => this.getWeathersWithCoordinate(position),
+      () => this.setEmptyWeathers()
+    );
   }
 
-  updateLocation(location: string) {
+  getWeathersWithLocation(location: string) {
+    this.getWeathers(this.weatherService.getWeathersWithLocation(location));
+  }
+
+  openWeather(weather: Weather) {
+    this.openedWeather = weather;
+  }
+
+  changeTemperatureUnit() {
+    this.weathers.forEach(weather => {
+      weather.actualTemperature.changeUnit();
+      weather.maxTemperature.changeUnit();
+      weather.minTemperature.changeUnit();
+    });
+  }
+
+  private setEmptyWeathers() {
+    this.updateLocation('');
+    this.weathers = [];
+  }
+
+  private getWeathersWithCoordinate(position: Position) {
+    this.getWeathers(this.weatherService.getWeathersWithCoordinate(position.coords));
+  }
+
+  private getWeathers(observable: Observable<any>) {
+    this.spinnerService.startSpin();
+    observable.subscribe(response => {
+      this.spinnerService.stopSpin();
+      this.setWeathers(response);
+    }, () => {
+      setTimeout(() => alert("Não foram encontradas previsões para a localidade informada."), 200); //Alert after spinner stops
+      this.setEmptyWeathers();
+    }).add(() => this.spinnerService.stopSpin());
+  }
+
+  private setWeathers(response: any) {
+    this.updateLocation(`${response.location.city},${response.location.region}`);
+    this.weathers = response.weathers;
+    this.openWeather(this.weathers[0]);
+  }
+
+  private updateLocation(location: string) {
     this.searchComponent.updateLocation(location);
   }
 
--- challenge-charlie-front/src/app/weather/weather.component.ts
+++ challenge-charlie-front/src/app/weather/weather.component.ts
@@ -1,79 +1,16 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-import { Observable } from 'rxjs';
-
-import { SpinnerService } from '../spinner/spinner.service';
 import { Weather } from './weather';
-import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.less']
 })
-export class WeatherComponent implements OnInit {
+export class WeatherComponent {
 
-  weathers: Weather[];
-  openedWeather: Weather;
+  @Input() weather: Weather;
+  @Input() isOpened: boolean;
+  @Output() changeTemperatureUnit = new EventEmitter<void>();
 
-  @Output() updateLocation = new EventEmitter<string>();
-
-  constructor(
-    private weatherService: WeatherService,
-    private spinnerService: SpinnerService
-  ) { }
-
-  ngOnInit() {
-    if (!navigator.geolocation) {
-      this.setEmptyWeathers();
-      return;
-    }
-
-    navigator.geolocation.getCurrentPosition(
-      position => this.getWeathersWithCoordinate(position),
-      () => this.setEmptyWeathers()
-    );
-  }
-
-  openWeather(weather: Weather) {
-    this.openedWeather = weather;
-  }
-
-  isOpened(weather: Weather) {
-    return this.openedWeather == weather;
-  }
-
-  changeTemperatureUnit() {
-    this.weathers.forEach(weather => {
-      weather.actualTemperature.changeUnit();
-      weather.maxTemperature.changeUnit();
-      weather.minTemperature.changeUnit();
-    });
-  }
-
-  private setEmptyWeathers() {
-    this.updateLocation.emit('');
-    //TODO: exibir cards cinzas
-  }
-
-  private getWeathersWithCoordinate(position: Position) {
-    this.getWeathers(this.weatherService.getWeathersWithCoordinate(position.coords));
-  }
-
-  private setWeathers(response: any) {
-    this.updateLocation.emit(`${response.location.city},${response.location.region}`);
-    this.weathers = response.weathers;
-    this.openWeather(this.weathers[0]);
-  }
-
-  getWeathers(observable: Observable<any>) {
-    this.spinnerService.startSpin();
-    observable.subscribe(response => {
-      this.spinnerService.stopSpin();
-      this.setWeathers(response);
-    }, () => {
-      setTimeout(() => alert("Não foram encontradas previsões para a localidade informada."), 200); //Alert after spinner stops
-      this.setEmptyWeathers();
-    }).add(() => this.spinnerService.stopSpin());
-  }
 }
