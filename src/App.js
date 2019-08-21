513e00d08f8e3592a3a59ce548b1bb7e5963bdeb
jpcanto <jplopessk8@gmail.com>
2019-08-21 00:00:28 -0300

    adição de temperatura máxima

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class Main extends React.Component {
       icone: '',
       grau: '',
       grau2: '',
+      grau2max: '',
       grau3: '',
+      grau3max: '',
       modo: 'F',
       descricao: '',
     };
@@ -37,7 +39,9 @@ class Main extends React.Component {
       icone: 'B',
       grau: `${data.current_observation.condition.temperature}º`,
       grau2: `${data.forecasts[1].low}º`,
+      grau2max: `${data.forecasts[1].high}º`,
       grau3: `${data.forecasts[2].low}º`,
+      grau3max: `${data.forecasts[2].high}º`,
     });
   };
 
@@ -45,23 +49,31 @@ class Main extends React.Component {
     const data = await WeatherService.getWeather();
     const grauTemp = `${data.current_observation.condition.temperature}`;
     const grauTemp2 = `${data.forecasts[1].low}`;
+    const grauTemp2max = `${data.forecasts[1].high}`;
     const grauTemp3 = `${data.forecasts[2].low}`;
+    const grauTemp3max = `${data.forecasts[2].high}`;
     if (this.state.modo === 'F') {
       const celsius = Math.floor((grauTemp - 32) * (5 / 9)) + 'º';
       const celsius2 = Math.floor((grauTemp2 - 32) * (5 / 9)) + 'º';
+      const celsius2max = Math.floor((grauTemp2max - 32) * (5 / 9)) + 'º';
       const celsius3 = Math.floor((grauTemp3 - 32) * (5 / 9)) + 'º';
+      const celsius3max = Math.floor((grauTemp3max - 32) * (5 / 9)) + 'º';
       this.setState({
         modo: 'C',
         grau: celsius,
         grau2: celsius2,
-        grau3: celsius3
+        grau2max: celsius2max,
+        grau3: celsius3,
+        grau3max: celsius3max
       });
     } else {
       this.setState({
         modo: 'F',
         grau: `${data.current_observation.condition.temperature}º`,
         grau2: `${data.forecasts[1].low}º`,
-        grau3: `${data.forecasts[1].low}º`
+        grau2max: `${data.forecasts[1].high}º`,
+        grau3: `${data.forecasts[1].low}º`,
+        grau3max: `${data.forecasts[1].high}º`
       })
     }
   }
@@ -84,11 +96,13 @@ class Main extends React.Component {
           </div>
           <div className="amanha">
             <p className="dia">AMANHÃ</p>
-            <h2 className="grau" onClick={this.changeDegree}>{this.state.grau2}<span className="modo">{this.state.modo}</span></h2>
+            <h2 className="grau" onClick={this.changeDegree}>Min: {this.state.grau2}<span className="modo">{this.state.modo}</span></h2>
+            <h2 className="grau" onClick={this.changeDegree}>Max: {this.state.grau2max}<span className="modo">{this.state.modo}</span></h2>
           </div>
           <div className="dia-depois-de-amanha">
             <p className="dia">DEPOIS DE AMANHÃ</p>
-            <h2 className="grau" onClick={this.changeDegree}>{this.state.grau3}<span className="modo">{this.state.modo}</span></h2>
+            <h2 className="grau" onClick={this.changeDegree}>Min: {this.state.grau3}<span className="modo">{this.state.modo}</span></h2>
+            <h2 className="grau" onClick={this.changeDegree}>Max: {this.state.grau3max}<span className="modo">{this.state.modo}</span></h2>
           </div>
         </div>
       </section>
