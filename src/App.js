93cfa05ad666eb64f6b4a831a98cca6e6e318d36
jpcanto <jplopessk8@gmail.com>
2019-08-20 23:49:59 -0300

    ajustes layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import * as BackgroundService from './services/background.service';
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.trocaDeGrau = this.trocaDeGrau.bind(this);
+    this.changeDegree = this.changeDegree.bind(this);
     this.state = {
       background: '',
       localizacao: '',
@@ -28,7 +28,6 @@ class Main extends React.Component {
     this.setState({
       background: `https://www.bing.com${data.images[0].url}`
     })
-    console.log(this.state.background);
   };
 
   async getWeather() {
@@ -42,27 +41,34 @@ class Main extends React.Component {
     });
   };
 
-  async trocaDeGrau() {
+  async changeDegree() {
     const data = await WeatherService.getWeather();
-    const grauTemp = `${data.current_observation.condition.temperature}`
+    const grauTemp = `${data.current_observation.condition.temperature}`;
+    const grauTemp2 = `${data.forecasts[1].low}`;
+    const grauTemp3 = `${data.forecasts[2].low}`;
     if (this.state.modo === 'F') {
-      const celsius = (grauTemp - 32) * (5 / 9);
-      const celsiusMath = Math.floor(celsius) + 'º';
+      const celsius = Math.floor((grauTemp - 32) * (5 / 9)) + 'º';
+      const celsius2 = Math.floor((grauTemp2 - 32) * (5 / 9)) + 'º';
+      const celsius3 = Math.floor((grauTemp3 - 32) * (5 / 9)) + 'º';
       this.setState({
         modo: 'C',
-        grau: celsiusMath
+        grau: celsius,
+        grau2: celsius2,
+        grau3: celsius3
       });
     } else {
       this.setState({
         modo: 'F',
-        grau: `${data.current_observation.condition.temperature}º`
+        grau: `${data.current_observation.condition.temperature}º`,
+        grau2: `${data.forecasts[1].low}º`,
+        grau3: `${data.forecasts[1].low}º`
       })
     }
   }
 
   render() {
     return (
-      <section className="main-content" style={{backgroundImage: `url(${this.state.background})`}}>
+      <section className="main-content" style={{ backgroundImage: `url(${this.state.background})` }}>
         <div className="container">
           <div className="localizacao">
             <h1 className="localizacao-titulo" data-icon="(">{this.state.localizacao}</h1>
@@ -71,20 +77,18 @@ class Main extends React.Component {
             <div className="icone" data-icon={this.state.icone}></div>
             <div className="grau-section">
               <p className="dia">HOJE</p>
-              <h2 className="grau" onClick={this.trocaDeGrau}>{this.state.grau}</h2>
+              <h2 className="grau" onClick={this.changeDegree}>{this.state.grau}</h2>
               <span className="modo">{this.state.modo}</span>
               <div className="descricao">{this.state.descricao}</div>
             </div>
           </div>
           <div className="amanha">
             <p className="dia">AMANHÃ</p>
-            <h2 className="grau">{this.state.grau2}</h2>
-            <span className="modo">{this.state.modo}</span>
+            <h2 className="grau" onClick={this.changeDegree}>{this.state.grau2}<span className="modo">{this.state.modo}</span></h2>
           </div>
           <div className="dia-depois-de-amanha">
             <p className="dia">DEPOIS DE AMANHÃ</p>
-            <h2 className="grau">{this.state.grau3}</h2>
-            <span className="modo">{this.state.modo}</span>
+            <h2 className="grau" onClick={this.changeDegree}>{this.state.grau3}<span className="modo">{this.state.modo}</span></h2>
           </div>
         </div>
       </section>
