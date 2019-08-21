df9594728bd3c2601a49afa4d737aa075dd1a7e0
jorge <jorge@go2web.com.br>
2019-08-20 21:37:15 -0300

    add a bit more style to project :)

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
 
   return (
     <div className="App">
-      <Background />
       <Header location={loc}/>
       <Body city={city}/>
     </div>
--- src/components/body/body.js
+++ src/components/body/body.js
@@ -11,20 +11,34 @@ const Body = (city, ...props) => {
 
   return (
   
-  <div className={style.container}>
-    <div className={style.firstHalf}>
-        <p data-icon="B" />
+  <div className={style.layout}>
+    <div className={style.container}> 
+        <div className={style.firstHalf}>
+            <p data-icon="B" />
+        </div>
+        <div className={style.secondHalf}>
+            <h3 className={style.heading}>Hoje</h3>
+            <p className={style.text}> 32ºC</p>
+            <h3 className={style.heading}>Ensolarado</h3>
+            <p className={style.text}>Vento: NO 6.4km/h</p>
+            <p className={style.text}>Humidade: 78%</p>
+            <p className={style.text}>Pressão: 1003hPA</p>
+        </div>
     </div>
-    <div className={style.secondHalf}>
-        <h3 className={style.heading}>Hoje</h3>
-        <p className={style.text}> 32ºC</p>
-        <h3 className={style.heading}>Ensolarado</h3>
-        <p className={style.text}>Vento: NO 6.4km/h</p>
-        <p className={style.text}>Humidade: 78%</p>
-        <p className={style.text}>Pressão: 1003hPA</p>
+    <div className={style.secondContainer}>
+        <div className={style.firstHalf} />
+        <div className={style.secondHalf}>
+            <h3 className={style.heading}>Amanhã</h3>
+            <p className={style.text}> 25ºC</p>
+        </div>
     </div>
-    
-
+    <div className={style.thirdContainer}>
+        <div className={style.firstHalf} />
+        <div className={style.secondHalf}>
+            <h3 className={style.heading}>Depois de amanhã</h3>
+            <p className={style.text}>  22ºC</p>
+        </div>
+    </div>
   </div>)
 };
 
