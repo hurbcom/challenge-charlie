3f30163dcea20dc38244edca32c06fcfcb7f9c77
vinas <desenv@desenvs-MacBook-Pro.local>
2019-08-23 15:56:24 -0300

    refactor

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,25 @@ const https = require('https');
 
 
 router.get('/', function(req, res, next) {
-  
+
+  getBackgroundImage(
+    (image) => { renderMe(res, 'https://www.bing.com/'+image) },
+    () => { renderMe(res, '/images/default-wallpaper.jpg') } 
+  );
+
+});
+
+function renderMe(res, bgImage) {
+  res.render(
+    'index',
+    {
+      title: 'Challange Charlie',
+      bgImage: bgImage
+    }
+  );
+}
+
+function getBackgroundImage(success, error) {
   https.get('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR', (resp) => {
     let data = '';
     resp.on('data', (chunk) => {
@@ -13,26 +31,11 @@ router.get('/', function(req, res, next) {
     });
     resp.on('end', () => {
       data = JSON.parse(data);
-      console.log(data);
-      res.render(
-        'index',
-        {
-          title: 'Challange Charlie',
-          bgImage: 'https://www.bing.com/'+data.images[0].url
-        }
-      );
+      success(data.images[0].url);
     });
-  }).on("error", (err) => {
-    res.render(
-      'index',
-      {
-        title: 'Challange Charlie',
-        bgImage: '/images/default-wallpaper.jpg'
-      }
-    );
+  }).on('error', () => {
+    error();
   });
-
-});
-
+}
 
 module.exports = router;
