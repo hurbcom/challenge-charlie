74913b3d8590a1c68aae6852d324f96aff578075
eron c <deandradeeron@gmail.com>
2019-09-16 11:50:51 -0300

    Final commit - Redux + filtered items + bug fixes

--- src/store/actions/fetchWeather.js
+++ src/store/actions/fetchWeather.js
@@ -4,10 +4,18 @@ import axios from 'axios';
 
 const openWeatherKey = '63f579b5964ac8e740baf76c46b58699';
 
+    // 1 - Função para escolher a previsão dos cartões amanhã e depois de amanhã optando pelos horários sempre de meio dia.:
+
 export const fetchWeather = (tim) => {
+    const digitoDoDiaDeHoje = tim.data.list[0].dt_txt.charAt(9);
+    const arraySemHoje = tim.data.list.filter(item => item.dt_txt.charAt(9) !== digitoDoDiaDeHoje)
+    const arrayDeObjetosDePrevisaoNaHoraMeioDia = arraySemHoje.filter(filtrado => filtrado.dt_txt.charAt(12) === '2')
+    const objeto0 = tim.data.list[0]
+    const objeto1 = arrayDeObjetosDePrevisaoNaHoraMeioDia[0]
+    const objeto2 = arrayDeObjetosDePrevisaoNaHoraMeioDia[1]
     return {
         type: actionTypes.FETCH_WEATHER,
-        tim: tim
+        tim: {objeto0, objeto1, objeto2}
     }
 }
 
@@ -31,3 +39,4 @@ export const getPlace = (place) => {
         }
     }
 }
+
