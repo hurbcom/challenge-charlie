ff214a2494631c8f954fbf9a01f040abdf482623
Tiago Rocha <tiagofarocha95@gmail.com>
2019-12-11 11:42:19 -0300

    removing cors

--- app/src/services/ImageService.js
+++ app/src/services/ImageService.js
@@ -4,7 +4,7 @@ export default class ImageService {
 
     async getImage() {
         try {
-            const response = await axios.get(`https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR`, {mode: 'cors'})
+            const response = await axios.get(`https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR`)
             return Promise.resolve(response.data)
         } catch (error) {
             return Promise.reject(error)
