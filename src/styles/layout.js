f8b9305b4d1e95f6f706fe2c8c97ce2822930eff
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2019-12-25 20:17:04 -0300

    preparing layout

---
+++ src/styles/layout.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default styled.div`
+    position: relative;
+    margin: auto;
+    width: 800px;
+    height: 100vh;
+    max-width: calc(100vw - 40px);
+
+    display: grid;
+    grid-template-rows: 1fr 3fr 2fr 2fr;
+`;
