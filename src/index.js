47478cdaf27a8acce8cc8f383b4cec7e80ce21ad
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2019-12-25 20:23:39 -0300

    changing index.js and app.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Layout from "./styles/layout";
+import Input from "./components/input";
+import BingBackground from "./components/bing-background";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+        <>
+            <BingBackground />
+            <Layout>
+                <Input />
+            </Layout>
+        </>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
