b758ef249410bd95671f0f60f159083557b99fb5
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2019-12-25 20:23:09 -0300

    initial input

---
+++ src/components/input.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.input`
+    display: block;
+    height: 100%;
+    outline: none;
+    padding: 20px;
+    box-sizing: border-box;
+`;
