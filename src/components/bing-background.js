957feec2bda29a567af5249507305aac7d6e62bc
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2019-12-30 10:13:34 -0300

    refactoring bing background

--- src/components/bing-background.js
+++ src/components/bing-background.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import axios from "axios";
-
 import styled from "styled-components";
 import { fetchBingImage } from "../services/bingImage";
 
@@ -13,7 +11,7 @@ const Background = styled.div`
     left: 0;
 `;
 
-export default ({ currentColor }) => {
+export default () => {
     const [bingHomepageImage, setImage] = useState();
 
     useEffect(() => {
