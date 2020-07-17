921be470450e4342705735df3179e30ee8144656
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-07-17 16:22:20 -0300

    feat: get user location

--- weather-app/src/pages/Main/index.tsx
+++ weather-app/src/pages/Main/index.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 import BackgroundImage from '../../components/BackgroundImage';
 
-import { Container } from './styles';
+import { Container, WeatherContainer, InputContainer } from './styles';
+import { useGeolocation } from '../../hooks/geolocation';
+
+const fetcher = (url: string) => fetch(url).then(r => r.json());
 
 const Main: React.FC = () => {
+  const [defaultLocation, setDefaultLocation] = useState('');
+  const coords = useGeolocation();
+
+  const { data: defaultLocationData } = useSWR(
+    () =>
+      typeof coords.latitude !== 'undefined'
+        ? `https://api.opencagedata.com/geocode/v1/json?q=${coords.latitude},${coords.longitude}&key=c63386b4f77e46de817bdf94f552cddf&language=en`
+        : null,
+    fetcher,
+  );
+
+  useEffect(() => {
+    if (defaultLocationData) {
+      const [result] = defaultLocationData.results;
+      setDefaultLocation(
+        `${result.components.city}, ${result.components.state}`,
+      );
+    }
+  }, [defaultLocationData]);
+
   return (
     <>
       <Container>
-        <h1>Main page</h1>
+        <WeatherContainer>
+          <InputContainer>
+            <span data-icon="(" />
+            <input
+              type="text"
+              placeholder="Digite aqui sua localização"
+              defaultValue={defaultLocation}
+            />
+          </InputContainer>
+        </WeatherContainer>
       </Container>
       <BackgroundImage />
     </>
--- weather-app/src/pages/Main/styles.ts
+++ weather-app/src/pages/Main/styles.ts
@@ -6,5 +6,45 @@ export const Container = styled.div`
 
   display: flex;
   justify-content: center;
+
+  padding: 48px 2.4rem 2.4rem 2.4rem;
+`;
+
+export const WeatherContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 560px;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
   align-items: center;
+  background: #fff;
+  padding: 1.6rem;
+
+  span,
+  input {
+    color: #424242;
+  }
+
+  span {
+    font-size: 4rem;
+    line-height: 4rem;
+    height: 4rem;
+  }
+
+  input {
+    flex: 1;
+    font-size: 3.2rem;
+    line-height: 3.2rem;
+    width: calc(100% - 4rem);
+    padding-left: 1.6rem;
+    font-weight: bold;
+
+    &::placeholder {
+      color: #bdbdbd;
+    }
+  }
 `;
