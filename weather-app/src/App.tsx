53628935b1d1c5c9c4c9bb688a83efd0e5c72725
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-07-17 16:22:39 -0300

    chore: setup swr sync storage

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
+import { syncWithLocalStorage } from 'swr-sync-storage';
+import { GeolocationProvider } from './hooks/geolocation';
 import Main from './pages/Main';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <Main />
+      <GeolocationProvider>
+        <Main />
+      </GeolocationProvider>
       <GlobalStyle />
     </>
   );
 };
+
+syncWithLocalStorage();
 
 export default App;
