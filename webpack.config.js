c2278163a2dbc956101c4d0fefd51f8843f1a651
Gabriel Pireda <gabriel.pireda@gmail.com>
2020-07-28 18:19:36 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { DefinePlugin } = require('webpack')
+const path = require('path')
+const dotenv = require('dotenv')
+
+module.exports = {
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                auto: true,
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed),
+    }),
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      helpers: path.resolve(__dirname, 'src/helpers'),
+      icons: path.resolve(__dirname, 'src/icons'),
+      services: path.resolve(__dirname, 'src/services'),
+    },
+    extensions: ['.js', '.jsx'],
+  },
+}
