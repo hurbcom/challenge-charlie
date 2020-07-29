3e410c358e6ff6097db263468c8f0d2ed0f89ccf
Gabriel Pireda <gabriel.pireda@gmail.com>
2020-07-29 09:35:35 -0300

    uses client and maps forecasts method

--- src/services/openCagedService.js
+++ src/services/openCagedService.js
@@ -1,19 +1,19 @@
+import { client } from 'utils/client'
+
 const { OPEN_CAGED_API_KEY, OPEN_CAGED_API_URL } = process.env
 
 export function OpenCagedService({ latitude, longitude }) {
-  const queryString = `q=${latitude}+${longitude}&key=${OPEN_CAGED_API_KEY}&language=en`
+  const queryString = `?q=${latitude}+${longitude}&key=${OPEN_CAGED_API_KEY}&language=en`
 
   this.getLocale = function () {
-    return fetch(`${OPEN_CAGED_API_URL}${queryString}`)
-      .then(r => r.json())
-      .then(data => {
-        const [results] = data.results
-        const { components } = results
+    return client(`${OPEN_CAGED_API_URL}${queryString}`).then(data => {
+      const [results] = data.results
+      const { components } = results
 
-        return {
-          city: components.city,
-          state: components.state,
-        }
-      })
+      return {
+        city: components.city,
+        state: components.state,
+      }
+    })
   }
 }
--- src/services/openWeatherService.js
+++ src/services/openWeatherService.js
@@ -1,26 +1,50 @@
+import { client } from 'utils/client'
+
 const { OPEN_WEATHER_API_KEY, OPEN_WEATHER_API_URL } = process.env
 
 export function OpenWeatherService({ city }) {
   const language = navigator.language.replace(/-/g, '_')
-  const queryString = `q=${city}&APPID=${OPEN_WEATHER_API_KEY}&lang=${language}&units=metric`
+  const queryString = `?q=${city}&APPID=${OPEN_WEATHER_API_KEY}&lang=${language}&units=metric`
 
   this.getWeather = function () {
-    return fetch(`${OPEN_WEATHER_API_URL}${queryString}`)
-      .then(r => r.json())
-      .then(data => {
-        const [weather] = data.weather
+    return client(`${OPEN_WEATHER_API_URL}/weather${queryString}`).then(data => {
+      const [weather] = data.weather
 
-        return {
-          description: weather.description,
-          descriptionId: weather.id,
-          humidity: data.main.humidity,
-          pressure: data.main.pressure,
-          temperature: data.main.temp,
-          wind: {
-            degrees: data.wind.deg,
-            speed: data.wind.speed,
-          },
-        }
-      })
+      return {
+        description: weather.description,
+        descriptionId: weather.id,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        wind: {
+          degrees: data.wind.deg,
+          speed: data.wind.speed,
+        },
+      }
+    })
+  }
+
+  this.getForecasts = function (limitOfDays = 2) {
+    return client(`${OPEN_WEATHER_API_URL}/forecast${queryString}`).then(({ list }) => {
+      const currentDate = new Date().getDate()
+
+      const filtered = list
+        .filter(({ dt_txt }) => dt_txt.includes('00:00:00')) // get avg_temp from every included hour
+        .map(({ dt_txt, main }) => {
+          const forecastDate = new Date(dt_txt).getDate()
+          const mapDifferenceToPlainText = {
+            1: 'Amanhã',
+            2: 'Depois de amanhã',
+          }
+
+          return {
+            day: mapDifferenceToPlainText[forecastDate - currentDate],
+            temperature: main.temp,
+          }
+        })
+        .slice(0, limitOfDays)
+
+      return filtered
+    })
   }
 }
