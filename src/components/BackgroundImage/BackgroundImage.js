4ed72ae67a15d791a630a85613b4b518c5f1ddbc
Vitor Martins <vitorfigmartins@gmail.com>
2020-07-30 15:31:57 -0300

    Moving background image logic to a single component

--- src/components/BackgroundImage/BackgroundImage.js
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -1,10 +1,30 @@
 import React from 'react';
+
 import LoadingBackground from '../utils/LoadingBackground/LoadingBackground';
 
-export default (props) => (
-  props.img ?
-    <div className="background" style={{ backgroundImage: `url(${props.img})` }}>
-      {props.children}
-    </div> :
-    <LoadingBackground />
-);
+export default (props) => {
+  const [bgImg, setBgImg] = React.useState(null);
+
+  React.useEffect(() => {
+    getBingImg();
+  }, []);
+
+  const getBingImg = () => {
+    return fetch('https://cors-anywhere.herokuapp.com/https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR')
+      .then(res => res.json())
+      .then(data => {
+        setBgImg(`https://www.bing.com${data.images[0].url}`);
+      }).catch(err => console.log(err));
+  }
+
+  return (
+    <>
+      {bgImg ?
+        <div className="background" style={{ backgroundImage: `url(${bgImg})` }}>
+          {props.children}
+        </div> :
+        <LoadingBackground />
+      }
+    </>
+  );
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,27 +3,10 @@ import React from 'react';
 import WeatherCard from '../../components/WeatherCard';
 import BackgroundImage from '../../components/BackgroundImage/BackgroundImage';
 
-const Home = () => {
-  const [bgImg, setBgImg] = React.useState(null);
-
-  React.useEffect(() => {
-    getBingImg();
-  }, []);
-
-  const getBingImg = () => {
-    return fetch('https://cors-anywhere.herokuapp.com/https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR')
-      .then(res => res.json())
-      .then(data => {
-        setBgImg(`https://www.bing.com${data.images[0].url}`);
-      }).catch(err => console.log('err', err));
-  }
-
-  return (
-    <BackgroundImage img={bgImg}>
-      <WeatherCard />
-    </BackgroundImage>
-  );
-
-}
+const Home = () => (
+  <BackgroundImage>
+    <WeatherCard />
+  </BackgroundImage>
+);
 
 export default Home;
