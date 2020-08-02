72c07a1750b43c6af419bebccea07f859114eebb
Gabriel Pireda <gabriel.pireda@gmail.com>
2020-08-02 12:17:55 -0300

    translates wind directions

--- src/helpers/transformDegreesToText.js
+++ src/helpers/transformDegreesToText.js
@@ -1,6 +1,9 @@
+import i18n from 'utils/i18n'
+
+const rosePoints = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'].map(i18n)
+const circleFractions = 360 / rosePoints.length
+
 export default function transformDegreesToText(degrees) {
-  const rosePoints = ['N', 'NE', 'E', 'SE', 'S', 'SO', 'O', 'NO']
-  const circleFractions = 360 / rosePoints.length
   const index = Math.round(degrees / circleFractions)
 
   return rosePoints[index] || 'N'
