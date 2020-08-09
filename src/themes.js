69ed541df6a780da3416ae01e8ef3965f3eb0368
Romulo Tavares <romuloctavares@gmail.com>
2020-08-09 18:47:03 -0300

    Cria variações de tema conforme clima

---
+++ src/themes.js
@@ -0,0 +1,19 @@
+const themes = {
+    'hot': {
+        mainColor: '#e65100',
+        lightColor: '#ff833a',
+        darkColor: '#ac1900'
+    },
+    'medium': {
+        mainColor: '#ffc107',
+        lightColor: '#fff350',
+        darkColor: '#c79100'
+    },
+    'cold': {
+        mainColor: '#90caf9',
+        lightColor: '#a6d4fa',
+        darkColor: '#648dae'
+    }
+};
+
+export default themes;
