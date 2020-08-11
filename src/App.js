069ca9c55b6f9b4b8ca1587338152d2a395aa88a
Romulo Tavares <romuloctavares@gmail.com>
2020-08-11 16:09:50 -0300

    Altera App para ter estilo global

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
 import Background from './components/Background';
+import Weather from './components/Weather';
+
+import themes from './themes';
+import OpenCage from './services/opencage';
+import OpenWeather from './services/openweather';
+
+import GlobalStyle from './App.css.js';
 
 
 function App() {
-  return (
-    <Background>
-        Edit <code>src/App.js</code> and save to sda.
-    </Background>
-  );
+    const OpenWeatherApi = new OpenWeather('7ba73e0eb8efe773ed08bfd0627f07b8');
+    const OpenCageApi = new OpenCage('c63386b4f77e46de817bdf94f552cddf');
+
+    return (
+        <React.Fragment>
+            <GlobalStyle />
+            <Background>
+                <Weather
+                    themes={themes}
+                    opencage={OpenCageApi}
+                    openweather={OpenWeatherApi}
+                />
+            </Background>
+        </React.Fragment>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders background', () => {
-  render(<App />);
-  const backgroundElement = screen.getByRole('img');
-  expect(backgroundElement).toBeInTheDocument();
+    render(<App />);
+    const backgroundElement = screen.getByTitle('Background');
+    expect(backgroundElement).toBeInTheDocument();
 });
