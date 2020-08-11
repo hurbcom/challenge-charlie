a14c7c022982cee51798f6424ac2d5de9645cc47
Romulo Tavares <romuloctavares@gmail.com>
2020-08-11 16:10:36 -0300

    Melhora testes de hook de usefetch

--- src/hooks/index.test.js
+++ src/hooks/index.test.js
@@ -1,12 +1,9 @@
-import { renderHook, act } from '@testing-library/react-hooks';
-import { wait } from '@testing-library/react';
-
+import { renderHook } from '@testing-library/react-hooks';
 
 import { rest } from 'msw';
 import { setupServer } from 'msw/node'
 
-
-import { useFetch } from './';
+import useFetch from './';
 
 const server = setupServer(
     rest.get('http://example.com', (req, res, ctx) => {
@@ -26,15 +23,17 @@ test('initial state is idle', () => {
 });
 
 test('should start to fecth data', () => {
-    const {result} = renderHook(() => useFetch('http://example.com'));
+    const { result, waitForNextUpdate } = renderHook(() => useFetch('http://example.com'));
 
     expect(result.current.status).toEqual('fetching');
     expect(result.current.data).toEqual([]);
 });
 
 test('should return fecth data', async () => {
-    const {result} = renderHook(() => useFetch('http://example.com'));
+    const { result, waitForNextUpdate } = renderHook(() => useFetch('http://example.com'));
 
-    await wait(() => expect(result.current.status).toEqual('fetched'));
+    await waitForNextUpdate();
+
+    expect(result.current.status).toEqual('fetched');
     expect(result.current.data).toEqual({ results: 'test' });
 });
