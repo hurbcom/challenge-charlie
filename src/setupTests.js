5d46aaf21e4164868cea2a329a40d4633c2e2a6d
Romulo Tavares <romuloctavares@gmail.com>
2020-08-11 16:10:12 -0300

    Cria mock de getCurrentPosition

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+
+const mockGeolocation = {
+    getCurrentPosition: callback => {
+        return callback({
+            coords: {
+                latitude: -23.0126811,
+                longitude: -43.454839
+            }
+        });
+    }
+};
+
+global.navigator.geolocation = mockGeolocation;
