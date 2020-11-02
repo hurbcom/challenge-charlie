cea7634e76ea6537cbd306c9e85ab61c27015514
Renato Lins <renato@cuponeria.com.br>
2020-11-02 17:42:23 -0300

    add WeatherCard on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Background from 'components/Background'
 import SearchBar from 'components/SearchBar'
+import WeatherCard from 'components/WeatherCard'
 
 export default function Home() {
   return (
     <Background>
       <SearchBar />
+      <WeatherCard />
     </Background>
   )
 }
