8f91f2b18d3a1e794b277829c31dff78b976971f
Renato Lins <renato@cuponeria.com.br>
2020-11-02 17:42:08 -0300

    add meteocons reference on global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,22 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'MeteoconsRegular';
+    src: url('/meteocons-font/meteocons-webfont.eot');
+    src: url('/meteocons-font/meteocons-webfont.eot?#iefix') format('embedded-opentype'),
+        url('/meteocons-font/meteocons-webfont.woff') format('woff'),
+        url('/meteocons-font/meteocons-webfont.ttf') format('truetype'),
+        url('/meteocons-font/meteocons-webfont.svg#MeteoconsRegular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    outline: none;
   }
 
   html {
@@ -17,6 +29,11 @@ const GlobalStyles = createGlobalStyle`
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  .icon:before {
+    font-family: 'MeteoconsRegular';
+    content: attr(data-icon);
   }
 `
 
