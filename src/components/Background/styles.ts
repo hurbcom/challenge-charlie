82d38131033ba254d41c74c582b9dec5d1296910
Renato Lins <renato@cuponeria.com.br>
2020-11-02 17:40:49 -0300

    changes on background to change brightness

--- src/components/Background/styles.ts
+++ src/components/Background/styles.ts
@@ -1,17 +1,32 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main`
-  background: ${({ bg }) =>
-    bg === '#06092b' ? '#06092b' : `url(https://www.bing.com${bg})`};
-  background-size: cover;
-  background-position: center;
-  filter: ${({ bg }) => (bg === '#06092b' ? 'unset' : 'brightness(0.7)')};
+  position: relative;
   width: 100%;
-  height: 100%;
-  padding: 3rem;
+  min-height: 100vh;
+  padding: 8rem 3rem 3rem;
   text-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: ${({ bg }) =>
+      bg === '#06092b' ? '#06092b' : `url(https://www.bing.com${bg})`};
+    background-size: cover;
+    background-position: center;
+    filter: brightness(0.8);
+  }
+
+  & > * {
+    /* This will make it stack on top of the ::before */
+    position: relative;
+  }
 `
