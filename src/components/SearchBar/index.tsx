7457a80c621d6e8a6157edc7e305a8995a3cd328
Renato Lins <renato@cuponeria.com.br>
2020-11-02 17:41:09 -0300

    style search bar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react'
+
 import * as S from './styles'
 
-const SearchBar = () => <input placeholder="buscar" />
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState<string>()
+
+  const onChangeInputSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
+  }
+
+  const onPerformSearch = (e: React.SyntheticEvent<EventTarget>) => {
+    e.preventDefault()
+    if (!searchTerm) return false
+    console.log(searchTerm)
+  }
+  return (
+    <S.Wrapper onSubmit={onPerformSearch}>
+      <S.Input
+        placeholder="País, Estado ou Cidade"
+        onChange={onChangeInputSearch}
+      />
+      <S.Submit type="submit" value="buscar" />
+    </S.Wrapper>
+  )
+}
 
 export default SearchBar
--- src/components/SearchBar/styles.ts
+++ src/components/SearchBar/styles.ts
@@ -1,18 +1,58 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.main`
-  background-color: #06092b;
-  color: #fff;
-  width: 100%;
-  height: 100%;
-  padding: 3rem;
-  text-align: center;
+export const Wrapper = styled.form`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-wrap: wrap;
   justify-content: center;
+  background-color: #42454e;
+  padding: 1.6rem;
+  border-radius: 1.2rem 1.2rem 0 0;
+  width: 100%;
+  max-width: 425px;
 `
 
-export const Title = styled.h1`
-  font-size: 2.5rem;
+export const Input = styled.input`
+  background-color: #fff;
+  color: #42454e;
+  flex-grow: 1;
+  text-align: center;
+  height: 3rem;
+  padding: 2rem 1.5rem;
+  font-size: 1.3rem;
+  border: none;
+  border-radius: 1.2rem;
+  @media (min-width: 425px) {
+    font-size: 1.6rem;
+  }
+  @media (min-width: 768px) {
+    font-size: 2rem;
+  }
+`
+
+export const Submit = styled.input`
+  background-color: #1bba9a;
+  color: #fff;
+  text-align: center;
+  font-weight: bold;
+  border-radius: 1.2rem;
+  font-size: 1.3rem;
+  padding: 0.4rem 1.5rem;
+  transition: background-color ease 0.25s;
+  margin-left: 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
+  &:hover {
+    background-color: #189e83;
+  }
+  @media (min-width: 320px) {
+    margin-left: 0;
+  }
+  @media (min-width: 425px) {
+    font-size: 1.6rem;
+    margin-top: 0;
+    margin-left: 1rem;
+  }
+  @media (min-width: 768px) {
+    font-size: 2rem;
+  }
 `
