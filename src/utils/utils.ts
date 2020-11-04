68d83edbcf354e3894f23bc8081c22e8b0dc79f0
Renato Lins <renato@cuponeria.com.br>
2020-11-04 18:16:59 -0300

    add capitalize and windDeg util functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,6 +7,34 @@ class Utils {
     const celsius = ((fahrenheit - 32) * 5) / 9
     return celsius
   }
+  public static capitalize = (str: string) =>
+    str
+      .toLowerCase()
+      .replace(/(?:^|\s|["'([{])+\S/g, (match: string) => match.toUpperCase())
+  public static getWindDeg = (value: number) => {
+    if (value <= 11.25) return 'N'
+    const newValue = value - 11.25
+    const allDirections = [
+      'NNE',
+      'NE',
+      'ENE',
+      'E',
+      'ESE',
+      'SE',
+      'SSE',
+      'S',
+      'SSW',
+      'SW',
+      'WSW',
+      'W',
+      'WNW',
+      'NW',
+      'NNW',
+      'N'
+    ]
+    const dIndex = Math.floor(newValue / 22.5)
+    return allDirections[dIndex] ? allDirections[dIndex] : 'N'
+  }
 }
 
 export default Utils
