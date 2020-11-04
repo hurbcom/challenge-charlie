0cc5e57864dffc37ce55f405cefd840d085fe3ff
Renato Lins <renato@cuponeria.com.br>
2020-11-04 18:18:04 -0300

    get weather daily on open weather service

--- src/services/openWeatherService.ts
+++ src/services/openWeatherService.ts
@@ -13,6 +13,26 @@ class OpenWeatherService {
       console.error(err)
     }
   }
+  public static getWeatherDaily = async (
+    lat: number,
+    long: number,
+    language = 'pt_br'
+  ) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&appid=7ba73e0eb8efe773ed08bfd0627f07b8&units=metric&lang=${language}`
+      )
+      const results = await response.json()
+      const { current, daily } = results
+      return {
+        today: current,
+        tomorrow: daily[1],
+        afterTomorrow: daily[2]
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
 }
 
 export default OpenWeatherService
