607c0a71196b4fb64b7bd9503d4a1d3238ffdc84
Renato Lins <renato@cuponeria.com.br>
2020-11-04 23:09:25 -0300

    error handling on opencageservice

--- src/services/openCageService.ts
+++ src/services/openCageService.ts
@@ -24,11 +24,13 @@ class OpenCageService {
         `https://api.opencagedata.com/geocode/v1/json?q=${cityName}&key=c63386b4f77e46de817bdf94f552cddf&language=${language}`
       )
       const results = await response.json()
+      if (results.results.length == 0) throw new Error('cidade não encontrada')
       const { lat, lng } = results.results[0].geometry
       const { formatted } = results.results[0]
       return { lat, long: lng, city: formatted }
     } catch (err) {
       console.error(err)
+      return { message: 'Não encontramos o local pesquisado, tente novamente' }
     }
   }
 }
