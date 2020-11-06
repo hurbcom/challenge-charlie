e7d52ab1c18aa2428005afcead667c713b6fd8c2
mateusxis <mateusx@poli.ufrj.br>
2020-11-05 21:13:52 -0300

    feat(BackgroundImage): ✨ create container

---
+++ src/container/BackgroundImageContainer.tsx
@@ -0,0 +1,31 @@
+import React, {
+  FunctionComponent,
+  HtmlHTMLAttributes,
+  useEffect,
+  useState
+} from 'react';
+
+import BackgroundImage from '../components/BackgroundImage';
+import bing from '../services/externals/bing';
+
+type TBackgroundImageContainer = HtmlHTMLAttributes<HTMLDivElement>;
+
+const BackgroundImageContainer: FunctionComponent<TBackgroundImageContainer> = ({
+  children
+}) => {
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  useEffect(() => {
+    bing.getBackgroundImageUrl().then((url) => {
+      console.log(url);
+      
+      setImageUrl(url);
+    });
+  }, []);
+
+  return (
+    <BackgroundImage backgroundImageUrl={imageUrl}>{children}</BackgroundImage>
+  );
+};
+
+export default BackgroundImageContainer;
