b988b91bbaed97664d123a0979ef1d087dd36353
mateusxis <mateusx@poli.ufrj.br>
2020-11-09 16:02:31 -0300

    refactor(pages): ✅ add store in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,26 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { FunctionComponent } from 'react';
+import { Provider } from 'react-redux';
 
+import store from '../src/app/store';
 import { GlobalStyle } from '../src/theme/GlobalStyle';
+
+import '../public/fonts/weather-icons.css';
 
 const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
   <>
     <GlobalStyle />
     <Head>
       <title>Challenge Charlie</title>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
     </Head>
-    <Component {...pageProps} />
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
   </>
 );
 
