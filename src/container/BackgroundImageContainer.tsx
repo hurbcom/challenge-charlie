8f4be6f5aef597a429e7a56000c55b7f8af521cd
mateusxis <mateusx@poli.ufrj.br>
2020-11-09 15:59:36 -0300

    refactor(container): ✅ update BackgroundImage

--- src/container/BackgroundImageContainer.tsx
+++ src/container/BackgroundImageContainer.tsx
@@ -1,25 +1,26 @@
-import React, {
-  FunctionComponent,
-  HtmlHTMLAttributes,
-  useEffect,
-  useState
-} from 'react';
+import React, { FunctionComponent, HtmlHTMLAttributes, useMemo } from 'react';
 
 import BackgroundImage from '../components/BackgroundImage';
-import { getBackgroundImageUrl } from '../services/externals/bing';
+import { useFetch } from '../services/api';
 
 type TBackgroundImageContainer = HtmlHTMLAttributes<HTMLDivElement>;
 
 const BackgroundImageContainer: FunctionComponent<TBackgroundImageContainer> = ({
   children
 }) => {
-  const [imageUrl, setImageUrl] = useState<string>();
+  const { data: imageData } = useFetch({
+    url: '/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR',
+    baseURL: process.env.bingUrl
+  });
 
-  useEffect(() => {
-    getBackgroundImageUrl().then((url) => {
-      setImageUrl(url);
-    });
-  }, []);
+  const imageUrl = useMemo(() => {
+    if (imageData) {
+      const { url } = imageData?.images?.[0];
+      return url ? `${process.env.bingUrl}${url}` : undefined;
+    }
+
+    return undefined;
+  }, [imageData]);
 
   return (
     <BackgroundImage backgroundImageUrl={imageUrl}>{children}</BackgroundImage>
