e5e29ae9562352e142206d1236bb03470170f4b9
mateusxis <mateusx@poli.ufrj.br>
2020-11-09 15:52:56 -0300

    feat(notificationSliece): ✨ create reducer

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,9 +1,24 @@
 import axios from 'axios';
+import useSWR from 'swr';
 
 import { IAPI } from './interface';
 
-export const get = ({ baseUrl, url }: IAPI) =>
+export const get = ({ baseURL, url }: IAPI) =>
   axios
-    .get(url, { baseURL: baseUrl })
+    .get(url, { baseURL })
     .then((resolve) => resolve?.data)
     .catch((err) => err);
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+export function useFetch<Data = any, Error = any>({ baseURL, url }: IAPI) {
+  const { data, error, isValidating } = useSWR<Data, Error>(
+    url,
+    async (url) => {
+      const response = await axios.get(url, { baseURL });
+
+      return response.data;
+    }
+  );
+
+  return { data, error, isValidating };
+}
