42d08bcf091b85487343957512bdf8964e7d995d
mateusxis <mateusx@poli.ufrj.br>
2020-11-09 15:49:09 -0300

    feat(WeatherInformation): ✨ create component

---
+++ src/components/WeatherInformation/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SDescription = styled.strong`
+  margin-bottom: 1rem;
+  text-transform: capitalize;
+  color: #ffffff;
+  font-size: 1.5rem;
+  font-weight: 500;
+`;
+
+export const SInformation = styled.p`
+  text-transform: capitalize;
+  color: #ffffff;
+  font-size: 1rem;
+`;
