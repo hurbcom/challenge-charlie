787828920577c249980a15852b080271db57bd9c
mateusxis <mateusx@poli.ufrj.br>
2020-11-09 15:58:19 -0300

    refactor(externals): ✅ update

--- src/services/externals/openCage/openCage.ts
+++ src/services/externals/openCage/openCage.ts
@@ -1,18 +1,24 @@
 import { get } from '../../api';
-import { formatLocationName } from '../../../helpers/string';
+import { formatLocationName } from '../../../helpers/weather';
 
 import { IOpenCage, ICoordinates, ILocation } from './interface';
 
 export const getGeoLocationCoordinates = ({ locationName }: ILocation) => {
   return get({
     url: `geocode/v1/json?q=${locationName}&key=${process.env.openCageKey}&language=pt-BR&limit=1`,
-    baseUrl: process.env.openCageUrl
+    baseURL: process.env.openCageUrl
   })
     .then((data: IOpenCage) => {
       const { city, country, state } = data?.results?.[0]?.components;
+      const { formatted } = data?.results?.[0];
       const { lat, lng } = data?.results?.[0]?.geometry;
 
-      const nameFormated = formatLocationName({ city, country, state });
+      const nameFormated = formatLocationName({
+        city,
+        country,
+        formatted,
+        state
+      });
 
       return {
         coordinates: {
@@ -28,12 +34,18 @@ export const getGeoLocationCoordinates = ({ locationName }: ILocation) => {
 export const getGeoLocationName = ({ lat, lng }: ICoordinates) => {
   return get({
     url: `geocode/v1/json?q=${lat},${lng}&key=${process.env.openCageKey}&language=pt-BR`,
-    baseUrl: process.env.openCageUrl
+    baseURL: process.env.openCageUrl
   })
     .then((data: IOpenCage) => {
       const { city, country, state } = data?.results?.[0]?.components;
+      const { formatted } = data?.results?.[0];
 
-      const nameFormated = formatLocationName({ city, country, state });
+      const nameFormated = formatLocationName({
+        city,
+        country,
+        formatted,
+        state
+      });
 
       return {
         locationName: nameFormated
