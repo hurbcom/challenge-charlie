96e671399a7a225ad41459c7ea10e545e1541b56
mateusxis <mateusx@poli.ufrj.br>
2020-11-09 16:03:08 -0300

    refactor(index): ✅ add laytout in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
 import React, { FunctionComponent } from 'react';
 
 import BackgroundImage from '../container/BackgroundImageContainer';
+import Store from '../container/StoreContainer';
+import CardWeather from '../container/CardWeatherContainer';
+import FormInput from '../container/FormInputContainer';
+import Page from '../components/Page';
 
 const Index: FunctionComponent = () => {
-  return <BackgroundImage>Hello World</BackgroundImage>;
+  return (
+    <>
+      <BackgroundImage>
+        <Page>
+          <FormInput />
+          <CardWeather />
+        </Page>
+      </BackgroundImage>
+      <Store />
+    </>
+  );
 };
 
 export default Index;
