d8217343578635febb429bb5de0172367e53e12f
Douglas Thomaz <douglas.thomaz@spot-edu.com.br>
2020-11-24 20:19:43 -0300

    commit-01 - scss config & folder structure defined

---
+++ src/components/MainCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainCard = () => {
+    return (
+        <div className="main_card--container">
+            <h1>alo mundo</h1>
+        </div>
+    );
+};
+
+export default MainCard;
