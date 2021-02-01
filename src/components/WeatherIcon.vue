d4355a5ed167d7447620c32e91fc2c5099886366
Filipe <filipebsmonteiro@gmail.com>
2021-02-01 09:05:35 -0300

    - Homepage   - Specific componenet to manage Weather Icons   - Get location with window.navigator   - Using openCage to location   - Using openWeather to Weather

---
+++ src/components/WeatherIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="meteocons" :data-icon="icon"/>
+</template>
+
+<script>
+export default {
+  name: "WeatherIcon",
+  props: {
+    icon: {
+      type: String,
+      default: ')'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.meteocons:before {
+  font-family: 'MeteoconsRegular';
+  content: attr(data-icon);
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,7 @@
   <div class="d-flex vh-100 justify-content-center align-items-center">
     <b-card body-class="p-0">
       <b-card-header>
+        <WeatherIcon icon="("/>
         Rio De Janeiro, Rio De Janeiro
       </b-card-header>
     </b-card>
@@ -13,9 +14,93 @@ import WeatherIcon from "@/components/WeatherIcon";
 import {mapActions, mapGetters} from 'vuex';
 
 export default {
-  name: "HomePage"
+  name: "HomePage",
+  components: {WeatherIcon},
+  computed: {
+    ...mapGetters({
+      location: 'openCage/list',
+      weather: 'openWeather/list'
+    })
+  },
+  methods: {
+    ...mapActions([
+      'openCage/loadList',
+      'openWeather/loadList'
+    ])
+  },
+  async mounted() {
+    window.navigator.geolocation.getCurrentPosition(async GeoLocationPostion => {
+      await this['openCage/loadList']({
+        q: `${GeoLocationPostion.coords.latitude}+${GeoLocationPostion.coords.longitude}`,
+        pretty: 1,
+        no_annotations: 1
+      })
+
+      if (this.location && this.location.results.length > 0) {
+        const userLocation = this.location.results[0]
+
+        await this['openWeather/loadList']({
+          q: `${userLocation.components.city},${userLocation.components.state_code},${userLocation.components.country_code}`
+        })
+        console.log(this.weather)
+      }
+    }, console.error);
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.card-header {
+  color: #898788;
+  font-weight: bold;
+}
+
+.card-cold {
+  color: white;
+
+  .weather:nth-child(1) {
+    background-color: hsla(212, 83%, 49%, 0.7);
+  }
+
+  .weather:nth-child(1) {
+    background-color: hsla(212, 97%, 50%, 0.8);
+  }
+
+  .weather:nth-child(1) {
+    background-color: hsla(213, 97%, 36%, 0.9);
+  }
+}
+
+.card-normal {
+  color: white;
+
+  .weather:nth-child(1) {
+    background-color: hsla(48, 83%, 49%, 0.7);
+  }
+
+  .weather:nth-child(1) {
+    background-color: hsla(48, 97%, 50%, 0.8);
+  }
+
+  .weather:nth-child(1) {
+    background-color: hsla(49, 97%, 36%, 0.9);
+  }
+}
+
+.card-hot {
+  color: white;
+
+  .weather:nth-child(1) {
+    background-color: hsla(354, 83%, 49%, 0.7);
+  }
+
+  .weather:nth-child(1) {
+    background-color: hsla(354, 97%, 50%, 0.8);
+  }
+
+  .weather:nth-child(1) {
+    background-color: hsla(355, 97%, 36%, 0.9);
+  }
+}
+
 </style>
