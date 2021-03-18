4583236a409072fd362541bc404585e4a6e1d2e6
marcio <ciorma7@hotmail.com>
2021-03-18 19:28:53 -0300

    Dicionário de tradução de temperaturas

---
+++ src/app/utils/translationDictionary.ts
@@ -0,0 +1,65 @@
+let translateDictionary = 
+{
+    //clear sky
+    "clear sky": "Céu limpo",
+
+    //clouds
+    "few clouds": "Poucas nuvens",
+    "scattered clouds": "Nuvens dispersas",
+    "broken clouds": "Nuvens quebradas",
+    "overcast clouds": "Céu nublado",
+
+    //rain
+    "light rain": "Chuva fraca",
+    "shower rain": "Chuva forte",
+    "moderate rain": "Chuva moderada",
+    "heavy intensity rain": "Chuva intensa",
+    "very heavy rain": "Chuva muito forte",
+    "extreme rain": "Chuva extremamente forte",
+    "freezing rain": "Chuva de granizo",
+    "light intensity shower rain": "Chuva de baixa intensidade",
+    "heavy intensity shower rain": "Chuva de alta intensidade",
+    "ragged shower rain": "Chuva irregular",
+    "rain": "Chuva",
+
+    //thunderstorm
+    "thunderstorm":"Raios e trovões",
+    "light thunderstorm":"Tempestade leve",
+    "heavy thunderstorm":"Tempestade forte",
+    "ragged thunderstorm":"Tempestade irregular",
+    "thunderstorm with light drizzle":"Tempestade com garoa leve",
+    "thunderstorm with drizzle":"Tempestade com garoa",
+    "thunderstorm with heavy drizzle":"Tempestade com garoa forte",
+    "thunderstorm with heavy rain":"Chuva forte com raios e trovões",
+    "thunderstorm with rain":"Chuva com raios e trovões",
+    "thunderstorm with light rain":"Chuva leve com raios e trovões",
+
+    //snow
+    "Snow": "Neve",
+    "light snow": "Neve fraca",
+    "Heavy snow": "Neve forte",
+    "Sleet": "Granizo",
+    "Light shower sleet": "Chuva de granizo fraca",
+    "Shower sleet": "Chuva de granizo",
+    "Light rain and snow": "Chuva fraca e neve",
+    "Rain and snow": "Chuva e neve",
+    "Light shower snow": "Chuva fraca de neve",
+    "Shower snow": "Chuva de neve",
+    "Heavy shower snow": "Chuva forte de neve",
+
+    // Drizzle
+    "light intensity drizzle": "Garoa de baixa intensidade",
+    "drizzle": "Chuvisco",
+    "heavy intensity drizzle": "Garoa forte",
+    "light intensity drizzle rain": "Chuva com garoa leve",
+    "drizzle rain": "Garoa",
+    "heavy intensity drizzle rain": "Chuva forte com garoa",
+    "shower rain and drizzle": "Chuva e garoa",
+    "heavy shower rain and drizzle": "Chuva forte e garoa",
+    "shower drizzle": "Chuvisco intenso",
+
+    //mist
+    "mist": "Névoa"
+}
+
+export const dictionary = translateDictionary;
