5da4077014c50cad409367029e369f2155fa978e
marcio <ciorma7@hotmail.com>
2021-03-18 19:29:54 -0300

    Commit do modelo localWeather

---
+++ src/app/models/localWeather.ts
@@ -0,0 +1,15 @@
+export class LocalWeather
+{
+	public min: string;
+	public max: string;
+	public time: string;
+	public icon: string;
+	public wind: string;
+	public humidity: string;
+	public pressure: string;
+	public seaLevel: string;
+	public feelsLike: string;
+	public visibility: string;
+	public temperature: string;
+	public description: string;
+}
