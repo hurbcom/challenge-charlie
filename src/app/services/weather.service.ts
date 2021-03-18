dc34d757ef9abe30199f08cb337c8b9ea9d769e4
marcio <ciorma7@hotmail.com>
2021-03-18 19:30:13 -0300

    Commit do Service de weather

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -6,23 +6,31 @@ import { Http } from '@angular/http';
 })
 export class WeatherService
 {
-	baseUrl: string = "http://api.openweathermap.org/data/2.5/weather?";
+	baseUrl: string = "http://api.openweathermap.org/data/2.5/forecast?";
 	urlByName = "q="
-	UrlByLat: string = "lat=";
-	UrlByLong: string = "&lon=";
-	weatherApiId: string = "&appid=7ba73e0eb8efe773ed08bfd0627f07b8";
+	urlByLat: string = "lat=";
+	urlByLong: string = "&lon=";
+	urlByUnit: string = "&units="
+	weatherApiId: string = "&cnt=4&appid=7ba73e0eb8efe773ed08bfd0627f07b8";
+	iconUrl: string = "http://openweathermap.org/img/wn/";
 
 	constructor(private http: Http) { }
 
-	public getWeather(locationName: string): any
+	public getWeather(locationName: string, unit: string): any
 	{
-		let url = this.baseUrl + this.urlByName + locationName + this.weatherApiId;
+		let url = this.baseUrl + this.urlByName + locationName + this.urlByUnit + unit + this.weatherApiId;
 		return this.http.get(url);
 	}
 	
-	public getWeatherByLatLong(lat: any, long: any)
+	public getWeatherByLatLong(lat: any, long: any, unit: string)
 	{
-		let url = this.baseUrl + this.UrlByLat + lat + this.UrlByLong + long + this.weatherApiId;
+		let url = this.baseUrl + this.urlByLat + lat + this.urlByLong + long + this.urlByUnit + unit + this.weatherApiId;
+		return this.http.get(url);
+	}
+
+	public getWeatherIcon(iconCode: string)
+	{
+		let url = this.iconUrl + iconCode;
 		return this.http.get(url);
 	}
 }
