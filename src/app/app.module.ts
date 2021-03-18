a9557dbcc5e28a7d1cbbdb278282c6559726e4ac
marcio <ciorma7@hotmail.com>
2021-03-18 19:42:15 -0300

    Retirando módulo não utilizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,10 +13,6 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LoadingComponent } from './components/loading/loading.component';
-
-export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient);
-}
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,14 +26,6 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
     HttpModule,
     FormsModule,
     MatProgressSpinnerModule,
-    TranslateModule.forRoot({ 
-      loader: 
-      {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
     BrowserAnimationsModule
   ],
   providers: [
