51fe6c68e8fe96d915503ecef26b68975047d66c
Alex Tavares <alex1302@live.com>
2021-03-27 20:30:00 -0300

    add night icons

--- src/Icons/WeatherIcon.tsx
+++ src/Icons/WeatherIcon.tsx
@@ -7,29 +7,43 @@ import {
     WiDayRainMix,
     WiLightning,
     WiSnowflakeCold,
-    WiFog
+    WiFog,
+    WiMoonAltNew,
+    WiNightCloudy,
+    WiNightRainMix
 } from "react-icons/wi";
 
 function WeatherIcon({ iconId, className }: any) {
-    console.log('ICON', iconId)
     switch (iconId) {
         case "01d":
             return <WiDaySunny className={className} />;
+        case "01n":
+            return <WiMoonAltNew className={className} />;
         case "02d":
             return <WiDayCloudy className={className} />;
+        case "02n":
+            return <WiNightCloudy className={className} />;
         case "03d":
+        case "03n":
             return <WiCloud className={className} />;
         case "04d":
+        case "04n":
             return <WiCloudy className={className} />;
         case "09d":
+        case "09n":
             return <WiShowers className={className} />;
         case "10d":
             return <WiDayRainMix className={className} />;
+        case "10n":
+            return <WiNightRainMix className={className} />;
         case "11d":
+        case "11n":
             return <WiLightning className={className} />;
         case "13d":
+        case "13n":
             return <WiSnowflakeCold className={className} />;
         case "50d":
+        case "50n":
             return <WiFog className={className} />;
         default:
             return null;
