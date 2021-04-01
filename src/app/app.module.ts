6ac738925c3c835db3cef32b1bdf1b1b101db672
marcio antonio <ciorma7@hotmail.com>
2021-04-01 17:42:28 -0300

    Corrigindo warning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,16 +8,14 @@ import { HomeComponent } from './pages/home/home.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { WeatherService } from './services/weather.service';
-import { LoadingComponent } from './components/loading/loading.component';
+import { LoadingModule } from './components/loading/loading.module';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoadingModule } from './components/loading/loading.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LoadingComponent
+    HomeComponent
   ],
   imports: [
 	FormsModule,
