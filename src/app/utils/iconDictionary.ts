2bdb0da85030b8d21458cbf94b06cee416bec88d
marcio <ciorma7@hotmail.com>
2021-04-03 12:42:46 -0300

    Criação do dicionário de ícones

---
+++ src/app/utils/iconDictionary.ts
@@ -0,0 +1,65 @@
+let iconDictionary = 
+{
+    //clear sky
+    "clear sky": "../../../assets/icons/SVG/2.svg",
+
+    //clouds
+    "few clouds": "../../../assets/icons/SVG/8.svg",
+    "scattered clouds": "../../../assets/icons/SVG/25.svg",
+    "broken clouds": "../../../assets/icons/SVG/25.svg",
+    "overcast clouds": "../../../assets/icons/SVG/14.svg",
+
+    //rain
+    "light rain": "../../../assets/icons/SVG/17.svg",
+    "shower rain": "../../../assets/icons/SVG/18.svg",
+    "moderate rain": "../../../assets/icons/SVG/18.svg",
+    "heavy intensity rain": "../../../assets/icons/SVG/18.svg",
+    "very heavy rain": "../../../assets/icons/SVG/18.svg",
+    "extreme rain": "../../../assets/icons/SVG/18.svg",
+    "freezing rain": "../../../assets/icons/SVG/24.svg",
+    "light intensity shower rain": "../../../assets/icons/SVG/17.svg",
+    "heavy intensity shower rain": "../../../assets/icons/SVG/18.svg",
+    "ragged shower rain": "../../../assets/icons/SVG/17.svg",
+    "rain": "../../../assets/icons/SVG/18.svg",
+
+    //thunderstorm
+    "thunderstorm":"26.svg",
+    "light thunderstorm":"16.svg",
+    "heavy thunderstorm":"27.svg",
+    "ragged thunderstorm":"27.svg",
+    "thunderstorm with light drizzle":"20.svg",
+    "thunderstorm with drizzle":"20.svg",
+    "thunderstorm with heavy drizzle":"24.svg",
+    "thunderstorm with heavy rain":"27.svg",
+    "thunderstorm with rain":"27.svg",
+    "thunderstorm with light rain":"26.svg",
+
+    //snow
+    "snow": "../../../assets/icons/SVG/23.svg",
+    "light snow": "../../../assets/icons/SVG/21.svg",
+    "Heavy snow": "../../../assets/icons/SVG/23.svg",
+    "Sleet": "../../../assets/icons/SVG/24.svg",
+    "Light shower sleet": "../../../assets/icons/SVG/24.svg",
+    "Shower sleet": "../../../assets/icons/SVG/24.svg",
+    "Light rain and snow": "../../../assets/icons/SVG/23.svg",
+    "Rain and snow": "../../../assets/icons/SVG/23.svg",
+    "Light shower snow": "../../../assets/icons/SVG/22.svg",
+    "Shower snow": "../../../assets/icons/SVG/23.svg",
+    "Heavy shower snow": "../../../assets/icons/SVG/23.svg",
+
+    // Drizzle
+    "light intensity drizzle": "../../../assets/icons/SVG/20.svg",
+    "drizzle": "../../../assets/icons/SVG/20.svg",
+    "heavy intensity drizzle": "../../../assets/icons/SVG/28.svg",
+    "light intensity drizzle rain": "../../../assets/icons/SVG/20.svg",
+    "drizzle rain": "../../../assets/icons/SVG/18.svg",
+    "heavy intensity drizzle rain": "../../../assets/icons/SVG/18.svg",
+    "shower rain and drizzle": "../../../assets/icons/SVG/18.svg",
+    "heavy shower rain and drizzle": "../../../assets/icons/SVG/18.svg",
+    "shower drizzle": "../../../assets/icons/SVG/18.svg",
+
+    //mist
+    "mist": "../../../assets/icons/SVG/25.svg"
+}
+
+export const iconsDictionary = iconDictionary;
