c9a495991dafce2dc4cae21e21a556c1c3cc191a
marcio <ciorma7@hotmail.com>
2021-04-07 22:08:38 -0300

    refactor

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -5,20 +5,13 @@ import { HttpClient } from '@angular/common/http';
 	providedIn: 'root'
 })
 export class WeatherService {
-	baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?';
-	urlByName = 'q=';
-	urlByLat = 'lat=';
-	urlByLong = '&lon=';
-	urlByUnit = '&units=';
-	weatherApiId = '&cnt=4&appid=7ba73e0eb8efe773ed08bfd0627f07b8';
-	iconUrl = 'http://openweathermap.org/img/wn/';
 
 	constructor(private http: HttpClient) { }
 
 	public getWeather(locationName: string, unit: string): any {
-		console.log('unidade 1:', unit);
-		const url = this.baseUrl + this.urlByName + locationName + this.urlByUnit + unit + this.weatherApiId;
-		//passar o retorno para um dto
+		const url = `http://api.openweathermap.org/data/2.5/forecast?q=
+		${locationName}&units=
+		${unit}&cnt=17&appid=7ba73e0eb8efe773ed08bfd0627f07b8`;
 		try
 		{
 			const response = this.http.get(url);
@@ -31,7 +24,9 @@ export class WeatherService {
 	}
 
 	public getWeatherByLatLong(lat: any, long: any, unit: string): any {
-		const url = this.baseUrl + this.urlByLat + lat + this.urlByLong + long + this.urlByUnit + unit + this.weatherApiId;
+		const url = `http://api.openweathermap.org/data/2.5/forecast?lat=
+		${lat}&lon=${long}&units=
+		${unit}&cnt=17&appid=7ba73e0eb8efe773ed08bfd0627f07b8`;
 		try
 		{
 			const response = this.http.get(url);
