c4c2eefb77ff89197a4a3a929702959ac01e2210
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-04-19 19:21:06 -0300

    background

--- charlie/src/App.js
+++ charlie/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Weather from "../src/components/Weather/Weather";
+import Background from '../src/components/Background/Background';
+
 
 function App() {
   return (
     <div className="App">
+        <Background />
+          
       <Weather />
     </div>
   );
---
+++ charlie/src/components/Background/BackgroundStyle.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const BackgroundImg = styled.div`
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+`;
