a035d0d5babf995bb9d6ebb0ac87f3450da4676d
Gustavo Brayn <gustavo.augusto.brayn@gmail.com>
2021-05-04 19:19:10 -0300

    added lang property to html tag to improve accessibility

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en-US">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
