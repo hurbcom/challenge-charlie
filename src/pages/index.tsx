b6ebd3cbe5273d667ec8bc4443cce4ebbe6d3212
Gustavo Brayn <gustavo.augusto.brayn@gmail.com>
2021-05-04 19:28:01 -0300

    finished home page with services and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,157 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GetStaticProps } from "next";
+import Head from "next/head";
 import axios from "axios";
 import AsyncSelect from "react-select/async";
 
-import styles from "./home.module.scss";
-import fetchLocationOpenCage from "../services/openCage";
+import styles from "./styles.module.scss";
+import fetchLocationOpenCage from "../services/ForwardGeocoding";
+import FetchWeatherService from "../services/FetchWeather";
+import Card from "../components/Card";
+import SubCard from "../components/Subcard";
+import ReverseGeocoding from "../services/ReverseGeocoding";
 
 type HomeProps = {
   bingImageUrl: string;
 };
+interface City {
+  label: string;
+  name: string;
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
+interface Temperature {
+  celsius: number;
+  fahrenheit: number;
+}
+
+interface Weather {
+  today: {
+    temperature: Temperature;
+    condition: string;
+    wind: number;
+    humidity: number;
+    pressure: number;
+    icon: string;
+  };
+  tomorrow: {
+    label: string;
+    temperature: Temperature;
+  };
+  afterTomorrow: {
+    label: string;
+    temperature: Temperature;
+  };
+}
 
 const Home = ({ bingImageUrl }: HomeProps) => {
-  const [place, setPlace] = useState({});
+  const [city, setCity] = useState({} as City);
+  const [weather, setWeather] = useState({} as Weather);
+  const [toggleCelsius, setToggleCelsius] = useState(true);
+  const [toggleFahrenheit, setToggleFahrenheit] = useState(false);
+
+  const changeTemperatureUnit = () => {
+    if (toggleCelsius) {
+      setToggleCelsius(false);
+      setToggleFahrenheit(true);
+      return;
+    }
+
+    setToggleCelsius(true);
+    setToggleFahrenheit(false);
+  };
 
   const loadOptions = async (
     inputValue: string,
-    callback: (places: unknown[]) => void
+    callback: (cities: unknown[]) => void
   ) => {
-    if (inputValue.length < 5) return;
+    if (inputValue.length < 3) return;
 
-    const places = await fetchLocationOpenCage(inputValue);
+    const cities = await fetchLocationOpenCage(inputValue);
 
-    callback(places);
+    callback(cities);
   };
 
-  return (
-    <div className={styles.container}>
-      <img src={bingImageUrl} />
+  useEffect(() => {
+    if (!city.label) return;
 
-      <div className={styles.inputContainer}>
-        <AsyncSelect
-          placeholder="Digite uma cidade para busca"
-          classNamePrefix="filter"
-          cacheOptions
-          loadOptions={loadOptions}
-          onChange={setPlace}
-          value={place}
-        />
+    async function getWeather() {
+      const weatherPayload = await FetchWeatherService(city.coords);
+      setWeather(weatherPayload);
+    }
+
+    getWeather();
+  }, [city]);
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const weatherPayload = await FetchWeatherService({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+
+        const cityPayload = await ReverseGeocoding({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+
+        setWeather(weatherPayload);
+        setCity(cityPayload);
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Weather App</title>
+      </Head>
+      <div
+        className={styles.homeContainer}
+        style={{ backgroundImage: `url(${bingImageUrl})` }}
+      >
+        <main>
+          <div className={styles.inputContainer}>
+            <AsyncSelect
+              placeholder="Digite uma cidade para busca"
+              classNamePrefix="filter"
+              //@ts-ignore
+              loadOptions={loadOptions}
+              onChange={setCity}
+              value={city}
+              instanceId="select"
+            />
+          </div>
+
+          <Card
+            today={weather.today}
+            toggleCelsius={toggleCelsius}
+            toggleFahrenheit={toggleFahrenheit}
+            changeTemperatureUnit={changeTemperatureUnit}
+          />
+
+          <SubCard
+            temperature={weather.tomorrow?.temperature}
+            label={weather.tomorrow?.label}
+            toggleCelsius={toggleCelsius}
+            toggleFahrenheit={toggleFahrenheit}
+            changeTemperatureUnit={changeTemperatureUnit}
+          />
+
+          <SubCard
+            temperature={weather.afterTomorrow?.temperature}
+            label={weather.afterTomorrow?.label}
+            toggleCelsius={toggleCelsius}
+            toggleFahrenheit={toggleFahrenheit}
+            changeTemperatureUnit={changeTemperatureUnit}
+          />
+        </main>
       </div>
-      {JSON.stringify(place)}
-    </div>
+    </>
   );
 };
 
