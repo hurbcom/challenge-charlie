5fb9bc5931e5cecbd5bd34f574946ebab1720b20
aquistapace <59019615+aquistapace@users.noreply.github.com>
2021-06-10 11:15:42 -0300

    fixed wind direction

--- src/utils/windDirection.ts
+++ src/utils/windDirection.ts
@@ -2,11 +2,11 @@
 
 export function windDirection(deg: number) {
     if (deg <= 0 || deg >= 360) return 'N';
-    if (deg > 0 || deg <= 45) return 'NE';
-    if (deg > 45 || deg <= 95) return 'L';
-    if (deg > 90 || deg <= 135) return 'SE';
-    if (deg > 135 || deg <= 180) return 'S';
-    if (deg > 180 || deg <= 225) return 'S0';
-    if (deg > 225 || deg <= 270) return 'O';
-    if (deg > 270 || deg <= 325) return 'N0';
+    if (deg <= 45) return 'NE ';
+    if (deg <= 95) return 'L ';
+    if (deg <= 135) return 'SE ';
+    if (deg <= 180) return 'S ';
+    if (deg <= 225) return 'S0 ';
+    if (deg <= 270) return 'O ';
+    if (deg <= 325) return 'N0 ';
 }
