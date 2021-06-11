29acabafd8f1b01bd3c774ef1686709196e9ddc5
aquistapace <59019615+aquistapace@users.noreply.github.com>
2021-06-11 11:06:59 -0300

    adding animation on mouse over temperatures

--- src/components/SecondarySectionWeather/styles.tsx
+++ src/components/SecondarySectionWeather/styles.tsx
@@ -11,6 +11,30 @@ export const Container = styled.div`
         > span {
             margin-right: 1rem;
             cursor: pointer;
+            transition: transform 0.1s;
+            -webkit-transition: transform 0.1s;
+            :hover {
+                -webkit-animation: pulse 0.25s;
+                animation: pulse 0.25s;
+                -webkit-transform: scale(1.1);
+                -ms-transform: scale(1.1);
+                -moz-transform: scale(1.1);
+                transform: scale(1.1);
+            }
+            @keyframes pulse {
+                0% {
+                    -webkit-transform: scale(1);
+                    -ms-transform: scale(1);
+                    -moz-transform: scale(1);
+                    transform: scale(1);
+                }
+                100% {
+                    -webkit-transform: scale(1.1);
+                    -ms-transform: scale(1.1);
+                    -moz-transform: scale(1.1);
+                    transform: scale(1.1);
+                }
+            }
         }
         > img {
             width: 40px;
--- src/components/WeatherForecastCard/styles.tsx
+++ src/components/WeatherForecastCard/styles.tsx
@@ -40,6 +40,30 @@ export const TodayInfo = styled.div`
         font-size: 3.375rem;
         line-height: 3.5rem;
         cursor: pointer;
+        transition: transform 0.1s;
+        -webkit-transition: transform 0.1s;
+        :hover {
+            -webkit-animation: pulse 0.25s;
+            animation: pulse 0.25s;
+            -webkit-transform: scale(1.1);
+            -ms-transform: scale(1.1);
+            -moz-transform: scale(1.1);
+            transform: scale(1.1);
+        }
+        @keyframes pulse {
+            0% {
+                -webkit-transform: scale(1);
+                -ms-transform: scale(1);
+                -moz-transform: scale(1);
+                transform: scale(1);
+            }
+            100% {
+                -webkit-transform: scale(1.1);
+                -ms-transform: scale(1.1);
+                -moz-transform: scale(1.1);
+                transform: scale(1.1);
+            }
+        }
     }
     > span {
         font-size: 1.5rem;
