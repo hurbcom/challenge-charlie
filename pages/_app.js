aa23c52fbdef0029e0a5680e52b4e19be6233b38
Matheus Assed <matheusassed@gmail.com>
2021-09-11 21:10:02 -0300

    estilos basicos e localização

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,46 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+  body {
+    font-family: sans-serif;
+    background-color: #FFF;
+  }
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
   }
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    boxBlue: '#006E90DE',
+    boxYellow: '#E9C46ADE',
+    boxRed: '#E76F51DE',
+    backgroundBlue: '#006E9080',
+    backgroundYellow: '#E9C46A80',
+    backgroundRed: '#E76F5180',
+    text: '#535353',
+    boxText: '#FFF'
   },
+  measures: {
+    radius: '4px',
+    tablet: '768px',
+    phone: '460px',
+    marginIn: '16px',
+    marginOut: '20px',
+    padding: '20px 20px 30px'
+  }
 }
 
 export default function App({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,63 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const FakeBody = styled.div`
+  width: 100vh;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  const TOKENS = {
+    location: 'c63386b4f77e46de817bdf94f552cddf'
+  }
+  const [loading, setLoading] = useState(true);
+  const [coords, setCoords] = useState({latitude: '', longitude: ''});
+  const [navigatorLocationAvailability, setNavigatorLocationAvailability] = useState(false);
+  const [city, setCity] = useState('');
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      setNavigatorLocationAvailability(true);
+      
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCoords({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      }, (error) => {
+        setNavigatorLocationAvailability(false);
+        console.error(error);
+      });
+    } else {
+      setNavigatorLocationAvailability(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if(!!coords.longitude && !!coords.latitude){
+      fetch(`https://api.opencagedata.com/geocode/v1/json?q=${coords.latitude}+${coords.longitude}&key=${TOKENS.location}`)
+      .then(serverResponse => serverResponse.json())
+      .then((response) => {
+        setCity(response.results[0].components.city);
+      });
+
+      setLoading(false);
+    }
+  }, [coords])
+  
+  return(
+    <FakeBody>
+      {
+        !loading &&
+        !!coords &&
+        !!navigatorLocationAvailability &&
+        !!city &&
+        <>
+          <p>{city}</p>
+        </>
+      }
+    </FakeBody>
+  )
 }
