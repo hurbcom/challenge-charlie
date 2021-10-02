12831507a74f6a93cfbb03eaca33e4d99aef8d71
francisco <francisco@cuponeria.com.br>
2021-10-02 16:34:15 -0300

    limpando alguns arquivos e adicionando styled-components e react-icons

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body{
+    height: 100%;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
