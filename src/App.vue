73f32959a6e8d4378b156a441fbe4b04ea394afa
Tiago Urbano <tiago.urbano@oracle.com>
2021-11-19 09:42:47 -0300

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper-app">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        color: blue;
+    }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.vue$/, use: 'vue-loader' },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          publicPath: 'assets',
+        },
+      },
+      { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] },
+      { test: /\.scss$/, use: ['vue-style-loader', 'css-loader', 'sass-loader'] }
+    ]
+  },
+  devServer: {
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+};
