93362291279ba25a077076a925a2deb2c85c472e
Tiago Urbano <tiago.urbano@oracle.com>
2021-11-20 09:30:19 -0300

    Adds the first structure and L&F

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,46 @@
 <template>
   <div class="wrapper-app">
-    <h1>Hello World!</h1>
+    <div class="container d-flex flex-column">
+        <div class="wrapper-form-field d-flex">
+            <span class="icon" data-icon="("></span>
+            <input type="text">
+        </div>
 
-    <span class="icon" data-icon="A"></span>
+        <div class="wrapper-weather d-flex flex-column">
+            <div class="d-flex today-weather">
+                <div class="icon icon-weather">
+                    <span class="icon" data-icon="B"></span>
+                </div>
+
+                <div class="details-weather d-flex flex-column">
+                    <p class="date">Hoje</p>
+                    <p class="temperature">32º</p>
+
+                    <p class="state">Ensolarado</p>
+
+                    <p class="wind">Vento: NO 6.4km/h</p>
+                    <p class="humidty">Humidade: 78%</p>
+                    <p class="pressure">Pressão: 1003hPa</p>
+                </div>
+            </div>
+
+            <div class="d-flex tomorrow-weather">
+                <div class="icon icon-weather"></div>
+                <div class="details-weather d-flex flex-column">
+                    <p class="date">Amanhã</p>
+                    <p class="temperature">25º</p>
+                </div>
+            </div>
+
+            <div class="d-flex after-tomorrow-weather">
+                <div class="icon icon-weather"></div>
+                <div class="details-weather d-flex flex-column">
+                    <p class="date">Depois de Amanhã</p>
+                    <p class="temperature">22º</p>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -14,7 +52,100 @@ export default {};
     @import url('./fonts/stylesheet.css');
     @import url('./sass/icons.scss');
 
-    h1 {
-        color: blue;
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    html, body {
+        background-color: #cdcdcd;
+    }
+
+    .d-flex {
+        display: flex;
+
+        &.flex-column {
+            flex-direction: column;
+        }
+    }
+
+    .wrapper-app {
+        .container {
+            height: 100vh;
+            width: 50vw;
+            min-width: 505px;
+            margin: 0 auto;
+            background-color: #fff;
+
+            .wrapper-form-field {
+                padding: 10px;
+                height: 15vh;
+                align-items: center;
+
+                .icon {
+                    color: #7d7978;
+                }
+
+                input {
+                    margin-left: 20px;
+                    width: 100%;
+                    border: none;
+                    color: #7d7978;
+                    font-weight: 500;
+                    font-size: 2rem;
+                    outline: none;
+                }
+            }
+
+            .wrapper-weather {
+                > div {
+                    padding: 10px 0 20px 0;
+                    height: 20vh;
+
+                    &:first-child {
+                        height: 45vh;
+                    }
+                }
+
+                .icon-weather {
+                    width: 60%;
+                    text-align: center;
+
+                    .icon::before {
+                        font-size: 15rem;
+                        color: #fff;
+                    }
+                }
+
+                .details-weather {
+                    p {
+                        color: #fff;
+                        font-size: 1.3rem;
+                        font-weight: 400;
+
+                        &.date {
+                            text-transform: uppercase;
+                        }
+
+                        &.state {
+                            padding: 15px 0 20px;
+                        }
+
+                        &.wind,
+                        &.humidty,
+                        &.pressure {
+                            font-size: 1rem;
+                        }
+                    }
+                }
+
+                .today-weather { background-color:rgb(212, 198, 0); }
+                .tomorrow-weather { background-color:rgb(235, 223, 59); }
+                .after-tomorrow-weather { background-color:rgb(165, 156, 26); }
+            }
+
+        }
     }
 </style>
