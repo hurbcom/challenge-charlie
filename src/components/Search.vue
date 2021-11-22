c16a4299401002c447d9de2f490086022aa2c48a
Tiago Urbano <tiago.urbano@oracle.com>
2021-11-22 13:18:59 -0300

    Allows the user share your lat and long, and convert this lat long to location

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,13 +1,40 @@
 <template>
     <div class="wrapper-form-field d-flex">
         <span class="icon" data-icon="("></span>
-        <input type="text">
+        <input type="text" v-model="userLocation" />
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Search'
+    name: 'Search',
+    data: () => ({
+        userLocation: '',
+    }),
+    methods: {
+        getUserCurrentLatLong() {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    this.convertLatLongInLocation(position.coords.latitude, position.coords.longitude);
+                },
+                error => {
+                    console.log(error.message);
+                },
+            )
+        },
+        async convertLatLongInLocation(lat, long) {
+            const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat},${long}&key=c63386b4f77e46de817bdf94f552cddf&language=en`;
+            const { data: { results } } = await axios.get(url);
+            const [result] = results;
+            const { components: location } = result;
+            this.userLocation = `${location.village}, ${location.state} - ${location.country}`;
+        },
+    },
+    created() {
+        this.getUserCurrentLatLong();
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import 'regenerator-runtime/runtime';
 import Vue from 'vue';
 import App from './App.vue';
 
