9b3202027d7ca2324bd51cefb7f22bf5bbbdef67
Pamela Santos <pamesantoss@hotmail.com>
2021-11-28 20:00:43 -0300

    feat: iniciando estrutura da aplicação + importando ícones

--- hurb-challenge/src/App.js
+++ hurb-challenge/src/App.js
@@ -1,6 +1,62 @@
 function App() {
   return (
-    <h1>Hello Hurb!</h1>
+    <div className="weatherWallpaper">
+      <div className="weatherApp">
+        <div className="weatherField">
+          <input
+            type="text"
+            name="weatherLocation"
+            className="weatherFieldBox"
+            value="Rio de Janeiro, Rio de Janeiro"
+          />
+        </div>
+
+        <div className="weatherInfo">
+          <ul>
+            <li className="weatherInfoToday">
+              <div className="icon">
+                <i class="wi wi-day-sunny"></i>
+              </div>
+              <div className="weatherInfoData">
+                <p>
+                  Hoje <span>32ºC</span>
+                </p>
+                <p className="spotlight">
+                  Ensolarado
+                </p>
+                <p className="details">
+                  Vento: NO 6.4km/h<br />
+                  Humidade: 78%<br />
+                  Pressão: 1003hPA
+                </p>
+              </div>
+            </li>
+
+            <li className="weatherInfoTomorrow">
+              <div className="icon">
+                <i class="wi wi-day-sunny"></i>
+              </div>
+              <div className="weatherInfoData">
+                <p>
+                  Amanhã <span>25ºC</span>
+                </p>
+              </div>
+            </li>
+
+            <li className="weatherInfoAfter">
+              <div className="icon">
+                <i class="wi wi-day-sunny"></i>
+              </div>
+              <div className="weatherInfoData">
+                <p>
+                  Depois de amanhã <span>22ºC</span>
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
