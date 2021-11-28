e96285f871af5b4cba8c661c348391aaf0445e99
Pamela Santos <pamesantoss@hotmail.com>
2021-11-28 18:56:22 -0300

    feat: adicionando sass e configurações iniciais

--- hurb-challenge/src/index.js
+++ hurb-challenge/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style/App.scss';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('app')
 );
