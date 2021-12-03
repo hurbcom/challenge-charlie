473d2cba2a638379ef54dd46aa70ca4b2a1bc14e
Paulo Gabriel <paulogabriel.dev@gmail.com>
2021-12-03 13:01:25 -0300

    Add and apply prettier to api

--- api/index.js
+++ api/index.js
@@ -1,26 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
 var express = require('express');
 var cors = require('cors');
 var axios = require('axios');
 var app = express();
 app.use(cors());
 
+var { BING_BASE_URL, BING_WALLPAPER_API_URL, API_PORT } = process.env;
+
 app.get('/background', function (req, res) {
-  var url = process.env.BING_BASE_URL + process.env.BING_WALLPAPER_API_URL;
-  axios.get(url).then(
-    function (response) {
-      return res.send(
-        process.env.BING_BASE_URL + response.data.images[0].url
-      );
-    }).catch(function (err) {
+  var url = BING_BASE_URL + BING_WALLPAPER_API_URL;
+  axios
+    .get(url)
+    .then(function (response) {
+      var backgroundImageUrl = BING_BASE_URL + response.data.images[0].url;
+      return res.send(backgroundImageUrl);
+    })
+    .catch(function (err) {
       return res.send({
         status: '500',
-        message: error
+        message: error,
       });
-    }
-  );
+    });
 });
 
-app.listen(process.env.API_PORT, function() {
-  console.log("Server is running on localhost:" + process.env.API_PORT);
+app.listen(API_PORT, function () {
+  console.log('Server is running on localhost:' + API_PORT);
 });
