bd7d39e4c2b248f7633e6bcbdc07e54eae10530d
Paulo Gabriel <paulogabriel.dev@gmail.com>
2021-12-03 01:54:50 -0300

    Remove unused files and format

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,13 +1,18 @@
+import styled from 'styled-components/macro';
 import React, { useState, useEffect } from 'react';
-import axios from "axios";
-import '../Home.css';
+import axios from 'axios';
 
+const { API_BASE_URL } = process.env;
 
-const API_BASE_URL='http://localhost:3001'
+const HomePage = styled.div`
+  background-image: url(${(props) => props.backgroundUrl});
+  background-size: cover;
+  height: 100vh;
+`;
 
 function Home() {
-  const [city, setCity] = useState("");
-  const [backgroundUrl, setBackgroundUrl] = useState("");
+  const [city, setCity] = useState('');
+  const [backgroundUrl, setBackgroundUrl] = useState('');
   const onChangeCity = ({ target: { value: city } }) => {
     setCity(city);
   };
@@ -19,25 +24,29 @@ function Home() {
   const headers = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type'
+    'Access-Control-Allow-Headers': 'Content-Type',
   };
 
   useEffect(() => {
-    axios.get(API_BASE_URL + '/background', {headers, mode: 'cors'}).then(
-      (res) => setBackgroundUrl(res.data)
-    );
+    axios
+      .get(API_BASE_URL + '/background', { headers, mode: 'cors' })
+      .then((res) => setBackgroundUrl(res.data));
   }, []);
 
   return (
-    <div className="Home" style={{backgroundImage: `url(${backgroundUrl})`}}>
-      <header className="Home-header">
-      </header>
-      <div className="Home-body">
-        <input name="city" type="text" value={city} onChange={onChangeCity} onSubmit={onSubmitCity} />
+    <HomePage className='Home' backgroundUrl={backgroundUrl}>
+      <header className='Home-header'></header>
+      <div className='Home-body'>
+        <input
+          name='city'
+          type='text'
+          value={city}
+          onChange={onChangeCity}
+          onSubmit={onSubmitCity}
+        />
       </div>
-      <footer className="Home-footer">
-      </footer>
-    </div>
+      <footer className='Home-footer'></footer>
+    </HomePage>
   );
 }
 
