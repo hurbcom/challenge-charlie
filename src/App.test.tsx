27fad73ddcedbf57cfae7fc7ee307a13f7f859c4
Diego Varela <diego.varela@corp.globo.com>
2022-02-07 12:32:20 -0300

    feat(CRA): removing some CRA files / content

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,4 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
+test('', () => {})
