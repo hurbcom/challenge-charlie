26aca4ba9ae9eab9aa64be895bcf518cda9945e1
Diego Varela <diego.varela@corp.globo.com>
2022-02-07 13:54:19 -0300

    feat(index): import WeatherForecast at index

--- src/App.test.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('', () => {})
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import WeatherForecast from './containers/WeatherForecast'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WeatherForecast />
   </React.StrictMode>,
   document.getElementById('root')
 )
