e0dd7455f71ffef3aec5ad2668af1b3b08fb9c62
almeigab <gabrielalmeida140395@gmail.com>
2022-05-06 03:12:59 -0300

    feat: event handler for search and location buttons

--- src/components/locationInput/index.js
+++ src/components/locationInput/index.js
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react';
 import './styles.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import locationService from '../../services/locationService';
-import { update } from '../../store/slices/locationSlice';
+import weatherService from '../../services/weatherService';
+import { update as updateLocation } from '../../store/slices/locationSlice';
+import { update as updateWeather } from '../../store/slices/weatherSlice';
 
 function LocationInput() {
     const location = useSelector((state) => state.location.value);
@@ -12,7 +14,7 @@ function LocationInput() {
         navigator.geolocation.getCurrentPosition((position) => {
             locationService.fetchLocation(position.coords.latitude, position.coords.longitude)
                 .then((locationString) => {
-                    dispatch(update(locationString));
+                    dispatch(updateLocation(locationString));
                 })
                 // TODO: error treatment
                 .catch((error) => console.log(error));
@@ -24,20 +26,34 @@ function LocationInput() {
 
     useEffect(getCurrentPosition, []);
 
-    function onInputChange(event) {
-        dispatch(update(event.target.value));
+    function handleInput(event) {
+        dispatch(updateLocation(event.target.value));
+    }
+
+    function handleClickSearch() {
+        weatherService.fetchWeather(location)
+            .then((result) => locationService
+                .fetchLocation(result.coord.lat, result.coord.lon)
+                .then((locationString) => {
+                    dispatch(updateLocation(locationString));
+                    dispatch(updateWeather(result));
+                }));
+    }
+
+    function handleClickLocation() {
+        getCurrentPosition();
     }
 
     return (
         <div id="location-input">
-            <input name="search" placeholder="Search for a city..." value={location} onChange={onInputChange} />
-            <button type="button">
+            <input name="search" placeholder="Search for a city..." value={location} onChange={handleInput} />
+            <button type="button" onClick={handleClickSearch}>
                 <span className="material-symbols-outlined">
                     search
                 </span>
             </button>
             <hr />
-            <button type="button">
+            <button type="button" onClick={handleClickLocation}>
                 <span className="material-symbols-outlined">
                     my_location
                 </span>
