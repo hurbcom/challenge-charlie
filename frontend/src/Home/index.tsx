b662d368556f2e739b199402d7f2745ab9064bd8
joaomarccelino <joao.marcelino.esc@gmail.com>
2022-05-29 19:40:47 -0300

    style: added all page styling

--- frontend/src/Home/index.tsx
+++ frontend/src/Home/index.tsx
@@ -18,23 +18,38 @@ type Coordinates = {
   latitude: number;
   longitude: number;
 }
-
-type City = {
-  name: string;
-  state: string;
-}
 export function Home() {
   const [weatherCondition, setWeatherCondition] = useState();
-  const [unitsBr, setUnitsBr] = useState(
+  const [units, setUnits] = useState(
     {
       unitForm: 'metric',
       metricLetter: '°C',
       speed: 'km/h'
     }
-  )
+  );
+  function convertTemperature(temp: number[], unit: string) {
+    if (unit === '°C') {
+      setTodayWeather({ ...todayWeather, temperature: ((temp[0]) * 1.8 + 32) });
+      setNextWeather({
+        tomorrow: (temp[1]) * 1.8 + 32,
+        afterTomorrow: (temp[2]) * 1.8 + 32
+      })
+      setUnits({ ...units, metricLetter: '°F' });
+    } else {
+      setTodayWeather({ ...todayWeather, temperature: ((temp[0] - 32) / 1.8) });
+      setNextWeather({
+        tomorrow: (temp[1] - 32) / 1.8,
+        afterTomorrow: (temp[2] - 32) / 1.8
+      })
+      setUnits({ ...units, metricLetter: '°C' });
+    }
+  }
+
   const [inputValue, setInputValue] = useState('');
   const [todayWeather, setTodayWeather] = useState({} as TodayWeather);
   const [nextWeather, setNextWeather] = useState({} as NextWeather)
+
+  const convertTemperatureArray = [todayWeather.temperature, nextWeather.tomorrow, nextWeather.afterTomorrow]
 
   const skyWeather = {
     Clear: 'Ensolarado',
@@ -78,7 +93,7 @@ export function Home() {
 
 
   async function getWeather(cityName: string) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unitsBr.unitForm}&appid=772920597e4ec8f00de8d376dfb3f094`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${units.unitForm}&appid=772920597e4ec8f00de8d376dfb3f094`)
       .then(res => res.json())
       .then(res => {
         setWeatherCondition(res.weather[0].main);
@@ -96,7 +111,7 @@ export function Home() {
 
 
   async function getNextWeather(wId: number) {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${wId}&units=${unitsBr.unitForm}&appid=772920597e4ec8f00de8d376dfb3f094&cnt=2`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${wId}&units=${units.unitForm}&appid=772920597e4ec8f00de8d376dfb3f094&cnt=2`)
       .then(res => res.json())
       .then(res => {
         const nextWeatherData = {
@@ -105,6 +120,7 @@ export function Home() {
         }
         setNextWeather(nextWeatherData);
       });
+      
   }
 
   async function getWeatherByInput() {
@@ -117,17 +133,42 @@ export function Home() {
   }
   useEffect(() => {
     getCoordinates()
-    }, [])
+  }, []);
+  useEffect(() => {
+    getColor(todayWeather.temperature);
+  }, [inputValue, todayWeather.temperature]);
+
+  function getColor(temp: number) {
+    if (inputValue === '') {
+      document.documentElement.classList.add('grey');
+      document.documentElement.classList.remove('blue');
+      document.documentElement.classList.remove('red');
+    } else if (temp > 35) {
+      document.documentElement.classList.add('red');
+      document.documentElement.classList.remove('blue');
+      document.documentElement.classList.remove('grey');
+    } else if (temp < 15) {
+      document.documentElement.classList.add('blue');
+      document.documentElement.classList.remove('red');
+      document.documentElement.classList.remove('grey');
+    } else {
+      document.documentElement.classList.remove('blue');
+      document.documentElement.classList.remove('red');
+      document.documentElement.classList.remove('grey');
+    }
+  }
 
   return (
-    <main>
+    <main className="container">
       <div className="input-area">
         <button
-          onClick={() => {getWeatherByInput()}}
+          className="btn"
+          onClick={() => { getWeatherByInput() }}
         >
           <img src={Compass} alt="Pesquisar" />
-          </button>
+        </button>
         <input
+          className="weather-input"
           type="text"
           value={inputValue}
           placeholder={"Cidade"}
@@ -140,20 +181,30 @@ export function Home() {
         }
         <div className="today-info">
           <h2>Hoje</h2>
-          <p>{`${todayWeather?.temperature} ${unitsBr.metricLetter}`}</p>
+          <button className="btn" onClick={() => convertTemperature(convertTemperatureArray, units.metricLetter)}>
+            {`${Math.round(todayWeather?.temperature)} ${units.metricLetter}`}
+          </button>
           <h3>{weatherCondition && skyWeather[weatherCondition]}</h3>
-          <p>{`Vento: ${todayWeather?.wind} ${unitsBr.speed}`}</p>
+          <p>{`Vento: ${todayWeather?.wind} ${units.speed}`}</p>
           <p>{`Humidade: ${todayWeather.humidity}%`}</p>
           <p>{`Pressão: ${todayWeather?.pressure}hPA`}</p>
         </div>
       </div>
       <div className="tomorrow">
-        <h2>Amanhã</h2>
-        <p>{`${nextWeather?.tomorrow} ${unitsBr.metricLetter}`}</p>
+        <div className="content">
+          <h2>Amanhã</h2>
+          <button className="btn" onClick={() => convertTemperature(convertTemperatureArray, units.metricLetter)}>
+            {`${Math.round(nextWeather?.tomorrow)} ${units.metricLetter}`}
+          </button>
+        </div>
       </div>
       <div className="after-tomorrow">
-        <h2>Depois de Amanhã</h2>
-        <p>{`${nextWeather?.afterTomorrow} ${unitsBr.metricLetter}`}</p>
+        <div className="content">
+          <h2>Depois de Amanhã</h2>
+          <button className="btn" onClick={() => convertTemperature(convertTemperatureArray, units.metricLetter)}>
+            {`${Math.round(nextWeather?.afterTomorrow)} ${units.metricLetter}`}
+          </button>
+        </div>
       </div>
     </main>
 
