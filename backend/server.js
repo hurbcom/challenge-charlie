9a852e684a58953b06ffc71707e8fbaffd8f88d0
joaomarccelino <joao.marcelino.esc@gmail.com>
2022-05-29 11:51:42 -0300

    feat: create-react-app & backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+let cors = require('cors');
+
+const PORT = 3003;
+const HOST = '0.0.0.0'
+
+app.use(cors());
+const url ='https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR';
+
+app.get('/image', async (req, res)=>{
+  request('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR', function (error, response, body) {
+      res.status(200).send(body)
+  });
+})
+
+app.listen(PORT, HOST)
