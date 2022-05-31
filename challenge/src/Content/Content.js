a77ec16e8f67f6e6f586094c60d67a39c62d6cd7
JPougano <pedroougano@gmail.com>
2022-05-31 00:56:30 -0300

    feat: Condicionando o tema da aplicação de acordo com a temperatura de hoje

--- challenge/src/Content/Content.js
+++ challenge/src/Content/Content.js
@@ -1,11 +1,18 @@
 import React from "react";
 import './style.css'
 import Icon from '../download.png'
+import WeatherTheme from '../WeatherTheme'
 
 export default function Content (props){
+
+// Condicionando tema de acordo com a temperatura da localidade
+const defWeather = props.temp < 15 ? WeatherTheme.coldSWeather
+                    : props.temp > 35 ? WeatherTheme.warmWeather
+                    : WeatherTheme.defaultWeather;
+
     return (
         <section className="content__container">
-            <div className="forecast__subcontainer">
+            <div className="forecast__subcontainer" style={defWeather[0]}>
                 <div className="forecast__icon"> 
                     <img src={Icon}/>
                 </div>
@@ -22,13 +29,13 @@ export default function Content (props){
                     </ul>
                 </div>
             </div>
-            <div className="forecast__subcontainer">
+            <div className="forecast__subcontainer" style={defWeather[0]}>
                 <div className="forecast__data">
                     <h3>AMANHÃ</h3>
                     <p>25°C</p>
                 </div>
             </div>
-            <div className="forecast__subcontainer">
+            <div className="forecast__subcontainer" style={defWeather[0]}>
                 <div className="forecast__data">
                     <h3>DEPOIS DE AMANHÃ</h3>
                     <p>25°C</p>
---
+++ challenge/src/WeatherTheme.js
@@ -0,0 +1,15 @@
+const WeatherSetting = {
+    coldSWeather:[
+        {background: 'linear-gradient(to bottom, rgb(7, 213, 240), rgb(2, 135, 153))'}, //"today"
+    ],
+    
+    defaultWeather:[
+        {background: 'linear-gradient(to bottom, rgb(214, 214, 88),  rgb(240, 205, 7))'}, //"today"
+    ],
+    
+    warmWeather:[
+        {background: 'linear-gradient(to bottom, rgb(238, 121, 67), rgb(207, 92, 39))'}, //"today"
+    ]
+}
+
+export default WeatherSetting;
