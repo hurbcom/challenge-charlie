bb27ae5b1e176f5a2cbabab04d2dfa2d266fd6e0
JPougano <pedroougano@gmail.com>
2022-05-31 00:48:12 -0300

    feat: Populando componente Search com dados da localização do usuário

--- challenge/src/Search/Search.js
+++ challenge/src/Search/Search.js
@@ -3,10 +3,15 @@ import './style.css'
 import Bussola from '../download (1).png'
 
 export default function Search (props){
+
+const locationRecieved= props.curCity && props.curState ? 
+                        props.curCity + ', ' + props.curState:
+                        'Carregando..';
+
     return (
         <section className="search__container">
             <img className="search__icon" src={Bussola}/>
-            <input type="text" placeholder="Rio de Janeiro, Rio de Janeiro"/>
+            <input type="text" placeholder={locationRecieved}/>
         </section>
     )
 }
