0f728e1a09865af1fac627cf16bb4dd9fce941e8
JPougano <pedroougano@gmail.com>
2022-05-30 23:52:30 -0300

    feat: Consumindo API da bing para gerar imagem de fundo automática

--- challenge/src/App.js
+++ challenge/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 
 import Search from './Search/Search';
 import Content from './Content/Content';
@@ -6,12 +6,32 @@ import Content from './Content/Content';
 
 export default function App (){
 
-  const BackgroundImage = {
-    backgroundImage: `url(https://bing.com/th?id=OHR.PurnululuNP_PT-BR3234973741_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp)`
+// Área dos states
+const [ bingWallpaper, setBingWallpaper] = useState();
+
+  // Pegando o papel de parede do bing (alo alo CORS!)
+useEffect(() => {
+  let getWallpaper = async () => {
+    const allOriginsUrl = 'http://api.allorigins.win/get?url=';
+    const bingUrl = 'https://www.bing.com';
+    const fullUrl = allOriginsUrl + encodeURIComponent(bingUrl + '/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR');
+  
+    try{
+        fetch(fullUrl)
+            .then(res => res.json())
+            .then(data => JSON.parse(data.contents))
+            .then(info => setBingWallpaper(bingUrl + info.images[0].url));
+    }catch{
+        throw new Error('Not able to load wallpaper');
+    }
   }
 
+  getWallpaper();
+  
+}, []) 
+
   return (
-    <div className="container" style={BackgroundImage}>
+    <div className="container" style={{backgroundImage: `url(${bingWallpaper})`}}>
       <div className="main__container">
           <Search />
           <Content />
