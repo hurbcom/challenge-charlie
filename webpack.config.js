b0b1ed4dd12d8b75c07c011f5b807c1ebcaae298
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-08 17:35:50 -0300

    Configurando plugin de reenderização do html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -7,5 +8,12 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Challenge Chalie - Lucas Monteiro', 
+            template: path.resolve(__dirname, './src/html/template.html'), 
+            filename: 'index.html', 
+        }),
+    ]
 }
