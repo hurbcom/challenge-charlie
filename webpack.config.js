5662258531753fa17534f037a4b12e85b6c10a17
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-08 17:32:36 -0300

    Configurando ponto de saída do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,9 @@ const path = require('path')
 module.exports = {
     entry: {
         main: path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
     }
 }
