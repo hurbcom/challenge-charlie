2f2eb10ecbeb352c520a260f31140aba5ed671da
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-08 17:44:17 -0300

    Configurando uso de svg no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
             {
                 test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
                 type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
             }
         ],
     }
