a869c8fe3594946e9a3bbbd338b3eb3818607293
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-10 13:30:16 -0300

    Criando método de conversão de temperatura

--- src/components/modal-body-today/index.jsx
+++ src/components/modal-body-today/index.jsx
@@ -3,6 +3,10 @@ import React from "react"
 import './style.scss'
 const ModalBodyToday = props => {
     
+    const celsiusToFahrenheit = (celsiusTemp) => {
+        return (celsiusTemp * 9 / 5 + 32).toFixed(2)
+
+    }
 
     return (
         <div className="modal-body-container">
