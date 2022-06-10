66df14dd61c408bba1ee35cca94ca1ca706012d6
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-10 18:41:06 -0300

    Criando método de alteração de escala de temperatura e corpo do modal

--- src/components/modal-body-today/index.jsx
+++ src/components/modal-body-today/index.jsx
@@ -6,17 +6,21 @@ const ModalBodyToday = props => {
 
     const [celsiusTemp, setCelsiusTemp] = useState('')
     const [fahrenheitTemp, setFahrenheitTemp] = useState('')
- 
+    const [isCelsiusTemp, setIsCelsiusTemp] = useState(true)
 
     const celsiusToFahrenheit = (celsiusTemp) => {
-        return (celsiusTemp * 9 / 5 + 32).toFixed(2)
+        return (celsiusTemp * 9 / 5 + 32).toFixed(0)
 
+    }
+
+    const changeCurrentTempScale = () => {
+        setIsCelsiusTemp(!isCelsiusTemp)
     }
 
     useEffect(() => {
 
         if (data.main) {
-            const celsius = data.main.temp
+            const celsius = data.main.temp.toFixed(0)
             const fahrenheit = celsiusToFahrenheit(celsius)
             setFahrenheitTemp(fahrenheit)
             setCelsiusTemp(celsius)
@@ -25,8 +29,41 @@ const ModalBodyToday = props => {
     })
 
     return (
-        <div className="modal-body-container">
-            
+        <div className="modal-body-today-container">
+            {
+                data.main ?
+                    <div className="modal-body-today-content">
+                        <div className="modal-body-today-temp-icon">
+                            <a data-icon="B" className="modal-body-today-sun-status"></a>
+                        </div>
+                        <div className="modal-body-today-weather-infos">
+                            <div className="modal-body-today-temp-status" onClick={changeCurrentTempScale}>
+                                <div className="modal-body-today-temp-title">
+                                    <span>HOJE</span>
+                                </div>
+                                <div className="modal-body-today-temp-value">
+                                    <span>{isCelsiusTemp ? celsiusTemp : fahrenheitTemp}</span>
+                                    <span>{isCelsiusTemp ? 'ºC' : 'ºF'}</span>
+                                </div>
+                            </div>
+                            <div className="modal-body-today-sky-status">
+                                <div className="modal-body-today-sky-title">
+                                    <span>Ensolarado</span>
+                                </div>
+                                <div className="modal-body-today-sky-wind">
+                                    <span>Vento: NO 6.4 km/h</span>
+                                </div>
+                                <div className="modal-body-today-sky-hum">
+                                    <span>Humidade: 78%</span>
+                                </div>
+                                <div className="modal-body-today-sky-press">
+                                    <span>Pressão: 1003hPA</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    : null
+            }
         </div>
     )
 }
