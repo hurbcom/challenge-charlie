7a5a104ac06f0b9fd2163223898f1f028f3e027c
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-10 13:32:19 -0300

    Convertendo temperaturas e salvando na state

--- src/components/modal-body-today/index.jsx
+++ src/components/modal-body-today/index.jsx
@@ -1,12 +1,28 @@
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import './style.scss'
 const ModalBodyToday = props => {
-    
+    const { data } = props
+
+    const [celsiusTemp, setCelsiusTemp] = useState('')
+    const [fahrenheitTemp, setFahrenheitTemp] = useState('')
+ 
+
     const celsiusToFahrenheit = (celsiusTemp) => {
         return (celsiusTemp * 9 / 5 + 32).toFixed(2)
 
     }
+
+    useEffect(() => {
+
+        if (data.main) {
+            const celsius = data.main.temp
+            const fahrenheit = celsiusToFahrenheit(celsius)
+            setFahrenheitTemp(fahrenheit)
+            setCelsiusTemp(celsius)
+
+        }
+    })
 
     return (
         <div className="modal-body-container">
