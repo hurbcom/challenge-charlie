5bc3a89b0664f99c6f3eda9cbe72252d06d4af2c
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-10 11:46:50 -0300

    Alterando webpack.config.js para receber variáveis de ambiente

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,61 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-    mode: 'development',
-    devServer: {
-        historyApiFallback: true,
-        static: {
-            directory: path.join(__dirname, "dist")
+module.exports = env => {
+    return {
+        mode: env.file === 'prod' ? 'production' : 'development',
+        devServer: {
+            historyApiFallback: true,
+            static: {
+                directory: path.join(__dirname, "dist")
+            },
+            open: true,
+            compress: true,
+            hot: true,
+            port: 3005,
         },
-        open: true,
-        compress: true,
-        hot: true,
-        port: 3005,
-    },
-    entry: {
-        main: path.resolve(__dirname, './src/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Challenge Chalie - Lucas Monteiro',
-            template: path.resolve(__dirname, './src/html/template.html'),
-            filename: 'index.html',
-        }),
-        new CleanWebpackPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/inline',
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            }
+        entry: {
+            main: path.resolve(__dirname, './src/index.js')
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: '[name].bundle.js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Challenge Chalie - Lucas Monteiro',
+                template: path.resolve(__dirname, './src/html/template.html'),
+                filename: 'index.html',
+            }),
+            new CleanWebpackPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new Dotenv({
+                path: path.resolve(__dirname, `src/environments/.env${env.file ? `.${env.file}` : ''}`)
+            })
         ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$|jsx/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                    type: 'asset/inline',
+                },
+                {
+                    test: /\.(scss|css)$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                }
+            ],
+        }
+
     }
 }
