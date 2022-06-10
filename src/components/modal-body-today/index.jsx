7579926d93c5a38f0d50744cf2547644d450e666
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-10 19:45:33 -0300

    Criando método para conversão de velocidade do vento

--- src/components/modal-body-today/index.jsx
+++ src/components/modal-body-today/index.jsx
@@ -12,7 +12,11 @@ const ModalBodyToday = props => {
     const [isCelsiusTemp, setIsCelsiusTemp] = useState(true)
     const [weatherDescription, setWeatherDescription] = useState('')
     const [humidity, setHumidity] = useState('')
-    const [windPosition, setWindPosition] = useState('')
+    const [windDirection, setWindDirection] = useState('')
+    const [windSpeedInKilometers, setWindSpeedInKilometers] = useState('')
+    const [windSpeedInMiles, setWindSpeedInMiles] = useState('')
+    const [isSpeedInKilometers, setIsSpeedInKilometers] = useState(true)
+
     const celsiusToFahrenheit = (celsiusTemp) => {
         return (celsiusTemp * 9 / 5 + 32).toFixed(0)
 
@@ -22,11 +26,15 @@ const ModalBodyToday = props => {
         setIsCelsiusTemp(!isCelsiusTemp)
     }
 
+    const changeCurrentSpeedScale = () => {
+        setIsSpeedInKilometers(!isSpeedInKilometers)
+    }
+
     const windDegreeToCompassDirection = degree => {
         /**
          * Convert wind degree unit to compass direction
          */
-        const value = Number((degree/22.5)+.5)
+        const value = Number((degree / 22.5) + .5)
         const positions = [
             "N", "NNE", "NE", "ENE",
             "E", "ESE", "SE", "SSE",
@@ -38,10 +46,13 @@ const ModalBodyToday = props => {
         return position
     }
 
+    const windSpeedMetersToKilometers = ms => (ms * 3.6).toFixed(2)
+    const windSpeedMetersToMiles = ms => (ms * 2.237).toFixed(2)
+
     useEffect(() => {
 
         if (data.main) {
-            
+
             const celsius = data?.main?.temp?.toFixed(0)
             const fahrenheit = celsiusToFahrenheit(celsius)
             const descriptionCapitalized = capitalize(data?.weather[0]?.description || '')
@@ -51,47 +62,58 @@ const ModalBodyToday = props => {
             setCelsiusTemp(celsius)
             setWeatherDescription(descriptionCapitalized)
             setHumidity(data?.main?.humidity)
-            setWindPosition(windPositionConverted)
-            
+            setWindDirection(windPositionConverted)
+            setWindSpeedInKilometers(
+                windSpeedMetersToKilometers(data?.wind?.speed)
+            )
+            setWindSpeedInMiles(
+                windSpeedMetersToMiles(data?.wind?.speed)
+            )
         }
     })
 
     return (
         <div className="modal-body-today-container">
-            {
-                data.main ?
-                    <div className="modal-body-today-content">
-                        <div className="modal-body-today-temp-icon">
-                            <a data-icon="B" className="modal-body-today-sun-status"></a>
+            <div className="modal-body-today-content">
+                <div className="modal-body-today-temp-icon">
+                    <a data-icon="B" className="modal-body-today-sun-status"></a>
+                </div>
+                <div className="modal-body-today-weather-infos">
+                    <div className="modal-body-today-temp-status">
+                        <div className="modal-body-today-temp-title">
+                            <span>HOJE</span>
                         </div>
-                        <div className="modal-body-today-weather-infos">
-                            <div className="modal-body-today-temp-status" onClick={changeCurrentTempScale}>
-                                <div className="modal-body-today-temp-title">
-                                    <span>HOJE</span>
-                                </div>
-                                <div className="modal-body-today-temp-value">
-                                    <span>{isCelsiusTemp ? celsiusTemp : fahrenheitTemp}</span>
-                                    <span>{isCelsiusTemp ? 'ºC' : 'ºF'}</span>
-                                </div>
-                            </div>
-                            <div className="modal-body-today-sky-status">
-                                <div className="modal-body-today-sky-title">
-                                    <span>{weatherDescription || 'N/A'}</span>
-                                </div>
-                                <div className="modal-body-today-sky-wind">
-                                    <span>Vento: NO 6.4 km/h</span>
-                                </div>
-                                <div className="modal-body-today-sky-hum">
-                                    <span>Humidade: {humidity ? `${humidity}%` : 'N/A'}</span>
-                                </div>
-                                <div className="modal-body-today-sky-press">
-                                    <span>Pressão: 1003hPA</span>
-                                </div>
-                            </div>
+                        <div className="modal-body-today-temp-value" onClick={changeCurrentTempScale}>
+                            <span>{isCelsiusTemp ? celsiusTemp : fahrenheitTemp}</span>
+                            <span>{isCelsiusTemp ? 'ºC' : 'ºF'}</span>
                         </div>
                     </div>
-                    : null
-            }
+                    <div className="modal-body-today-sky-status">
+                        <div className="modal-body-today-sky-title">
+                            <span>{weatherDescription || 'N/A'}</span>
+                        </div>
+                        <div className="modal-body-today-sky-wind">
+                            <span onClick={changeCurrentSpeedScale}>
+                                Vento:
+                                {windDirection ? ` ${windDirection} ` : ' --'}
+                                {
+                                    windSpeedInKilometers && isSpeedInKilometers ?
+                                        `${windSpeedInKilometers} km/h`
+                                        : windSpeedInMiles && !isSpeedInKilometers ?
+                                            `${windSpeedInMiles} mph` :
+                                            '--'
+                                }
+                            </span>
+                        </div>
+                        <div className="modal-body-today-sky-hum">
+                            <span>Humidade: {humidity ? `${humidity}%` : 'N/A'}</span>
+                        </div>
+                        <div className="modal-body-today-sky-press">
+                            <span>Pressão: 1003hPA</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
