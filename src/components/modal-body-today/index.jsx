e4208fb15a95ee6cd82311b17483f419e49da75f
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-10 13:28:28 -0300

    Criando componente ModalBodyToday

---
+++ src/components/modal-body-today/index.jsx
@@ -0,0 +1,15 @@
+
+import React from "react"
+import './style.scss'
+const ModalBodyToday = props => {
+    
+
+    return (
+        <div className="modal-body-container">
+            
+        </div>
+    )
+}
+
+
+export default ModalBodyToday
