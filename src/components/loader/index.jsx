11fa96ed9724fbeea1fdc3e44c96569c8ffcef64
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-11 13:43:04 -0300

    Alterando classe do loader sm

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -5,7 +5,7 @@ const Loader = props => {
     const { sm, nm, lg, xlg } = props
     return (
         <div>
-            {sm ? <div className="loader sn"></div> : null}
+            {sm ? <div className="loader sm"></div> : null}
             {nm ? <div className="loader nm"></div> : null}
             {lg ? <div className="loader lg"></div> : null}
             {xlg ? <div className="loader xlg"></div> : null}
