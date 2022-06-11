715a5bb9cea4d68609dc8fff0922762094012fce
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-10 21:55:11 -0300

    Criando componente ModalBodyTomorrow

---
+++ src/components/modal-body-tomorrow/index.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+import './style.scss'
+const ModalBodyTomorrow = props => {
+
+
+
+    return (
+        <div className="modal-body-tomorrow-container">
+
+        </div>
+    )
+}
+
+export default ModalBodyTomorrow
