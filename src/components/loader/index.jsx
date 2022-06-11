91e6fd3379eb2df6407492503095bade9c0bff81
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-06-11 08:27:19 -0300

    Criando componente 'Loader'

---
+++ src/components/loader/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './style.scss'
+const Loader = props => {
+    const { sm, nm, lg, xlg } = props
+    return (
+        <div>
+            {sm ? <div className="loader sn"></div> : null}
+            {nm ? <div className="loader nm"></div> : null}
+            {lg ? <div className="loader lg"></div> : null}
+            {xlg ? <div className="loader xlg"></div> : null}
+
+        </div>
+    )
+}
+
+
+export default Loader
