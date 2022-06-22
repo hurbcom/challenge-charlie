a643f9960546dfff7d073453c54df8ee9d8dd686
Filipe André <filipeandres@hotmail.com>
2022-06-21 22:03:18 -0300

    feat: adding weather info UI

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LocationInput } from '../LocationInput';
+import { Weather } from '../Weather';
 import { Container } from './styles';
 
 export const App = () => {
+  const [currentId, setCurrentId] = useState(1);
+
+  const setId = id => () => setCurrentId(id);
+
   return (
     <Container>
       <LocationInput />
+      {[1, 2, 3].map((element, index) => {
+        return (
+          <Weather
+            key={index}
+            id={element}
+            setId={setId}
+            currentId={currentId}
+            day="Hoje"
+          />
+        );
+      })}
     </Container>
   );
 };
---
+++ src/components/Weather/styles.js
@@ -0,0 +1,96 @@
+import styled, { css, keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const conditionalBackground = css`
+  background-color: ${({ id }) =>
+    id === 2 ? 'rgba(255, 221, 0, 0.8);' : 'rgba(204, 177, 0, 0.7);'};
+`;
+
+export const WeatherContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  padding: 1rem;
+  transition: all 300ms ease-in;
+  height: ${({ isOpen }) => (isOpen ? '33rem' : '10rem')};
+  cursor: ${({ isOpen }) => (isOpen ? 'default' : 'pointer')};
+  ${({ id }) =>
+    id === 1
+      ? 'background-color: rgba(255, 234, 5, 0.6);'
+      : conditionalBackground};
+`;
+
+export const IconWrapper = styled.div`
+  flex: 1.5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    height: 28rem;
+    animation: ${fadeIn} 600ms linear normal;
+  }
+`;
+
+export const WeatherInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-end;
+
+  flex: 1;
+  color: #fff;
+  height: 100%;
+  font-size: 2rem;
+  position: relative;
+  padding-bottom: ${({ isOpen }) => (isOpen ? '8rem' : '1rem')};
+`;
+
+export const TopInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  position: absolute;
+  top: 0;
+  padding-top: 1rem;
+
+  span,
+  button {
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+
+  button {
+    color: #fff;
+    border: none;
+    font-size: 3rem;
+    cursor: pointer;
+    text-align: left;
+    background: transparent;
+  }
+`;
+
+export const MidInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  font-size: 3rem;
+  margin-bottom: 1.5rem;
+  animation: ${fadeIn} 600ms linear normal;
+`;
+
+export const BottomInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  animation: ${fadeIn} 600ms linear normal;
+`;
