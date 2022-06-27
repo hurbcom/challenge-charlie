786a07d494f766de07f626014bf6278300edd42f
Filipe André <filipeandres@hotmail.com>
2022-06-27 16:10:39 -0300

    refactor(TopInfo): removing unnecessary toString

--- src/components/Weather/TopInfo/index.js
+++ src/components/Weather/TopInfo/index.js
@@ -4,8 +4,8 @@ import { TopInfoContainer } from './styles';
 export const TopInfo = ({ isOpen, day, temperature }) => {
   const [isCelsius, setIsCelsius] = useState(true);
 
-  const celsiusTemp = `${temperature.toString()}ºC`;
-  const fahrenheitTemp = `${(temperature * 1.8 + 32).toFixed(0).toString()}ºF`;
+  const celsiusTemp = `${temperature}ºC`;
+  const fahrenheitTemp = `${(Number(temperature) * 1.8 + 32).toFixed(0)}ºF`;
 
   const toggleTemp = () => {
     setIsCelsius(!isCelsius);
