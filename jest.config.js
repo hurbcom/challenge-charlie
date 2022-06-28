09af60f4df5f0534ca1cf355ad4dc43e73b47a8c
Filipe André <filipeandres@hotmail.com>
2022-06-27 21:59:20 -0300

    test: adding loading component tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverageFrom: ['src/**/*.js(x)?'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
