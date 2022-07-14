03087292dafd6799b906aa912a267b3ee8694a4f
Genilson <geniilsonfernandes@gmail.com>
2022-07-14 11:53:03 -0300

    :zap:chore(search): add tests

---
+++ src/components/Search/Search.test.js
@@ -0,0 +1,31 @@
+import renderWithTheme from "../../utils/test/renderWithTheme";
+import { screen } from "@testing-library/react";
+import { Search } from ".";
+import userEvent from "@testing-library/user-event";
+
+describe("<Search /.>", () => {
+  it("should render the component with initial states", () => {
+    renderWithTheme(<Search />);
+    expect(
+      screen.getByPlaceholderText(/procure sua cidade/i)
+    ).toBeInTheDocument();
+  });
+  it("should show error in screen", () => {
+    renderWithTheme(<Search error="city not found" />);
+    expect(screen.getByText(/city not found/i)).toBeInTheDocument();
+  });
+  it("should show city when has", () => {
+    renderWithTheme(<Search city="rio" />);
+    expect(screen.getByPlaceholderText(/rio/i)).toBeInTheDocument();
+  });
+  it("should type in input and submit value", () => {
+    const onSubmit = jest.fn();
+    renderWithTheme(<Search city="rio" onSubmit={onSubmit} />);
+    const input = screen.getByPlaceholderText(/rio/i);
+
+    userEvent.type(input, "sao paulo");
+    expect(input).toHaveValue("sao paulo");
+    userEvent.keyboard("{enter}");
+    expect(onSubmit).toBeCalledWith("sao paulo");
+  });
+});
