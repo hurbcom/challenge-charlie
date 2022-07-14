c2c0eb7b9a791b52f84f6b1c36438a7057121259
Genilson <geniilsonfernandes@gmail.com>
2022-07-14 11:35:46 -0300

    :zap:chore(search): create component search

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { FaCompass as Compass } from "react-icons/fa";
+import P from "prop-types";
+import * as S from "./styles";
+
+export const Search = ({ city, onSubmit, error }) => {
+  const [value, setValue] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault(value);
+    onSubmit();
+  }
+
+  return (
+    <S.Wrapper onSubmit={handleSubmit}>
+      <S.Icon>
+        <Compass size={24} />
+      </S.Icon>
+      <input
+        type="text"
+        value={value}
+        placeholder={city ? city : "Procure sua cidade"}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {error && <S.Error>{error}</S.Error>}
+    </S.Wrapper>
+  );
+};
+
+Search.propTypes = {
+  city: P.string,
+  onSubmit: P.func,
+  error: P.string,
+};
---
+++ src/components/Search/styles.js
@@ -0,0 +1,48 @@
+import styled, { css } from "styled-components";
+
+export const Wrapper = styled.form`
+  ${({ theme }) => css`
+    background: ${theme.colors.white[100]};
+    height: 56px;
+    display: flex;
+    align-items: center;
+    transition: background ease-in-out 200ms;
+    box-shadow: 0px 3px 6px -1px rgba(121, 121, 121, 0.28);
+    :focus-within {
+      background: ${theme.colors.white[200]};
+    }
+    input {
+      background: none;
+      border: none;
+      height: 56px;
+      width: 100%;
+      padding-left: 8px;
+      color: ${theme.colors.textColors[400]};
+      outline: none;
+    }
+  `}
+  position: relative;
+`;
+export const Icon = styled.span`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    padding: 0 10px 0 16px;
+    color: ${theme.colors.textColors[400]};
+  `}
+`;
+
+export const Error = styled.span`
+  ${({ theme }) => css`
+    position: absolute;
+    bottom: -22px;
+    display: block;
+    width: 100%;
+    padding: 2px 4px;
+    font-size: 1.2rem;
+    border-radius: 0 0 4px 4px;
+    background: ${theme.colors.error};
+    z-index: 1;
+    color: ${theme.colors.textColors[100]};
+  `}
+`;
