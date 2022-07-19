11909c2ddbffd61fee1864d7443c1d266b062107
Genilson <geniilsonfernandes@gmail.com>
2022-07-19 17:30:31 -0300

    :add home and app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { ThemeProvider } from "styled-components";
+import { ForecastProvider } from "./context/ForecastContext";
+import { Home } from "./pages/Home";
 import GlobalStyle from "./styles/GlobalStyles";
 import theme from "./styles/theme";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-    </ThemeProvider>
+    <ForecastProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Home />
+      </ThemeProvider>
+    </ForecastProvider>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
