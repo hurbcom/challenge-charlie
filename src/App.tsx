aed5e36dffb73c66718a3012930f40a4dabb3064
Carlos Novaes <carlos.cruz@grupoboticario.com.br>
2022-07-25 20:44:24 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import GlobalStyles from './styles/GlobalStyles';
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyles />
+    </>
+  );
 }
