ebe2ba62d5e7e291d5b00096b57efacb4d2e7bc1
Maria Lemos <maria.lemos@b2wdigital.com>
2022-07-28 20:57:12 -0300

    chore: estrutura do projeto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import GlobalStyles from "./global-styles";
+
+function App() {
+    return (
+        <div className="App">
+            <GlobalStyles />
+        </div>
+    );
+}
+
+export default App;
