68173c282d84f30cbcd17f6c59cff1bffc572f63
renanlimaleite <renan.lima.softwares@gmail.com>
2022-07-28 14:43:26 -0300

    add: loader to webpack suport imgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import style from './app.module.scss'
+import image from './assets/images/exemplo.jpg'
 
 export const App = () => {
-  return <h1 className={style.teste}>Hello World</h1>
+  return (
+    <>
+       <h1 className={style.teste}>Hello World</h1>
+       <img src={image} alt="Imagem de exemplo" />
+    </>
+  )
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -33,6 +33,14 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   output: {
