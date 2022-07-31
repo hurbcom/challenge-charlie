7d40275f7ac7e689b056bcea46ef8256bbae8a3b
Matheus Henrique <matheus.181137@gmail.com>
2022-07-31 00:29:25 -0300

    feat: create background image component

---
+++ src/Components/Background/index.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import { getBackgroundImageURL } from '../../Core/services/bing'
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const Background: React.FC<Props> = ({ children }) => {
+  const [backgroundImage, setBackgroundImage] = useState('')
+
+  useEffect(() => {
+    async function getBackgroundImage () {
+      const backgroundImageData = await getBackgroundImageURL()
+      setBackgroundImage(backgroundImageData)
+    }
+    getBackgroundImage()
+  }, [])
+
+  return (
+    <div className='h-screen w-screen bg-slate-500' style={{ backgroundImage: `url('${backgroundImage}')` }}>
+      {children}
+    </div>
+  )
+}
