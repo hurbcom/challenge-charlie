65be92462c564836c0fe27edc7f08bfb8b6499e3
Matheus Henrique <matheus.181137@gmail.com>
2022-07-31 16:07:05 -0300

    test: array slice function

--- src/Common-functions/tests/array.test.ts
+++ src/Common-functions/tests/array.test.ts
@@ -1,4 +1,4 @@
-import { head } from '../functions/array'
+import { head, slice } from '../functions/array'
 
 describe('Array head testing', () => {
   it('Should return 42', () => {
@@ -11,5 +11,17 @@ describe('Array head testing', () => {
 
   it('Should return undefined', () => {
     expect(head([])).toEqual(undefined)
+  })
+})
+
+describe('Array splice testing', () => {
+  const mainArray = [1, 2, 3, 4, 5]
+
+  it('Should return the first two numbers', () => {
+    expect(slice(mainArray, 0, 2)).toEqual([1, 2])
+  })
+
+  it('Should remove the last 3 items', () => {
+    expect(slice(mainArray, 2, mainArray.length)).toEqual([3, 4, 5])
   })
 })
