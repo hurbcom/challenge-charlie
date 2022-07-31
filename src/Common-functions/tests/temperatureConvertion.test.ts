94592a641633e467cb2e42f45b9b094789474729
Matheus Henrique <matheus.181137@gmail.com>
2022-07-30 23:56:36 -0300

    test: kelvin to celsius temperature convertion function

--- src/Common-functions/tests/temperatureConvertion.test.ts
+++ src/Common-functions/tests/temperatureConvertion.test.ts
@@ -1,4 +1,4 @@
-import { fahrenheitToCelsius, celsiusToFahrenheit } from '../functions/temperatureConvertion'
+import { fahrenheitToCelsius, celsiusToFahrenheit, kelvinToCelsius } from '../functions/temperatureConvertion'
 
 describe('convert temperature values', () => {
   test('should convert fahrenheit to celsius and return 37', () => {
@@ -6,5 +6,11 @@ describe('convert temperature values', () => {
   })
   test('should convert celsius to fahrenheit and return 77', () => {
     expect(celsiusToFahrenheit(25)).toBe(77)
+  })
+  test('should convert kelvin to celsius and return 19.85', () => {
+    expect(kelvinToCelsius(293)).toBe(19.85)
+  })
+  test('should convert kelvin to celsius and return 19.85', () => {
+    expect(kelvinToCelsius(150)).toBe(-123.15)
   })
 })
