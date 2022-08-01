3a8275b26c10335bb5d3f0fe1a7d5c1b4019f64a
Maria Lemos <maria.lemos@b2wdigital.com>
2022-07-31 23:30:51 -0300

    fix: ajuste requests para buscar previsão por coordenadas

--- src/apis/weather.js
+++ src/apis/weather.js
@@ -1,20 +1,23 @@
 import config from "../config.json";
 import axios from "axios";
-export async function getWeatherByCityName(location) {
+
+export async function getWeatherByCoordenates(latitude, longitude) {
     try {
         const res = await axios({
             baseURL: `${config.apis.weather}`,
             method: "GET",
+            url: "/forecast",
             params: {
-                q: location.municipality,
+                lat: latitude,
+                lon: longitude,
                 units: "metric",
                 APPID: process.env.REACT_APP_WEATHER_API_KEY,
                 lang: "pt_br",
                 cnt: "3",
             },
         });
-        const { main, weather, wind } = res.data;
-        return { main, weather, wind };
+        const { city, list } = await res.data;
+        return { city: city.name, forecast: list };
     } catch (e) {
         console.log(e);
         return Promise.reject();
