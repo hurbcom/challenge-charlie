4f1a595bd12e05ba647cff1744ed242f76f5669d
Matheus Henrique <matheus.181137@gmail.com>
2022-08-02 06:46:40 -0300

    feat: move services to a Service object

--- src/Core/services/openCage.ts
+++ src/Core/services/openCage.ts
@@ -1,9 +1,13 @@
 import { OpenCageParser } from '../parsers/OpenCage'
-import { Location } from '../dto/OpenCageResponseDTO'
+import { Location } from '../dtos/OpenCageResponseDTO'
 
 const { REACT_APP_OPEN_CAGE_API_URL, REACT_APP_OPEN_CAGE_KEY } = process.env
 
-export const getLocality = (): Promise<Location> =>
-  fetch(`${REACT_APP_OPEN_CAGE_API_URL}/geocode/v1/json?q=-27.5787883%2C%20-48.4887322&key=${REACT_APP_OPEN_CAGE_KEY}&language=en&pretty=1`)
-    .then(res => res.json())
-    .then(OpenCageParser)
+export const OpenCageService = {
+  getLocality (coordinates: any): Promise<Location> {
+    const { lat, lon } = coordinates
+    return fetch(`${REACT_APP_OPEN_CAGE_API_URL}/geocode/v1/json?q=${lat}%2C%20${lon}&key=${REACT_APP_OPEN_CAGE_KEY}&language=en&pretty=1`)
+      .then(res => res.json())
+      .then(OpenCageParser)
+  }
+}
--- src/Core/services/openWeather.ts
+++ src/Core/services/openWeather.ts
@@ -1,14 +1,17 @@
-import { DailyForecastWeather } from '../dto/OpenWeatherResponseDTO'
+import { Coordinates } from '../dtos/OpenWeatherResponseDTO'
 import { OpenWeatherForecastParser, OpenWeatherParser } from '../parsers/OpenWeather'
 
 const { REACT_APP_OPEN_WEATHER_API_URL, REACT_APP_OPEN_WEATHER_KEY } = process.env
 
-export const getWeather = (): Promise<number> =>
-  fetch(`${REACT_APP_OPEN_WEATHER_API_URL}/data/2.5/weather?q=london&APPID=${REACT_APP_OPEN_WEATHER_KEY}`)
-    .then(res => res.json())
-    .then(OpenWeatherParser)
-
-export const getForecastWeather = (): Promise<DailyForecastWeather[]> =>
-  fetch(`${REACT_APP_OPEN_WEATHER_API_URL}/data/2.5/onecall?lat=-27.6146&lon=-48.5012&exclude=hourly,minutely,alerts&appid=${REACT_APP_OPEN_WEATHER_KEY}&units=metric&lang=pt`)
-    .then(res => res.json())
-    .then(OpenWeatherForecastParser)
+export const WeatherService = {
+  getWeather (locationName: string): Promise<Coordinates> {
+    return fetch(`${REACT_APP_OPEN_WEATHER_API_URL}/data/2.5/weather?q=${locationName}&APPID=${REACT_APP_OPEN_WEATHER_KEY}&units=metric`)
+      .then(res => res.json())
+      .then(OpenWeatherParser)
+  },
+  getForecastWeather ({ lat, lon }: Coordinates): Promise<any> {
+    return fetch(`${REACT_APP_OPEN_WEATHER_API_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&appid=${REACT_APP_OPEN_WEATHER_KEY}&units=metric&lang=pt`)
+      .then(res => res.json())
+      .then(OpenWeatherForecastParser)
+  }
+}
