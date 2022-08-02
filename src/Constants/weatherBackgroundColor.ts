e6802863221b45b732b612342791f6fbb15b4bf8
Matheus Henrique <matheus.181137@gmail.com>
2022-08-02 06:45:10 -0300

    feat: create weather background color constants

---
+++ src/Constants/weatherBackgroundColor.ts
@@ -0,0 +1,17 @@
+export const todayBackgroundColor = {
+  yellow: 'bg-amber-400',
+  blue: 'bg-blue-400',
+  red: 'bg-red-500'
+}
+
+export const tomorrowBackgroundColor = {
+  yellow: 'bg-amber-300',
+  blue: 'bg-blue-300',
+  red: 'bg-red-400'
+}
+
+export const afterTomorrowBackgroundColor = {
+  yellow: 'bg-amber-200',
+  blue: 'bg-blue-200',
+  red: 'bg-red-300'
+}
