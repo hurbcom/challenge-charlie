6367fca067fa1dd3b8152ec0ac621fd9bb9f6069
Matheus Henrique <matheus.181137@gmail.com>
2022-08-02 06:50:47 -0300

    feat: create get array tail function

--- src/Common-functions/functions/array.ts
+++ src/Common-functions/functions/array.ts
@@ -1,3 +1,5 @@
 export const head = (arr: any[]) => (arr && arr.length ? arr[0] : undefined)
 
+export const tail = (arr: any[]) => (arr && arr.length ? arr[arr.length - 1] : undefined)
+
 export const slice = (arr: any[], start: number, end: number) => (arr.slice(start, end))
