f0c2f32a3d1849aab1a1dc49205d8eed7ef8b635
Matheus Henrique <matheus.181137@gmail.com>
2022-08-02 06:50:58 -0300

    test: get array tail function

--- src/Common-functions/tests/array.test.ts
+++ src/Common-functions/tests/array.test.ts
@@ -1,4 +1,4 @@
-import { head, slice } from '../functions/array'
+import { head, slice, tail } from '../functions/array'
 
 describe('Array head testing', () => {
   it('Should return 42', () => {
@@ -11,6 +11,20 @@ describe('Array head testing', () => {
 
   it('Should return undefined', () => {
     expect(head([])).toEqual(undefined)
+  })
+})
+
+describe('Array tail testing', () => {
+  it('Should return 42', () => {
+    expect(tail([42])).toEqual(42)
+  })
+
+  it('Should return 21', () => {
+    expect(tail([42, 21])).toEqual(21)
+  })
+
+  it('Should return undefined', () => {
+    expect(tail([])).toEqual(undefined)
   })
 })
 
