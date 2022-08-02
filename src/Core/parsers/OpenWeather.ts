8fa615efa41aeaebf13babc57ef07de9d2a3364c
Matheus Henrique <matheus.181137@gmail.com>
2022-08-02 06:44:33 -0300

    feat: create parsing for forecast weathers

--- src/Core/parsers/OpenWeather.ts
+++ src/Core/parsers/OpenWeather.ts
@@ -1,16 +1,48 @@
-import { slice } from '../../Common-functions/functions/array'
-import { OpenWeatherResponseDTO, OpenWeatherForecastResponseDTO } from '../dto/OpenWeatherResponseDTO'
+/* eslint-disable camelcase */
+import { head, slice, tail } from '../../Common-functions/functions/array'
+import { degToCompass, metersPerSecondToKilometersPerHour } from '../../Common-functions/functions/metricConvertion'
+import { capitalizeFirstLetter } from '../../Common-functions/functions/stringFormat'
+import { OpenWeatherResponseDTO, OpenWeatherForecastResponseDTO, DailyForecastWeather } from '../dtos/OpenWeatherResponseDTO'
 
 export const OpenWeatherParser = (data: OpenWeatherResponseDTO) => {
-  const { main: { temp: celsiusTemperature } } = data
+  const { coord: coordinates } = data
 
-  return celsiusTemperature
+  return coordinates
+}
+
+const forecastParser = (forecast: DailyForecastWeather) => {
+  const { temp, weather, wind_speed, humidity, pressure } = forecast
+  const { description, main } = head(weather)
+
+  return {
+    temperature: Math.round(temp?.day),
+    description,
+    main,
+    windSpeed: wind_speed,
+    humidity,
+    pressure
+  }
 }
 
 export const OpenWeatherForecastParser = (data: OpenWeatherForecastResponseDTO) => {
   const { current, daily } = data
 
+  const { temp, weather, wind_speed, wind_deg, humidity, pressure } = current
+  const { description, main } = head(weather)
+
   const forecast = slice(daily, 0, 2)
 
-  return [current, ...forecast]
+  return {
+    todayWeather: {
+      temperature: Math.round(temp),
+      description: capitalizeFirstLetter(description),
+      main,
+      windSpeed: metersPerSecondToKilometersPerHour(wind_speed),
+      windPosition: degToCompass(wind_deg),
+      humidity,
+      pressure
+    },
+    tomorrowWeather: forecastParser(head(forecast)),
+    afterTomorrowWeather: forecastParser(tail(forecast))
+  }
 }
