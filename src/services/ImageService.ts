3e0db2597d60f97158b530404bc39619ed7d34b9
Karina <karina@cuponeria.com.br>
2022-08-07 21:14:16 -0300

    👽️  change request to call my on API to get bing Image

--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -7,17 +7,14 @@ export class ImageService {
         // To access documentation https://docs.microsoft.com/en-us/azure/cognitive-services/bing-image-search/bing-image-search-resource-faq for documentation
         try {
             const response = await axios({
-                url: "/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR",
-                baseURL:
-                    process.env.NODE_ENV === "development"
-                        ? `${config.proxy}/${config.apis.bing}`
-                        : config.apis.bing,
+                url: "/bingAPI",
+                baseURL: process.env.REACT_APP_API_URL,
                 method: "GET",
             });
 
             return `${config.apis.bing}${response.data?.images[0].url}`;
-        } catch (e) {
-            console.log(e);
+        } catch (error) {
+            console.log(error);
         }
     };
 }
