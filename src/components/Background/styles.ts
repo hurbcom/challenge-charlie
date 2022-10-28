f3b831e8fac03ce28eed05208800bb5bcb8da429
Lucas Harca <lucas.harca@gmail.com>
2022-10-28 11:10:50 -0300

    fixing problem with background

--- src/components/Background/styles.ts
+++ src/components/Background/styles.ts
@@ -7,7 +7,8 @@ interface BackgroundContainerProps {
 export const BackgroundContainer = styled.div<BackgroundContainerProps>`
   height: 100vh;
   width: 100vw;
-  background-size: cover;
+  background-size: cover !important;
+  background-repeat: no-repeat;
   background: url(${(props) => props.backgroundImage});
   overflow: auto;
 `
