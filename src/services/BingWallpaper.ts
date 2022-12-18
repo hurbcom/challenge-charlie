dbfb41bcc100a6c1e153a11a060a08510bd77d5d
Paulo Frare <pfrare.tads@gmail.com>
2022-12-17 22:25:35 -0300

    refactor: bing wallpaper service

--- src/services/BingWallpaper.ts
+++ src/services/BingWallpaper.ts
@@ -1,7 +1,12 @@
 import { api } from "./api";
 
 const getWallpaper = async () => {
-    return await api.get('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR')
+
+    const corsUrl = "https://api.allorigins.win/get?url=";
+    const url = corsUrl + encodeURIComponent("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR");
+    const result = await api.get(url)
+
+    return JSON.parse(result.data.contents)
 }
 
 export { getWallpaper };
