03ca145113842b129ac8b131783b296a6ae13442
Paulo Frare <pfrare.tads@gmail.com>
2022-12-17 22:26:07 -0300

    feat: create open cage service

--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext, useState } from 'react'
 import { getWallpaper } from '../../services/BingWallpaper'
+import { getLocation } from '../../services/OpenCage'
 import { WeatherContext } from '../../contexts/WeatherContext'
 import { HomeContainer, HomeWeatherContainer } from './Home.styles'
 import InputForm from '../../componentes/Input/input'
@@ -13,14 +14,28 @@ function Home() {
 
     useEffect(() => {
         getWallpaper().then((resp) => {
-            setBackgroundImage(`https://bing.com${resp.data.images[0].url}`)
+            setBackgroundImage(`https://bing.com${resp.images[0].url}`)
         })
+        getUserLocation()
 
-        getLocation()
     }, [])
 
-    const getLocation = async () => {
-        return navigator.geolocation.getCurrentPosition(function (position) {
+    const getUserLocation = () => {
+        try {
+            navigator.geolocation.getCurrentPosition(async (result) => {
+                try {
+                    const response = await getLocation(result.coords.latitude, result.coords.longitude)
+
+                } catch (error) {
+                    console.log(error);
+
+                }
+            })
+        } catch (error) {
+            console.error(error)
+        }
+
+        return navigator.geolocation.getCurrentPosition(async function (position) {
             getCordinates(position.coords.latitude, position.coords.longitude)
         })
     }
