e9c57c7f9f8f8ecff7590afe7ed68eeed59ff7dc
Paulo Frare <pfrare.tads@gmail.com>
2022-12-19 04:43:07 -0300

    feat: add react toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,27 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { Home } from './Views/Home/Home'
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+
 function App() {
 
     return (
         <>
             <ThemeProvider theme={defaultTheme}>
                 <WeatherContextProvider>
+                    <ToastContainer
+                        position='top-right'
+                        autoClose={3000}
+                        hideProgressBar={true}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                    />
                     <Home />
                     <GlobalStyle />
                 </WeatherContextProvider>
