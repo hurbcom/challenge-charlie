f0d49d8022a6f07d3cf208d1019ce4c705983445
Paulo Frare <pfrare.tads@gmail.com>
2022-12-19 02:25:11 -0300

    feat: add open weather geo service

--- src/services/OpenWeather.ts
+++ src/services/OpenWeather.ts
@@ -15,3 +15,7 @@ export const OpenWeatherForecast = (lat: number, long: number) => {
 export const OpenWeatherCoord = (lat: number, long: number) => {
     return api.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric&lang=pt_br`);
 };
+
+export const OpenWeatherGeo = (city: string) => {
+    return api.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`);
+};
