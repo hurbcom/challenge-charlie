9f87192e1d6650c8b0bc01164433e8e955afcca1
Paulo Frare <pfrare.tads@gmail.com>
2022-12-20 19:09:04 -0300

    test: Open Weather tests

---
+++ src/services/OpenWeather.spec.ts
@@ -0,0 +1,32 @@
+import { OpenWeather, OpenWeatherCoord, OpenWeatherForecast, OpenWeatherGeo } from './OpenWeather'
+import { api } from './api'
+
+
+jest.mock('./api')
+
+const city = 'Rio Grande'
+const lat = 12234
+const lon = 1279
+const apiID = '772920597e4ec8f00de8d376dfb3f094'
+
+describe("OpenWeatherAPI", () => {
+    it('should make the call to openWeather', async () => {
+        await OpenWeather(city)
+        expect(api.get).toHaveBeenCalledWith(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=772920597e4ec8f00de8d376dfb3f094&units=metric&lang=pt_br`)
+    })
+
+    it('should make the call to openWeatherForecast', async () => {
+        await OpenWeatherForecast(lat, lon)
+        expect(api.get).toHaveBeenCalledWith(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiID}&units=metric&lang=pt_br`)
+    })
+
+    it('should make the call to OpenWeatherGeo', async () => {
+        await OpenWeatherGeo(city)
+        expect(api.get).toHaveBeenCalledWith(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiID}&units=metric&lang=pt_br`)
+    })
+
+    it('should make the call to OpenWeatherCoord', async () => {
+        await OpenWeatherCoord(lat, lon)
+        expect(api.get).toHaveBeenCalledWith(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiID}&units=metric&lang=pt_br`)
+    })
+})
