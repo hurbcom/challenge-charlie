73c41e19b274b14367834d5f46071cc54191347b
Vinicius Augusto Ribeiro <vaugusto@outlook.com>
2022-12-21 20:32:10 -0300

    feat(mfe-weather-forecast): add weather forecast mfe

--- src/apps/frontend/mfes/weather-forecast/src/app/app.tsx
+++ src/apps/frontend/mfes/weather-forecast/src/app/app.tsx
@@ -1,10 +1,19 @@
-import { ForecastOverviewComponent } from "@challenge-charlie/frontend/weather-forecast/framework/presentation/components";
+import { ErrorBoundaryComponent, ForecastOverviewComponent } from "@challenge-charlie/frontend/weather-forecast/framework/presentation/components";
+import { StateContextProvider } from "@challenge-charlie/frontend/weather-forecast/framework/presentation/contexts";
+
+
+
+
 
 export function App() {
+
+
   return (
-    <div>
-      <ForecastOverviewComponent />
-    </div>
+    <ErrorBoundaryComponent>
+      <StateContextProvider>
+        <ForecastOverviewComponent />
+      </StateContextProvider>
+    </ErrorBoundaryComponent>
   );
 }
 
