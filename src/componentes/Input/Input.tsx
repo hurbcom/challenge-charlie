7288b2bc9e095c4083c972af240f108975c7de1d
Paulo Frare <pfrare.tads@gmail.com>
2022-12-22 05:14:17 -0300

    fix: format docs

--- src/componentes/Input/Input.tsx
+++ src/componentes/Input/Input.tsx
@@ -1,32 +1,24 @@
-import React from 'react';
-import { Input, InputContainer } from './Input.styles';
+import React from 'react'
+import { Input, InputContainer } from './Input.styles'
 
 interface InputProps {
-    onChange: React.ChangeEventHandler<HTMLInputElement>
-    value: string,
-    placeholder: string,
-    type: string
+  onChange: React.ChangeEventHandler<HTMLInputElement>
+  value: string
+  placeholder: string
+  type: string
 }
 
-const InputForm = ({
-    type,
-    placeholder,
-    value,
-    onChange
-}: InputProps) => {
+const InputForm = ({ type, placeholder, value, onChange }: InputProps) => {
+  return (
+    <InputContainer>
+      <Input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      ></Input>
+    </InputContainer>
+  )
+}
 
-
-
-    return (
-        <InputContainer>
-            <Input
-                type={type}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-            ></Input>
-        </InputContainer>
-    );
-};
-
-export { InputForm };
+export { InputForm }
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,186 +1,203 @@
-import React, { useEffect, useContext, useState, useRef } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { BingWallpaper } from '../../services/BingWallpaper'
-import { OpenWeather, OpenWeatherForecast, OpenWeatherCoord, OpenWeatherGeo } from '../../services/OpenWeather'
-import { WeatherContext, WeatherInformations } from '../../contexts/WeatherContext'
 import {
-    HomeContainer,
-    HomeWeatherContainer,
-    SecondColumn,
-    InformationTitle,
-    FormContainer,
-    CityInformationContainer,
-    CityInformationSection,
-    WeatherInformationsContainer
+  OpenWeather,
+  OpenWeatherForecast,
+  OpenWeatherCoord,
+  OpenWeatherGeo
+} from '../../services/OpenWeather'
+import {
+  WeatherContext,
+  WeatherInformations
+} from '../../contexts/WeatherContext'
+import {
+  HomeContainer,
+  HomeWeatherContainer,
+  SecondColumn,
+  InformationTitle,
+  FormContainer,
+  CityInformationContainer,
+  CityInformationSection,
+  WeatherInformationsContainer
 } from './Home.styles'
 import { InputForm } from '../../componentes/Input/Input'
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify'
 import { Puff } from 'react-loader-spinner'
 
-import {
-    formattedCityName
-} from '../../helpers/format'
+import { formattedCityName } from '../../helpers/format'
 
 import { getRangeTemp } from '../../helpers/range-temp'
 import { TodaySection } from '../../componentes/TodaySection/TodaySection'
 import { NextDaySection } from '../../componentes/NextDaySection/NextDaySection'
 
-
 function Home() {
+  const {
+    backgroundImage,
+    setBackgroundImage,
+    setLocation,
+    weatherInformations,
+    setWeatherInformations,
+    loading,
+    setLoading,
+    isFahrenheit,
+    setIsFahrenheit
+  } = useContext(WeatherContext)
+  const [city, setCity] = useState('')
 
-    const { backgroundImage,
-        setBackgroundImage,
-        setLocation,
-        weatherInformations,
-        setWeatherInformations,
-        loading,
-        setLoading,
-        isFahrenheit,
-        setIsFahrenheit
-    } = useContext(WeatherContext)
-    const [city, setCity] = useState('')
+  const showContent = !loading && weatherInformations
 
-    const showContent = !loading && weatherInformations
+  useEffect(() => {
+    getBackgroundImage()
+    getUserLocation()
+  }, [])
 
-    useEffect(() => {
-        getBackgroundImage();
-        getUserLocation()
-    }, [])
+  const getBackgroundImage = async () => {
+    try {
+      const { images } = await BingWallpaper()
+      setBackgroundImage(`https://bing.com${images[0].url}`)
+    } catch (error) {
+      notify('Não foi possível carregar a imagem de fundo')
+    }
+  }
 
-    const getBackgroundImage = async () => {
-        try {
-            const { images } = await BingWallpaper()
-            setBackgroundImage(`https://bing.com${images[0].url}`)
-        } catch (error) {
-            console.log(error);
+  const getUserLocation = () => {
+    try {
+      navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+        setLocation({ lat: coords.latitude, lon: coords.longitude })
+        getWeatherInformations(coords.latitude, coords.longitude)
+      })
+    } catch (error) {
+      notify('Não foi possível encontrar da sua cidade')
+    }
+  }
 
-            notify("Não foi possível carregar a imagem de fundo")
+  const getWeatherInformations = async (lat: number, long: number) => {
+    try {
+      setLoading(true)
+      const response = await Promise.all([
+        OpenWeatherCoord(lat, long),
+        OpenWeatherForecast(lat, long),
+        OpenWeatherGeo
+      ])
+      const coordResponse = response[0].data
+      const forecastResponse = response[1].data
+
+      const { data } = await OpenWeatherGeo(coordResponse.name)
+
+      const tomorrow = getRangeTemp(forecastResponse.list, 1)
+      const afterTomorrow = getRangeTemp(forecastResponse.list, 2)
+
+      const weatherData: WeatherInformations = {
+        city: formattedCityName(data[0]),
+        today: {
+          temp: coordResponse.main.temp,
+          description: coordResponse.weather[0].description,
+          humidity: coordResponse.main.humidity,
+          pressure: coordResponse.main.pressure,
+          wind: coordResponse.wind.speed,
+          icon: coordResponse.weather[0].icon
+        },
+        tomorrow: {
+          tempMin: tomorrow.min,
+          tempMax: tomorrow.max,
+          icon: tomorrow.icon
+        },
+        afterTomorrow: {
+          tempMin: afterTomorrow.min,
+          tempMax: afterTomorrow.max,
+          icon: afterTomorrow.icon
         }
+      }
+
+      setWeatherInformations(weatherData)
+    } catch (error) {
+      notify('Não foi possível encontrar as dados da cidade')
+    } finally {
+      setLoading(false)
     }
+  }
 
-    const getUserLocation = () => {
-        try {
-            navigator.geolocation.getCurrentPosition(async ({ coords }) => {
-                setLocation({ lat: coords.latitude, lon: coords.longitude })
-                getWeatherInformations(coords.latitude, coords.longitude)
-            })
-        } catch (error) {
-            notify("Não foi possível encontrar da sua cidade")
-        }
+  const notify = (message: string) => {
+    toast.error(message)
+  }
 
+  const getCityNameInformations = async () => {
+    try {
+      setLoading(true)
+      const { data } = await OpenWeather(city)
+      return data.coord
+    } catch (error) {
+      notify('Não foi possível encontrar a cidade digitada')
+    } finally {
+      setLoading(false)
     }
+  }
 
-    const getWeatherInformations = async (lat: number, long: number) => {
-        try {
-            setLoading(true)
-            const response = await Promise.all([OpenWeatherCoord(lat, long), OpenWeatherForecast(lat, long), OpenWeatherGeo])
-            const coordResponse = response[0].data
-            const forecastResponse = response[1].data
+  const handleSubmit = async (event: React.SyntheticEvent): Promise<void> => {
+    event.preventDefault()
+    const { lat, lon } = await getCityNameInformations()
+    getWeatherInformations(lat, lon)
+  }
 
-            const { data } = await OpenWeatherGeo(coordResponse.name)
+  const handleCelciusFahrenheit = () => {
+    if (isFahrenheit) setIsFahrenheit(false)
+    else setIsFahrenheit(true)
+  }
 
-
-            const tomorrow = getRangeTemp(forecastResponse.list, 1)
-            const afterTomorrow = getRangeTemp(forecastResponse.list, 2)
-
-            const weatherData: WeatherInformations = {
-                city: formattedCityName(data[0]),
-                today: {
-                    temp: coordResponse.main.temp,
-                    description: coordResponse.weather[0].description,
-                    humidity: coordResponse.main.humidity,
-                    pressure: coordResponse.main.pressure,
-                    wind: coordResponse.wind.speed,
-                    icon: coordResponse.weather[0].icon
-                },
-                tomorrow: {
-                    tempMin: tomorrow.min,
-                    tempMax: tomorrow.max,
-                    icon: tomorrow.icon
-
-                },
-                afterTomorrow: {
-                    tempMin: afterTomorrow.min,
-                    tempMax: afterTomorrow.max,
-                    icon: afterTomorrow.icon
-                }
-            }
-
-            setWeatherInformations(weatherData)
-
-        } catch (error) {
-            notify("Não foi possível encontrar as dados da cidade")
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    const notify = (message: string) => {
-        toast.error(message)
-    }
-
-    const getCityNameInformations = async () => {
-        try {
-            setLoading(true)
-            const { data } = await OpenWeather(city)
-            return data.coord
-        } catch (error) {
-            notify("Não foi possível encontrar a cidade digitada")
-
-        }
-        finally {
-            setLoading(false)
-        }
-    }
-
-    const handleSubmit = async (event: React.SyntheticEvent): Promise<void> => {
-        event.preventDefault()
-        const { lat, lon } = await getCityNameInformations()
-        getWeatherInformations(lat, lon)
-    }
-
-    const handleCelciusFahrenheit = () => {
-        if (isFahrenheit) setIsFahrenheit(false)
-        else setIsFahrenheit(true)
-    }
-
-    return (
-        <>
-            <HomeContainer backgroundImage={backgroundImage}>
-                <WeatherInformationsContainer>
-                    <FormContainer onSubmit={handleSubmit}>
-                        <InputForm
-                            type="text"
-                            placeholder="Escolha a cidade" value={city} onChange={(event: any) => setCity(event.target.value)}></InputForm>
-                    </FormContainer>
-                    {showContent ? <>
-                        <CityInformationContainer>
-                            <CityInformationSection variant='background-gray'>
-                                <img src={`/src/assets/WeatherIcons/44.svg`} alt="Bússola" />
-                                <InformationTitle>{weatherInformations.city}</InformationTitle>
-                            </CityInformationSection>
-                        </CityInformationContainer>
-                        <HomeWeatherContainer>
-                            <TodaySection onclick={handleCelciusFahrenheit} />
-                            <SecondColumn>
-                                <NextDaySection title={"Amanhã"} weatherInformations={weatherInformations.tomorrow} onclick={handleCelciusFahrenheit} />
-                                <NextDaySection title={"Depois de amanhã"} weatherInformations={weatherInformations.afterTomorrow} onclick={handleCelciusFahrenheit} />
-                            </SecondColumn>
-                        </HomeWeatherContainer>
-                    </> :
-                        <Puff
-                            height="100"
-                            width="100"
-                            radius={1}
-                            color="#e4e4e4"
-                            ariaLabel="puff-loading"
-                            wrapperStyle={{}}
-                            wrapperClass=""
-                            visible={true}
-                        />
-                    }
-                </WeatherInformationsContainer>
-            </HomeContainer>
-        </>)
+  return (
+    <>
+      <HomeContainer backgroundImage={backgroundImage}>
+        <WeatherInformationsContainer>
+          <FormContainer onSubmit={handleSubmit}>
+            <InputForm
+              type="text"
+              placeholder="Escolha a cidade"
+              value={city}
+              onChange={(event: any) => setCity(event.target.value)}
+            ></InputForm>
+          </FormContainer>
+          {showContent ? (
+            <>
+              <CityInformationContainer>
+                <CityInformationSection variant="background-gray">
+                  <img src={`/src/assets/WeatherIcons/44.svg`} alt="Bússola" />
+                  <InformationTitle>
+                    {weatherInformations.city}
+                  </InformationTitle>
+                </CityInformationSection>
+              </CityInformationContainer>
+              <HomeWeatherContainer>
+                <TodaySection onclick={handleCelciusFahrenheit} />
+                <SecondColumn>
+                  <NextDaySection
+                    title={'Amanhã'}
+                    weatherInformations={weatherInformations.tomorrow}
+                    onclick={handleCelciusFahrenheit}
+                  />
+                  <NextDaySection
+                    title={'Depois de amanhã'}
+                    weatherInformations={weatherInformations.afterTomorrow}
+                    onclick={handleCelciusFahrenheit}
+                  />
+                </SecondColumn>
+              </HomeWeatherContainer>
+            </>
+          ) : (
+            <Puff
+              height="100"
+              width="100"
+              radius={1}
+              color="#e4e4e4"
+              ariaLabel="puff-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          )}
+        </WeatherInformationsContainer>
+      </HomeContainer>
+    </>
+  )
 }
 
 export { Home }
