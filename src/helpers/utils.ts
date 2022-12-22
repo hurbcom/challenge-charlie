4820f07092fd0f70f34e7c579fe7e670c6d864ff
Paulo Frare <pfrare.tads@gmail.com>
2022-12-22 14:46:49 -0300

    fix: remove unnecessary import

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,6 +1,4 @@
 import { CardColorVariant } from "views/Home/Home.styles"
-import { WeatherContext } from '../contexts/WeatherContext'
-
 
 export const getCardBackgroundColor = (temp: number): CardColorVariant => {
     if (temp <= 15) return 'background-blue'
