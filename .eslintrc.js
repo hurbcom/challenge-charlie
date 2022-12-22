ae4a7125d4ce032529576c035c8c366645c0d75a
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2022-12-21 21:58:18 -0300

    Added lint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "react-app",
+    "react-app/jest",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
