fa7fb0da434295b0da50d31a025e84de856f5c5b
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2022-12-29 13:02:37 -0300

    Added round

--- src/modules/celsiusToFahrenheit.js
+++ src/modules/celsiusToFahrenheit.js
@@ -1,3 +1,7 @@
 export function celsiusToFahrenheit(celsius) {
-  return (celsius * 9) / 5 + 32;
+  return round((celsius * 9) / 5 + 32);
+}
+
+function round(num) {
+  return Math.round((num + Number.EPSILON) * 100) / 100;
 }
