fd0862ddc5c1480be7854fe16644e5a7604df766
Ana <asimplicio37@gmail.com>
2023-01-18 16:38:46 -0300

    adding header base component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <p className="header__location">Rio de Janeiro, Rio de Janeiro</p>
+    </header>
+  );
+};
+
+export default Header;
