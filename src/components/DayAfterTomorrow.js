8d3828c9538f680d27b0f2f83f4198fd343a79f8
Ana <asimplicio37@gmail.com>
2023-01-18 16:39:33 -0300

    add dayaftertomorrow base component

---
+++ src/components/DayAfterTomorrow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/DayAfterTomorrow.css';
+
+const DayAfterTomorrow = () => {
+  return (
+    <div className="day-after-tomorrow">
+      <p className="day-after-tomorrow__title">Depois de Amanha</p>
+    </div>
+  );
+};
+
+export default DayAfterTomorrow;
