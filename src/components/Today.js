6c85249f304e19ee326c411c468206721a2398c6
Ana <asimplicio37@gmail.com>
2023-01-18 17:42:05 -0300

    add today styling

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
 import './style/Today.css';
 
 const Today = () => {
   return (
     <div className="today">
-      <p className="today__title">Hoje</p>
+      <div className="today__icon-container">
+        <WbSunnyOutlinedIcon style={{ fontSize: '180px' }} />
+      </div>
+      <div className="today__info">
+        <p className="today__info__title">Hoje</p>
+        <p className="today__info__temperature">32C</p>
+        <p className="today__info__wheater">Ensolarado</p>
+        <p>Vento: NO 6.4km/h</p>
+        <p>Humidade: 78%</p>
+        <p>Pressao: 1003hPA</p>
+      </div>
     </div>
   );
 };
