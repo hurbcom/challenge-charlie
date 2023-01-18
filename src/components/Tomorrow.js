78cb1f3c595dc76f27c22a64a82939a4cb40362b
Ana <asimplicio37@gmail.com>
2023-01-18 16:39:13 -0300

    add tomorrow base component

---
+++ src/components/Tomorrow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/Tomorrow.css';
+
+const Tomorrow = () => {
+  return (
+    <div className="tomorrow">
+      <p className="tomorrow__title">Amanha</p>
+    </div>
+  );
+};
+
+export default Tomorrow;
