12365bf41fd4c4c7cd9b9b3f99e16f5a55954769
Ana <asimplicio37@gmail.com>
2023-01-18 16:39:01 -0300

    adding today base component

---
+++ src/components/Today.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/Today.css';
+
+const Today = () => {
+  return (
+    <div className="today">
+      <p className="today__title">Hoje</p>
+    </div>
+  );
+};
+
+export default Today;
