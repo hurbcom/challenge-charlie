cee47ecf5eb5a98a4464a519537b04f0fe2b002c
Daniela C. Barbosa <contato@danielacb.com>
2023-01-19 23:50:42 -0300

    create theme styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -5,13 +5,13 @@ type WrapperProps = {
 };
 
 export const Wrapper = styled.div<WrapperProps>`
-    ${({ imageURL }) => css`
+    ${({ theme, imageURL }) => css`
         width: 100%;
         height: 100vh;
         display: flex;
         align-items: center;
         justify-content: center;
 
-        background: ${imageURL ? `url(${imageURL})` : "#3468fc"};
+        background: ${imageURL ? `url(${imageURL})` : theme.colors.blue};
     `}
 `;
---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export default {
+    colors: {
+        white: "#ffffff",
+        black: "#000000",
+        darkGray: "#807f7d",
+        gray: "#d9d9d9",
+        darkBlue: "#274fc4",
+        blue: "#3468fc",
+        lightBlue: "#6189fd",
+        darkRed: "#a71200",
+        red: "#d61600",
+        lightRed: "#e04b39",
+        darkYellow: "#c19e07",
+        yellow: "#f8cc07",
+        lightYellow: "#fad83f",
+    },
+    shadow: "0px 4px 4px rgba(0, 0, 0, 0.1)",
+} as const;
