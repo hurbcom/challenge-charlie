39a5faa0fab411927c7a293f7b31a6853baa301d
Jose Vinicius <jviniciusoliveira@gmail.com>
2023-01-31 18:32:00 -0300

    build: add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         loader: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
