980c7f7f50ff2906ee21b8f12c3da709084e6137
Jose Vinicius <jviniciusoliveira@gmail.com>
2023-02-02 15:27:40 -0300

    feat: add useWeatherForecast hook

---
+++ src/hooks/useWeatherForecast.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import {
+  findCurrentWeatherByCityName,
+  findWeatherForecastByCityName,
+} from '../services/weather';
+import { WeatherInformation } from '../utils/models';
+
+export function useWeatherForecast(cityName?: string) {
+  const [loading, setLoading] = useState(false);
+  const [weatherForecast, setWeatherForecast] = useState<WeatherInformation[]>(
+    []
+  );
+
+  const handleWeatherForecast = async () => {
+    const currentWeatherPromise = findCurrentWeatherByCityName(cityName!);
+    const weatherForecastPromise = findWeatherForecastByCityName(cityName!);
+
+    const [current, forecast] = await Promise.allSettled([
+      currentWeatherPromise,
+      weatherForecastPromise,
+    ]);
+
+    if (current.status === 'fulfilled' && current.value) {
+      setWeatherForecast([current.value]);
+    }
+
+    if (forecast.status === 'fulfilled' && forecast.value) {
+      setWeatherForecast((state) => [...state, ...(forecast.value as [])]);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (cityName) {
+      setLoading(true);
+      handleWeatherForecast();
+    }
+  }, [cityName]);
+
+  return { weatherForecast, loading };
+}
