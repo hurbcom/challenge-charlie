e9df98dcbd416bbebda3266a1e943932499de349
Jose Vinicius <jviniciusoliveira@gmail.com>
2023-02-02 12:12:37 -0300

    feat: add useGeolocation hook

---
+++ src/hooks/useGeolocation.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useState } from 'react';
+import { findCityByCoordinates } from '../services/geocode';
+import { UserLocation } from '../utils/models';
+
+export function useGeolocation() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [location, setLocation] = useState<UserLocation>();
+
+  const handleError = useCallback(() => {
+    setError(true);
+    setLoading(false);
+  }, []);
+
+  const handleUserLocation = async ({ coords }: GeolocationPosition) => {
+    try {
+      const userLocation = await findCityByCoordinates(coords);
+
+      if (userLocation) {
+        setLocation({
+          city: userLocation.city,
+          state: userLocation.state,
+        });
+      }
+
+      setLoading(false);
+    } catch (_) {
+      handleError();
+    }
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(handleUserLocation, handleError);
+    } else {
+      handleError();
+    }
+  }, []);
+
+  return { loading, error, location };
+}
