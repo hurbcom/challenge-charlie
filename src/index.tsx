45f7725e6c473b200c0d7e16b21a2e6af9240f19
Jose Vinicius <jviniciusoliveira@gmail.com>
2023-02-07 15:58:53 -0300

    chore: remove strict

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 const RootElement = document.getElementById('root');
 
-const RootApp = () => (
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-ReactDOM.createRoot(RootElement as HTMLElement).render(<RootApp />);
+ReactDOM.createRoot(RootElement as HTMLElement).render(<App />);
