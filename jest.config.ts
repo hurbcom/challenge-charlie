2a608559f689322b87a43fabb226f575365ad8c9
Jose Vinicius <jviniciusoliveira@gmail.com>
2023-02-07 09:39:26 -0300

    test: add jest config

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+require('dotenv').config();
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  roots: ['<rootDir>/src'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['**/*.test.ts'],
+  setupFiles: ['dotenv/config'],
+};
+
+export default config;
