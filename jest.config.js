8e6cac04d5a56b1fa8123b42840e26a4a93c816e
marcosdissotti <marcosdissotti@gmail.com>
2023-02-09 16:49:18 -0300

    feat: 🚀 project structure / added tests settings to jest and testing-library

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testIgnorePatterns: ['/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  testEnvironment: 'jsdom',
+};
