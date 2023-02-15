6fd7226af0fd38c24730e05b70d61e74165da498
João Domingos <j13domingos@gmail.com>
2023-02-14 21:24:08 -0300

    feat: Iniciando o projeto com create react app, removido alguns conteúdos iniciais da criação do projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Olá mundo! Começando o desafio
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
