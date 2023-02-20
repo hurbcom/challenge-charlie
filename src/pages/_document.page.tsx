20d4ce7f411061da46ff20684d0e7cd8e4d34b72
Efraim Andrade <efraim.dev@gmail.com>
2023-02-20 20:34:40 -0300

    feat: favicon / added the favicon

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -7,21 +7,16 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
 
-        <style
-          id="stitches"
-          dangerouslySetInnerHTML={{ __html: getCssText() }}
-        />
+        <link rel="manifest" href="icon/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="icon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="icon/favicon-16x16.png" />
+
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
 
       <body>
