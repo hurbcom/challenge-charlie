3c7d454ae36ea2ddc35e1cbfd55cbdb201f2ff63
Efraim Andrade <efraim.dev@gmail.com>
2023-03-03 02:42:52 -0300

    chore: docker / added the necessary config to get hot reload work on docker

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig
