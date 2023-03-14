2ee6c0d8cffae78d44750503e49e6550e528d307
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2023-03-14 19:06:28 -0300

    add weather icons

--- src/components/weather-icon.jsx
+++ src/components/weather-icon.jsx
@@ -1,14 +1,17 @@
+import DrizzleIcon from "public/icons/drizzle.svg";
+import RainIcon from "public/icons/rain.svg";
 import SunIcon from "public/icons/sun.svg";
+import SnowIcon from "public/icons/snow.svg";
 import CloudsIcon from "public/icons/clouds.svg";
-import { useEffect } from "react";
-import classNames from "@/utils/classnames";
+import MistIcon from "public/icons/mist.svg";
+import ThunderIcon from "public/icons/thunder.svg";
 
 const WEATHER_ICONS = {
-    Thunderstorm: "",
-    Drizzle: "",
-    Rain: "",
-    Snow: "",
-    Mist: "",
+    Thunderstorm: ThunderIcon,
+    Drizzle: DrizzleIcon,
+    Rain: RainIcon,
+    Snow: SnowIcon,
+    Mist: MistIcon,
     Clear: SunIcon,
     Clouds: CloudsIcon,
     // Smoke,
