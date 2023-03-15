d6082d2f46e9b93556161eb8cf147fe5af30c691
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2023-03-15 00:23:48 -0300

    feat: add viewport meta tag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,16 @@
 import "@/styles/globals.css";
 import WeatherProvider from "@/utils/weather-context";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <WeatherProvider>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
             <Component {...pageProps} />
         </WeatherProvider>
     );
