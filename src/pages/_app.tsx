3deb59dcdc3917948341c199afae55225a182bcc
Marcel Fox <marcelfox@live.com>
2023-03-29 09:00:13 -0300

    adding _app to hold the spa content

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+
+const App = (): ReactNode => {
+    return (
+        <html>
+            <body>
+                <div>
+                    <h1>Hello, World!</h1>
+                </div>
+            </body>
+        </html>
+    );
+};
+
+export default App
