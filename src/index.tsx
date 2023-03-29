9ba0b925afbcd9a9b334845b55453d04d0164fc6
Marcel Fox <marcelfox@live.com>
2023-03-29 09:31:57 -0300

    changing port and log message

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { renderToPipeableStream } from "react-dom/server";
 import App from "@/pages/_app";
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.get("/", (req, res) => {
     const stream = renderToPipeableStream(App());
@@ -14,5 +14,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`\nRunning at ${port}\n`);
 });
