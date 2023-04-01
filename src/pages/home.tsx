2387ce9d8d709c4bc2753fce8d0838ffc2f24ed3
Marcel Fox <marcelfox@live.com>
2023-04-01 19:31:00 -0300

    imrpoving getGeolocation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,32 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { LocalityType } from '@/types/global';
 
 const Home = () => {
   const [textColor, setTextColor] = useState('text-red-500');
-  const [locality, setLocality] = useState<{
-    latitude: number | null;
-    longitude: number | null;
-  }>();
 
   useEffect(() => {
-    if (localStorage.getItem('locality')) {
-      const {latitude, longitude} = JSON.parse(localStorage.getItem('locality') as string)
-      setLocality(JSON.parse(localStorage.getItem('locality') as string));
-      fetch(`/api/locality?latitude=${latitude}&longitude=${longitude}`).then(async (res) => {
-        console.log(await res.json());
-      });
-    }
-    if (!localStorage.getItem('locality')) getLocation();
+    getGeolocation().then((res: LocalityType) => {
+      fetch(`/api/locality?latitude=${res.latitude}&longitude=${res.longitude}`).then(
+        async (res) => {
+          console.log(await res.json());
+        }
+      );
+    });
   }, []);
 
-  async function getLocation() {
-    return await new Promise((req, res) => {
+  async function getGeolocation(): Promise<LocalityType> {
+    if (!localStorage.getItem('locality')) {
       navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
-        setLocality({ latitude, longitude });
         localStorage.setItem('locality', JSON.stringify({ latitude, longitude }));
-        res({ latitude, longitude });
       });
-    });
+    }
+    return await JSON.parse(localStorage.getItem('locality') as string);
   }
 
   const changeColor = () => {
