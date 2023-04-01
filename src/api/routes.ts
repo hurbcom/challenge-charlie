e804e6d14c11330ee3ff21c0daf07a4e32bd866d
Marcel Fox <marcelfox@live.com>
2023-03-31 21:52:48 -0300

    adding locality route

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import OpenCageService, { LocalityType } from '@/api/services/OpenCagesService';
+
 const ApiRoutes = express.Router();
 
 ApiRoutes.get('/health', (req, res) => {
   res.json({ message: 'ok' }).status(200);
+});
+
+ApiRoutes.get('/locality', async (req, res) => {
+  // TODO: move logics into controller
+  const openCageService = new OpenCageService();
+  const { latitude, longitude } = req.query;
+  res.json(await openCageService.retrieveLocation({ latitude, longitude } as LocalityType));
 });
 
 export default ApiRoutes;
