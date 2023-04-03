e6cd5b5971c9021ed152b69c859d6fd0e327bd86
Marcel Fox <marcelfox@live.com>
2023-04-03 01:28:17 -0300

    adding middleware to check body of request

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,8 +3,16 @@ import express from 'express';
 import { LocalityType } from '@/types/global';
 import { getLocationResults, getWeatherForecast } from '@/api/controllers/LocalityController';
 import { getDailyImageUrl } from '@/api/controllers/BgImageController';
+import { nextTick } from 'process';
 
 const ApiRoutes = express.Router();
+
+ApiRoutes.all('*', (req, res, next) => {
+  if (req.method === 'POST' && !!req.body === false) {
+    res.status(400).json({ message: 'No request body' });
+  }
+  next()
+});
 
 ApiRoutes.get('/health', (_, res) => {
   res.json({ message: 'ok' }).status(200);
@@ -14,8 +22,8 @@ ApiRoutes.get('/locality', async (req, res) => {
   res.json(await getLocationResults(req.query as LocalityType));
 });
 
-ApiRoutes.get('/forecast', async (req, res) => {
-  res.json(await getWeatherForecast(req.query as LocalityType));
+ApiRoutes.post('/forecast', async (req, res) => {
+  res.json(await getWeatherForecast(req.query as LocalityType, req.body as {today: string}));
 });
 
 ApiRoutes.get('/backgroundImage', async (req, res) => {
