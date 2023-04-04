038309f1ddfdd5fd55068031c90ff1a1f6098bd6
Marcel Fox <marcelfox@live.com>
2023-04-04 16:12:01 -0300

    adding fixture to be loaded into style by webpack

---
+++ src/components/fixtures/bgTempColors.ts
@@ -0,0 +1,29 @@
+// pre render this colors in tailwindcss
+const bgTempColors = [
+  'bg-yellow-200',
+  'bg-red-200',
+  'bg-blue-200',
+  'bg-yellow-300',
+  'bg-red-300',
+  'bg-blue-300',
+  'bg-yellow-400',
+  'bg-red-400',
+  'bg-blue-400',
+  'bg-yellow-500',
+  'bg-red-500',
+  'bg-blue-500',
+  'bg-yellow-600',
+  'bg-red-600',
+  'bg-blue-600',
+  'bg-yellow-700',
+  'bg-red-700',
+  'bg-blue-700',
+  'bg-yellow-800',
+  'bg-red-800',
+  'bg-blue-800',
+  'bg-yellow-900',
+  'bg-red-900',
+  'bg-blue-900',
+];
+
+export { bgTempColors };
