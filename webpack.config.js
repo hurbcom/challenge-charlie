69b82147d89fda48d9dee3f928e29084bdc180c0
Marcel Fox <marcelfox@live.com>
2023-04-05 01:05:51 -0300

    simplyfing webpack into single file

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,18 @@
  */
 
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
 const nodeExternals = require('webpack-node-externals');
-
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const path = require('path');
 
-let serverConfig = {
-  target: 'node',
-  entry: './src/main.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'server.js',
-  },
-  externals: [nodeExternals()],
-};
+require('dotenv').config({ path: './.env' });
 
-let clientConfig = {
+const NODE_ENV = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+const commonConfig = {
   target: 'web',
   entry: {
     client: './src/pages/_app.tsx',
@@ -27,9 +23,63 @@ let clientConfig = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name]/bundle.js',
   },
+  mode: NODE_ENV,
+  devtool: NODE_ENV === 'prodution' ? 'source-map' : 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    alias: {
+      '@': __dirname + '/src',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+        exclude: /(node_modules|public|dist)/,
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(mov|mp4|jpg|svg|gif|png|ico)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|ttf|svg|eot)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'client/styles.css',
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    usedExports: true,
+  },
 };
 
-serverConfig = merge(common, serverConfig);
-clientConfig = merge(common, clientConfig);
-
-module.exports = [clientConfig, serverConfig];
+module.exports = [
+  merge(commonConfig, {
+    target: 'web',
+  }),
+  merge(commonConfig, {
+    target: 'node',
+    entry: './src/main.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'server.js',
+    },
+    externals: [nodeExternals()],
+  }),
+];
