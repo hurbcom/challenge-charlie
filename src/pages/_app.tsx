a412f27b2047f7d88adc9c21436345006a9a3c88
pedro-phd <pedro_dominici@outlook.ccom>
2023-04-09 20:41:30 -0300

    feat: add toast to feedback user

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Toaster
+                position="bottom-right"
+                reverseOrder={false}
+            />
+            <Component {...pageProps} />
+        </div>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import { HttpResponse } from "@/data/http/http-client"
 import { bingImageModel } from "@/domain/models/bing-image-model"
 import { RemoteGetImageBing } from "@/useCases/remote-get-image-bing"
+import { useEffect, useState } from "react"
+import { toast } from 'react-hot-toast'
 
 type Props = {
-    data: HttpResponse<bingImageModel>
+    bingApi: HttpResponse<bingImageModel>
 }
 
-export default function Home(props: Props) {
-    console.log(props.data)
+type StateProps = {
+    bingApi: HttpResponse<bingImageModel>
+}
+export default function Home({ bingApi }: Props) {
+    const [state, setState] = useState<StateProps>()
 
+    useEffect(() => {
+        if (bingApi.error) {
+            toast.error(bingApi.error)
+        }
+        setState(old => ({ ...old, bingApi: bingApi }))
+    }, [])
+
+    console.log(state)
     return (
         <h1 className="text-blue-700">Hello world Hurb :D</h1>
     )
@@ -17,11 +30,11 @@ export default function Home(props: Props) {
 export async function getServerSideProps() {
 
     const bingClient = new RemoteGetImageBing()
-    const data = await bingClient.get()
+    const bingApi = await bingClient.get()
 
     return {
         props: {
-            data
+            bingApi
         }
     }
 }
