90934eadb88dc2f1c78b248c37c6b08e3528115e
pedro-phd <pedro_dominici@outlook.ccom>
2023-04-09 21:04:42 -0300

    feat: add bg image and default image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,18 +12,38 @@ type StateProps = {
     bingApi: HttpResponse<bingImageModel>
 }
 export default function Home({ bingApi }: Props) {
-    const [state, setState] = useState<StateProps>()
+    const [state, setState] = useState<StateProps>({
+        bingApi: {
+            body: {
+                images: [{
+                    url: '/th?id=OHR.LithuanianEggs_PT-BR5718719505_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp',
+                    title: "Uma exibição de Páscoa deliciosamente ornamentada"
+                }]
+            },
+            statusCode: 200
+        }
+    })
 
     useEffect(() => {
         if (bingApi.error) {
             toast.error(bingApi.error)
+            toast.error('Não foi possivel recuperar a imagem de fundo, uma imagem template será exibida até o proximo sucesso!')
+        } else {
+            setState(old => ({ ...old, bingApi: bingApi }))
         }
-        setState(old => ({ ...old, bingApi: bingApi }))
     }, [])
 
     console.log(state)
+
+    const bgImg = `https://www.bing.com${state?.bingApi.body?.images[0].url}`
+
     return (
-        <h1 className="text-blue-700">Hello world Hurb :D</h1>
+        <div
+            className={`w-full h-screen flex justify-center items-center`}
+            style={{ backgroundImage: `url(${bgImg})` }}
+        >
+            <h1 className="text-blue-700">Hello world Hurb :D</h1>
+        </div>
     )
 }
 
