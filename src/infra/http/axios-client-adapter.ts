835b24dfd0a4931963cf19693edca83a2f5060ce
pedro-phd <pedro_dominici@outlook.ccom>
2023-04-10 18:03:15 -0300

    fix: types

--- src/infra/http/axios-client-adapter.ts
+++ src/infra/http/axios-client-adapter.ts
@@ -3,33 +3,35 @@ import { HttpClient, HttpRequest, HttpResponse, HttpStatusCode } from '../../dat
 import axios, { AxiosResponse } from 'axios'
 
 export class AxiosHttpClientAdapter implements HttpClient {
-  async request (data: HttpRequest): Promise<HttpResponse> {
-    let axiosResponse: AxiosResponse
-    try {
-      axiosResponse = await axios.request({
-        url: data.url,
-        method: data.method,
-        data: data.body,
-        headers: data.headers
-      })
-    } catch (error: any) {
-      if (error.response) {
-        return {
-          statusCode: error.response.status,
-          error: error.response.data?.error ?? error.response.data
+    async request(data: HttpRequest): Promise<HttpResponse> {
+        let axiosResponse: AxiosResponse
+        try {
+            axiosResponse = await axios.request({
+                url: data.url,
+                method: data.method,
+                data: data.body,
+                headers: data.headers
+            })
+        } catch (error: any) {
+            if (error.response) {
+                return {
+                    statusCode: error.response.status,
+                    error: error.response.data?.error ?? error.response.data,
+                    body: {}
+                }
+            } else if (error.request) {
+                return {
+                    statusCode: HttpStatusCode.serverError,
+                    error: error.message,
+                    body: {}
+                }
+            } else {
+                return { statusCode: HttpStatusCode.serverError, body: {} }
+            }
         }
-      } else if (error.request) {
         return {
-          statusCode: HttpStatusCode.serverError,
-          error: error.message
+            statusCode: axiosResponse.status,
+            body: axiosResponse.data
         }
-      } else {
-        return { statusCode: HttpStatusCode.serverError }
-      }
     }
-    return {
-      statusCode: axiosResponse.status,
-      body: axiosResponse.data
-    }
-  }
 }
