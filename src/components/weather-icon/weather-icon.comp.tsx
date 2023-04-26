c95bdae2de3bcb0a7697bfb66513dd945168f5f6
EduardoPedrosa <eduardopedrosamf@gmail.com>
2023-04-25 21:08:28 -0300

    fix: weather icon ui

--- src/components/weather-icon/weather-icon.comp.tsx
+++ src/components/weather-icon/weather-icon.comp.tsx
@@ -8,6 +8,7 @@ import { ReactComponent as SmokeIcon } from '@assets/icons/smoke.svg'
 import { ReactComponent as SnowIcon } from '@assets/icons/snow.svg'
 import { ReactComponent as ThunderstormIcon } from '@assets/icons/thunderstorm.svg'
 import { WeatherName } from '@services/open-weather/open-weather.service.types'
+import { Box } from '@components/ui'
 
 interface WeatherIconProps {
     weather: WeatherName
@@ -15,94 +16,98 @@ interface WeatherIconProps {
 }
 
 export const WeatherIcon: React.FC<WeatherIconProps> = ({ weather, color }) => {
-    switch (weather) {
-        case 'Thunderstorm':
-        case 'Tornado':
-            return (
-                <ThunderstormIcon
-                    width={'100%'}
-                    height="100%"
-                    fill={color}
-                    data-testid="thunderstorm-icon"
-                />
-            )
-        case 'Drizzle':
-            return (
-                <DrizzleIcon
-                    width={'100%'}
-                    height="100%"
-                    fill={color}
-                    data-testid="drizzle-icon"
-                />
-            )
-        case 'Rain':
-            return (
-                <RainIcon
-                    width={'100%'}
-                    height="100%"
-                    fill={color}
-                    data-testid="rain-icon"
-                />
-            )
-        case 'Snow':
-            return (
-                <SnowIcon
-                    width={'100%'}
-                    height="100%"
-                    fill={color}
-                    data-testid="snow-icon"
-                />
-            )
-        case 'Mist':
-        case 'Haze':
-        case 'Fog':
-            return (
-                <MistIscon
-                    width={'100%'}
-                    height="100%"
-                    fill={color}
-                    data-testid="mist-icon"
-                />
-            )
-        case 'Smoke':
-            return (
-                <SmokeIcon
-                    width={'100%'}
-                    height="100%"
-                    fill={color}
-                    data-testid="smoke-icon"
-                />
-            )
-        case 'Dust':
-        case 'Sand':
-        case 'Ash':
-        case 'Squall':
-            return (
-                <WindIcon
-                    width={'100%'}
-                    height="100%"
-                    fill={color}
-                    data-testid="wind-icon"
-                />
-            )
-        case 'Clouds':
-            return (
-                <Cloudscon
-                    width={'100%'}
-                    height="100%"
-                    fill={color}
-                    data-testid="clouds-icon"
-                />
-            )
+    const renderIcon = () => {
+        switch (weather) {
+            case 'Thunderstorm':
+            case 'Tornado':
+                return (
+                    <ThunderstormIcon
+                        width="100%"
+                        height="100%"
+                        fill={color}
+                        data-testid="thunderstorm-icon"
+                    />
+                )
+            case 'Drizzle':
+                return (
+                    <DrizzleIcon
+                        width="100%"
+                        height="100%"
+                        fill={color}
+                        data-testid="drizzle-icon"
+                    />
+                )
+            case 'Rain':
+                return (
+                    <RainIcon
+                        width="100%"
+                        height="100%"
+                        fill={color}
+                        data-testid="rain-icon"
+                    />
+                )
+            case 'Snow':
+                return (
+                    <SnowIcon
+                        width="100%"
+                        height="100%"
+                        fill={color}
+                        data-testid="snow-icon"
+                    />
+                )
+            case 'Mist':
+            case 'Haze':
+            case 'Fog':
+                return (
+                    <MistIscon
+                        width="100%"
+                        height="100%"
+                        fill={color}
+                        data-testid="mist-icon"
+                    />
+                )
+            case 'Smoke':
+                return (
+                    <SmokeIcon
+                        width="100%"
+                        height="100%"
+                        fill={color}
+                        data-testid="smoke-icon"
+                    />
+                )
+            case 'Dust':
+            case 'Sand':
+            case 'Ash':
+            case 'Squall':
+                return (
+                    <WindIcon
+                        width="100%"
+                        height="100%"
+                        fill={color}
+                        data-testid="wind-icon"
+                    />
+                )
+            case 'Clouds':
+                return (
+                    <Cloudscon
+                        width="100%"
+                        height="100%"
+                        fill={color}
+                        data-testid="clouds-icon"
+                    />
+                )
 
-        default:
-            return (
-                <SunIcon
-                    width={'100%'}
-                    height="100%"
-                    fill={color}
-                    data-testid="sun-icon"
-                />
-            )
+            default:
+                return (
+                    <SunIcon
+                        width="100%"
+                        height="100%"
+                        fill={color}
+                        data-testid="sun-icon"
+                    />
+                )
+        }
     }
+
+    return <Box padding={8}>{renderIcon()}</Box>
 }
