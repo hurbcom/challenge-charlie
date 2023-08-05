f6bc6ce87ab8d987fcb610c7f90e1427573e1004
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2023-08-05 19:46:09 -0300

    feat: implement search function and implement useDebounce hook for search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,16 +5,20 @@ import {
   deleteForeCastLocalStorage,
 } from '../utils/weatherForecastLocalStorage.js';
 import useFetch from '../hooks/useFetch.js';
+import useDebounce from '../hooks/useDebounce.js';
 
 import style from '../style/search.module.css';
 
 const Search = () => {
   const [location, setLocation] = useState(null);
+  const [searchValue, setSearchValue] = useState(null);
   const [placeholderCity, setPlaceholderCity] = useState(null);
   const [LocationEnable, setLocationEnable] = useState(false);
+  const [errorRequest, setErrorRequest] = useState(false);
   const [data, setData] = useState(null);
 
   const { request } = useFetch();
+  const debouncedSearchValue = useDebounce(searchValue, 1000);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -46,9 +50,6 @@ const Search = () => {
         const { city: geoLocationCity } = geoLocationResponse.json.results[0].components;
         setPlaceholderCity(geoLocationCity);
         const formattedGeoLocationCity = geoLocationCity.replaceAll(' ', '+').toLowerCase();
-        const weatherForecastResponse = await request(
-          `https://api.openweathermap.org/data/2.5/forecast?q=${formattedGeoLocationCity}&units=metric&lang=pt_br&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`
-        );
         const foreCastLocalStorage = getForecastLocalStorage(formattedGeoLocationCity);
         if (foreCastLocalStorage) {
           const jsonForeCastLocalStorage = JSON.parse(foreCastLocalStorage);
@@ -66,10 +67,42 @@ const Search = () => {
     setDefaultWeatherForecast();
   }, [location]);
 
+  function handleChange(event) {
+    setSearchValue(event.target.value);
+    setErrorRequest(false);
+    setLocation(true);
+  }
+  useEffect(() => {
+    if (debouncedSearchValue) {
+      const searchHandler = async () => {
+        const formattedSearchValue = debouncedSearchValue.replaceAll(' ', '+').toLowerCase();
+        const foreCastLocalStorage = getForecastLocalStorage(formattedSearchValue);
+        if (foreCastLocalStorage) {
+          const jsonForeCastLocalStorage = JSON.parse(foreCastLocalStorage);
+          setData(jsonForeCastLocalStorage);
+        } else {
+          const weatherForecastResponse = await request(
+            `https://api.openweathermap.org/data/2.5/forecast?q=${formattedSearchValue}&units=metric&lang=pt_br&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`
+          );
+          setData(weatherForecastResponse.json);
+          if (weatherForecastResponse.json) {
+            saveForeCastLocalStorage(formattedSearchValue, weatherForecastResponse.json);
+          }
+        }
+      };
+      searchHandler();
+    }
+  }, [debouncedSearchValue, request]);
+
   return (
     <>
       <div className={style.search}>
-        <input placeholder={placeholderCity} className={style.search__bar}></input>
+        <input
+          data-search={'('}
+          onChange={handleChange}
+          placeholder={placeholderCity}
+          className={style.search__bar}
+        ></input>
       </div>
     </>
   );
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
