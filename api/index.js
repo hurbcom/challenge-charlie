b018feeb30051c484128b1ca4288d76795769167
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2023-08-07 12:44:00 -0300

    refactor: separate Bing Image API call logic into fetchBingImgData function

--- api/index.js
+++ api/index.js
@@ -1,30 +1,30 @@
-import express from 'express'
-import cors from 'cors'
-import fetch from 'node-fetch'
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
 
-const app = express()
+const app = express();
 app.use(
   cors({
     origin: 'http://localhost:3000',
   })
-)
+);
 
-const PORT = 8000
+const PORT = 8000;
 
-app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
 
-app.get('/bingImgAPI', (req, response) => {
-  const handleBingImgData = async () => {
-    const resp = await fetch(
-      'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-US'
-    )
+const fetchBingImgData = async () => {
+  const response = await fetch(
+    'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-US'
+  );
+  const data = await response.json();
+  return data.images[0];
+};
 
-    const respJson = await resp.json()
-    const { url, startdate } = respJson.images[0]
-    return response.status(200).json({
-      imgUrl: `https://www.bing.com${url}`,
-      imgStartDate: startdate,
-    })
-  }
-  handleBingImgData()
-})
+app.get('/bingImgAPI', async (req, res) => {
+  const { url, startdate } = await fetchBingImgData();
+  res.status(200).json({
+    imgUrl: `https://www.bing.com${url}`,
+    imgStartDate: startdate,
+  });
+});
